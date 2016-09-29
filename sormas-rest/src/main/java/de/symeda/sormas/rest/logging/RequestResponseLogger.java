0d944fc2d38bf6179fd4e506a70e4eecfcf32668
Jan Falkenstern <j.falkenstern@symeda.de>
2016-09-29 11:55:00 +0200

    #52: request and response-logging

---
+++ sormas-rest/src/main/java/de/symeda/sormas/rest/logging/RequestResponseLogger.java
@@ -0,0 +1,87 @@
+package de.symeda.sormas.rest.logging;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Servlet Filter implementation class RequestLogger
+ */
+@WebFilter("/*")
+public class RequestResponseLogger implements Filter {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	/**
+	 * Default constructor.
+	 */
+	public RequestResponseLogger() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		if (logger.isDebugEnabled()) {
+
+			// request logging
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			logger.debug("requestUri = {}", httpRequest.getRequestURI());
+
+			Map<String, String[]> params = httpRequest.getParameterMap();
+			for (String s : params.keySet()) {
+				// logger.debug(" " + s + " = " + req.getParameter(s));
+				logger.debug("  {} = {}", s, httpRequest.getParameter(s));
+			}
+		}
+
+		if (logger.isTraceEnabled()) {
+			// response logging
+			if (response.getCharacterEncoding() == null) {
+				response.setCharacterEncoding("UTF-8");
+			}
+
+			HttpServletResponseCopier responseCopier = new HttpServletResponseCopier((HttpServletResponse) response);
+
+			try {
+				// pass the request along the filter chain
+				chain.doFilter(request, responseCopier);
+				responseCopier.flushBuffer();
+			} finally {
+				byte[] copy = responseCopier.getCopy();
+				logger.trace(new String(copy, response.getCharacterEncoding()));
+			}
+		}
+
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ sormas-rest/src/main/java/de/symeda/sormas/rest/logging/ServletOutputStreamCopier.java
@@ -0,0 +1,48 @@
+package de.symeda.sormas.rest.logging;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.WriteListener;
+
+/**
+ * see https://patrickgrimard.io/2010/07/28/tutorial-implementing-a-servlet-filter-for-jsonp-callback-with-springs-delegatingfilterproxy/
+ * 
+ * @author Jan Falkenstern
+ *
+ */
+public class ServletOutputStreamCopier extends ServletOutputStream {
+
+    private OutputStream outputStream;
+    private ByteArrayOutputStream copy;
+
+    public ServletOutputStreamCopier(OutputStream outputStream) {
+        this.outputStream = outputStream;
+        this.copy = new ByteArrayOutputStream(1024);
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        outputStream.write(b);
+        copy.write(b);
+    }
+
+    public byte[] getCopy() {
+        return copy.toByteArray();
+    }
+
+	@Override
+	public boolean isReady() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setWriteListener(WriteListener writeListener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
