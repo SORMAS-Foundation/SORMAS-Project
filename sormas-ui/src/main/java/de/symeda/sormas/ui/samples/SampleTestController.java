68a095510ad1a5e8d9a16aa688a65ee86dc474ff
Maté Strysewske <m.strysewske@symeda.de>
2017-02-02 15:40:35 +0100

    #112 - Sample test list and create/edit forms and logic

---
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/samples/SampleTestController.java
@@ -0,0 +1,76 @@
+package de.symeda.sormas.ui.samples;
+
+import java.util.List;
+
+import de.symeda.sormas.api.FacadeProvider;
+import de.symeda.sormas.api.sample.SampleDto;
+import de.symeda.sormas.api.sample.SampleReferenceDto;
+import de.symeda.sormas.api.sample.SampleTestDto;
+import de.symeda.sormas.api.sample.SampleTestFacade;
+import de.symeda.sormas.api.utils.DataHelper;
+import de.symeda.sormas.ui.login.LoginHelper;
+import de.symeda.sormas.ui.utils.CommitDiscardWrapperComponent;
+import de.symeda.sormas.ui.utils.CommitDiscardWrapperComponent.CommitListener;
+import de.symeda.sormas.ui.utils.VaadinUiUtil;
+
+public class SampleTestController {
+	
+	private SampleTestFacade stf = FacadeProvider.getSampleTestFacade();
+	
+	public SampleTestController() { }
+
+	public List<SampleTestDto> getSampleTestsBySample(SampleReferenceDto sampleRef) {
+		return stf.getAllBySample(sampleRef);
+	}
+	
+	public void create(SampleReferenceDto sampleRef, SampleTestGrid grid) {
+		SampleTestEditForm createForm = new SampleTestEditForm();
+		createForm.setValue(createNewSampleTest(sampleRef));
+		final CommitDiscardWrapperComponent<SampleTestEditForm> editView = new CommitDiscardWrapperComponent<SampleTestEditForm>(createForm, createForm.getFieldGroup());
+	
+		editView.addCommitListener(new CommitListener() {
+			@Override
+			public void onCommit() {
+				if(createForm.getFieldGroup().isValid()) {
+					SampleTestDto dto = createForm.getValue();
+					stf.saveSampleTest(dto);
+					grid.reload();
+				}
+			}
+		});
+		
+		VaadinUiUtil.showModalPopupWindow(editView, "Create new sample test result");   
+	}
+	
+	public void edit(SampleTestDto dto, SampleTestGrid grid) {
+		// get fresh data
+		dto = stf.getByUuid(dto.getUuid());
+		
+		SampleTestEditForm form = new SampleTestEditForm();
+		form.setValue(dto);
+		final CommitDiscardWrapperComponent<SampleTestEditForm> editView = new CommitDiscardWrapperComponent<SampleTestEditForm>(form, form.getFieldGroup());
+		
+		editView.addCommitListener(new CommitListener() {
+			@Override
+			public void onCommit() {
+				if(form.getFieldGroup().isValid()) {
+					SampleTestDto dto = form.getValue();
+					stf.saveSampleTest(dto);
+					grid.reload();
+				}
+			}
+		});
+		
+		VaadinUiUtil.showModalPopupWindow(editView, "Edit sample test result");
+	}
+	
+	private SampleTestDto createNewSampleTest(SampleReferenceDto sampleRef) {
+		SampleTestDto sampleTest = new SampleTestDto();
+		sampleTest.setUuid(DataHelper.createUuid());
+		sampleTest.setSample(sampleRef);
+		SampleDto sampleDto = FacadeProvider.getSampleFacade().getSampleByUuid(sampleRef.getUuid());
+		sampleTest.setLab(sampleDto.getLab());
+		sampleTest.setLabUser(LoginHelper.getCurrentUserAsReference());
+		return sampleTest;
+	}
+}
---
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/samples/SampleTestEditForm.java
@@ -0,0 +1,58 @@
+package de.symeda.sormas.ui.samples;
+
+import java.util.Arrays;
+
+import com.vaadin.shared.ui.datefield.Resolution;
+import com.vaadin.ui.CheckBox;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.DateField;
+import com.vaadin.ui.TextArea;
+import com.vaadin.ui.TextField;
+
+import de.symeda.sormas.api.sample.SampleTestDto;
+import de.symeda.sormas.api.sample.SampleTestType;
+import de.symeda.sormas.api.utils.DateHelper;
+import de.symeda.sormas.ui.utils.AbstractEditForm;
+import de.symeda.sormas.ui.utils.FieldHelper;
+import de.symeda.sormas.ui.utils.LayoutUtil;
+
+@SuppressWarnings("serial")
+public class SampleTestEditForm extends AbstractEditForm<SampleTestDto> {
+
+	private static final String HTML_LAYOUT = 
+			LayoutUtil.fluidRowLocs(SampleTestDto.TEST_TYPE, SampleTestDto.TEST_TYPE_TEXT) +
+			LayoutUtil.fluidRowLocs(SampleTestDto.TEST_DATE_TIME, SampleTestDto.LAB) +
+			LayoutUtil.fluidRowLocs(SampleTestDto.TEST_RESULT, SampleTestDto.TEST_RESULT_VERIFIED) +
+			LayoutUtil.fluidRowLocs(SampleTestDto.TEST_RESULT_TEXT);
+	
+	public SampleTestEditForm() {
+		super(SampleTestDto.class, SampleTestDto.I18N_PREFIX);
+	}
+	
+	@Override
+	protected void addFields() {
+		addField(SampleTestDto.TEST_TYPE, ComboBox.class);
+		addField(SampleTestDto.TEST_TYPE_TEXT, TextField.class);
+		DateField testDateTime = addField(SampleTestDto.TEST_DATE_TIME, DateField.class);
+		testDateTime.setResolution(Resolution.MINUTE);
+		testDateTime.setDateFormat(DateHelper.getTimeDateFormat().toPattern());
+		addField(SampleTestDto.LAB, ComboBox.class);
+		addField(SampleTestDto.TEST_RESULT, ComboBox.class);
+		addField(SampleTestDto.TEST_RESULT_VERIFIED, CheckBox.class);
+		addField(SampleTestDto.TEST_RESULT_TEXT, TextArea.class).setRows(3);
+		
+		setReadOnly(true, SampleTestDto.LAB);
+
+		FieldHelper.setVisibleWhen(getFieldGroup(), SampleTestDto.TEST_TYPE_TEXT, SampleTestDto.TEST_TYPE, Arrays.asList(SampleTestType.OTHER), true);
+		FieldHelper.setRequiredWhen(getFieldGroup(), SampleTestDto.TEST_TYPE, Arrays.asList(SampleTestDto.TEST_TYPE_TEXT), Arrays.asList(SampleTestType.OTHER));
+		
+		setRequired(true, SampleTestDto.TEST_TYPE, SampleTestDto.TEST_DATE_TIME, SampleTestDto.LAB,
+				SampleTestDto.TEST_RESULT, SampleTestDto.TEST_RESULT_TEXT, SampleTestDto.TEST_RESULT_VERIFIED);
+	}
+	
+	@Override
+	protected String createHtmlLayout() {
+		return HTML_LAYOUT;
+	}
+	
+}
