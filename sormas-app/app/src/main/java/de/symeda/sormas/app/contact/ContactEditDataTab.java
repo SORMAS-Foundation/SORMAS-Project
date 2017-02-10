8e2d352fa4ff27fdb78a93c05377eac61c3839b6
Maté Strysewske <m.strysewske@symeda.de>
2017-02-10 08:44:35 +0100

    #96 - link to case from contact

--- sormas-app/app/src/main/java/de/symeda/sormas/app/contact/ContactEditDataTab.java
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/contact/ContactEditDataTab.java
@@ -1,5 +1,6 @@
 package de.symeda.sormas.app.contact;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -10,10 +11,13 @@ import android.view.ViewGroup;
 import de.symeda.sormas.api.contact.ContactProximity;
 import de.symeda.sormas.api.contact.ContactRelation;
 import de.symeda.sormas.app.R;
+import de.symeda.sormas.app.backend.caze.Case;
+import de.symeda.sormas.app.backend.caze.CaseDao;
 import de.symeda.sormas.app.backend.common.AbstractDomainObject;
 import de.symeda.sormas.app.backend.common.DatabaseHelper;
 import de.symeda.sormas.app.backend.contact.Contact;
 import de.symeda.sormas.app.backend.contact.ContactDao;
+import de.symeda.sormas.app.caze.CaseEditActivity;
 import de.symeda.sormas.app.component.FieldHelper;
 import de.symeda.sormas.app.databinding.ContactDataFragmentLayoutBinding;
 import de.symeda.sormas.app.util.DataUtils;
@@ -47,6 +51,27 @@ public class ContactEditDataTab extends FormTab {
         binding.contactContactProximity.initialize(ContactProximity.class);
 
         FieldHelper.initSpinnerField(binding.contactRelationToCase, ContactRelation.class);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        binding.contactCazeUuid.makeLink(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.getContact().getCaze() != null) {
+                    final CaseDao caseDao = DatabaseHelper.getCaseDao();
+                    final Case caze = caseDao.queryUuid(binding.getContact().getCaze().getUuid());
+                    showCaseEditView(caze);
+                }
+            }
+        });
+    }
+
+    public void showCaseEditView(Case caze) {
+        Intent intent = new Intent(getActivity(), CaseEditActivity.class);
+        intent.putExtra(CaseEditActivity.KEY_CASE_UUID, caze.getUuid());
+        startActivity(intent);
     }
 
     @Override
