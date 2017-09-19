14ee1dd6013833b521e7f829237e1f972335e5e9
Martin Wahnschaffe <m.wahnschaffe@symeda.de>
2017-09-19 13:09:13 +0200

    #82 comment

--- sormas-api/src/main/java/de/symeda/sormas/api/region/GeoShapeProvider.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/region/GeoShapeProvider.java
@@ -5,6 +5,9 @@ import javax.ejb.Remote;
 @Remote
 public interface GeoShapeProvider {
 
+	/**
+	 * @return array of polygons, which are arrays of geo-points
+	 */
 	GeoLatLon[][] getRegionShape(RegionReferenceDto region);
 
 	RegionReferenceDto getRegionByCoord(GeoLatLon latLon);
