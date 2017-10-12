6f2fe6246c23627b25a0455146eeb2fc3bb6b253
Maté Strysewske <m.strysewske@symeda.de>
2017-10-12 09:28:03 +0200

    #359 - created TestDataCreator class and refactored case and contact test classes

---
+++ sormas-backend/src/test/java/de/symeda/sormas/backend/TestDataCreator.java
@@ -0,0 +1,228 @@
+package de.symeda.sormas.backend;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashSet;
+
+import org.junit.Test;
+
+import de.symeda.sormas.api.Disease;
+import de.symeda.sormas.api.caze.CaseClassification;
+import de.symeda.sormas.api.caze.CaseDataDto;
+import de.symeda.sormas.api.caze.CaseFacade;
+import de.symeda.sormas.api.caze.InvestigationStatus;
+import de.symeda.sormas.api.contact.ContactDto;
+import de.symeda.sormas.api.contact.ContactFacade;
+import de.symeda.sormas.api.facility.FacilityFacade;
+import de.symeda.sormas.api.facility.FacilityType;
+import de.symeda.sormas.api.person.PersonDto;
+import de.symeda.sormas.api.person.PersonFacade;
+import de.symeda.sormas.api.region.CommunityFacade;
+import de.symeda.sormas.api.region.DistrictFacade;
+import de.symeda.sormas.api.region.RegionFacade;
+import de.symeda.sormas.api.task.TaskContext;
+import de.symeda.sormas.api.task.TaskDto;
+import de.symeda.sormas.api.task.TaskFacade;
+import de.symeda.sormas.api.task.TaskStatus;
+import de.symeda.sormas.api.task.TaskType;
+import de.symeda.sormas.api.user.UserDto;
+import de.symeda.sormas.api.user.UserFacade;
+import de.symeda.sormas.api.user.UserRole;
+import de.symeda.sormas.api.utils.DataHelper;
+import de.symeda.sormas.api.visit.VisitDto;
+import de.symeda.sormas.api.visit.VisitFacade;
+import de.symeda.sormas.api.visit.VisitStatus;
+import de.symeda.sormas.backend.facility.Facility;
+import de.symeda.sormas.backend.facility.FacilityService;
+import de.symeda.sormas.backend.region.Community;
+import de.symeda.sormas.backend.region.CommunityService;
+import de.symeda.sormas.backend.region.District;
+import de.symeda.sormas.backend.region.DistrictService;
+import de.symeda.sormas.backend.region.Region;
+import de.symeda.sormas.backend.region.RegionService;
+import info.novatec.beantest.api.BaseBeanTest;
+
+public class TestDataCreator extends BaseBeanTest {
+	
+	private final UserFacade userFacade;
+	private final PersonFacade personFacade;
+	private final CaseFacade caseFacade;
+	private final ContactFacade contactFacade;
+	private final TaskFacade taskFacade;
+	private final VisitFacade visitFacade;
+	private final RegionFacade regionFacade;
+	private final DistrictFacade districtFacade;
+	private final CommunityFacade communityFacade;
+	private final FacilityFacade facilityFacade;
+	
+	private final RegionService regionService;
+	private final DistrictService districtService;
+	private final CommunityService communityService;
+	private final FacilityService facilityService;
+
+	public TestDataCreator(UserFacade userFacade, PersonFacade personFacade, CaseFacade caseFacade,
+			ContactFacade contactFacade, TaskFacade taskFacade, VisitFacade visitFacade, RegionFacade regionFacade,
+			DistrictFacade districtFacade, CommunityFacade communityFacade, FacilityFacade facilityFacade,
+			RegionService regionService, DistrictService districtService, CommunityService communityService,
+			FacilityService facilityService) {
+		this.userFacade = userFacade;
+		this.personFacade = personFacade;
+		this.caseFacade = caseFacade;
+		this.contactFacade = contactFacade;
+		this.taskFacade = taskFacade;
+		this.visitFacade = visitFacade;
+		this.regionFacade = regionFacade;
+		this.districtFacade = districtFacade;
+		this.communityFacade = communityFacade;
+		this.facilityFacade = facilityFacade;
+		this.regionService = regionService;
+		this.districtService = districtService;
+		this.communityService = communityService;
+		this.facilityService = facilityService;
+	}
+	
+	@Test
+	public UserDto createUser(String regionUuid, String districtUuid, String firstName, String lastName, UserRole... roles) {
+		UserDto user = new UserDto();
+		user.setUuid(DataHelper.createUuid());
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setUserName(firstName + lastName);
+		user.setUserRoles(new HashSet<UserRole>(Arrays.asList(roles)));
+		user.setRegion(regionFacade.getRegionByUuid(regionUuid));
+		user.setDistrict(districtFacade.getDistrictByUuid(districtUuid));
+		user = userFacade.saveUser(user);
+		
+		return user;
+	}
+
+	@Test
+	public PersonDto createPerson(String firstName, String lastName) {
+		PersonDto cazePerson = new PersonDto();
+		cazePerson.setUuid(DataHelper.createUuid());
+		cazePerson.setFirstName(firstName);
+		cazePerson.setLastName(lastName);
+		cazePerson = personFacade.savePerson(cazePerson);
+		
+		return cazePerson;
+	}
+
+	@Test
+	public CaseDataDto createCase(UserDto user, PersonDto cazePerson, Disease disease, CaseClassification caseClassification, 
+			InvestigationStatus investigationStatus, Date reportDate, RDCF rdcf) {
+		CaseDataDto caze = new CaseDataDto();
+		caze.setUuid(DataHelper.createUuid());
+		caze.setPerson(cazePerson);
+		caze.setReportDate(reportDate);
+		caze.setReportingUser(user);
+		caze.setDisease(disease);
+		caze.setCaseClassification(caseClassification);
+		caze.setInvestigationStatus(investigationStatus);
+		caze.setRegion(regionFacade.getRegionByUuid(rdcf.region.getUuid()));
+		caze.setDistrict(districtFacade.getDistrictByUuid(rdcf.district.getUuid()));
+		caze.setCommunity(communityFacade.getByUuid(rdcf.community.getUuid()));
+		caze.setHealthFacility(facilityFacade.getByUuid(rdcf.facility.getUuid()));		
+		
+		caze = caseFacade.saveCase(caze);
+		
+		return caze;
+	}
+
+	@Test
+	public ContactDto createContact(UserDto reportingUser, UserDto contactOfficer, PersonDto contactPerson,
+			CaseDataDto caze, Date reportDateTime, Date lastContactDate) {
+		ContactDto contact = new ContactDto();
+		contact.setUuid(DataHelper.createUuid());
+		contact.setReportingUser(reportingUser);
+		contact.setContactOfficer(contactOfficer);
+		contact.setPerson(contactPerson);
+		contact.setCaze(caze);
+		contact.setReportDateTime(reportDateTime);
+		contact.setLastContactDate(lastContactDate);
+		
+		contact = contactFacade.saveContact(contact);
+		
+		return contact;
+	}
+
+	@Test
+	public TaskDto createTask(TaskContext context, TaskType type, TaskStatus status, CaseDataDto caze,
+			ContactDto contact, UserDto assigneeUser) {
+		TaskDto task = new TaskDto();
+		task.setUuid(DataHelper.createUuid());
+		task.setTaskContext(context);
+		task.setTaskType(type);
+		task.setTaskStatus(status);
+		if (caze != null) {
+			task.setCaze(caze);
+		}
+		if (contact != null) {
+			task.setContact(contact);
+		}
+		task.setAssigneeUser(assigneeUser);
+		
+		task = taskFacade.saveTask(task);
+		
+		return task;
+	}
+	
+	@Test
+	public VisitDto createVisit(Disease disease, PersonDto contactPerson, Date visitDateTime, VisitStatus visitStatus) {
+		VisitDto visit = new VisitDto();
+		visit.setUuid(DataHelper.createUuid());
+		visit.setDisease(disease);
+		visit.setPerson(contactPerson);
+		visit.setVisitDateTime(visitDateTime);
+		visit.setVisitStatus(visitStatus);
+		
+		visit = visitFacade.saveVisit(visit);
+		
+		return visit;
+	}
+	
+	@Test
+	public RDCF createRDCF(String regionName, String districtName, String communityName, String facilityName) {
+		Region region = new Region();
+		region.setUuid(DataHelper.createUuid());
+		region.setName(regionName);
+		regionService.persist(region);
+		
+		District district = new District();
+		district.setUuid(DataHelper.createUuid());
+		district.setName(districtName);
+		district.setRegion(region);
+		districtService.persist(district);
+		
+		Community community = new Community();
+		community.setUuid(DataHelper.createUuid());
+		community.setName(communityName);
+		community.setDistrict(district);
+		communityService.persist(community);
+		
+		Facility facility = new Facility();
+		facility.setUuid(DataHelper.createUuid());
+		facility.setName(facilityName);
+		facility.setType(FacilityType.PRIMARY);
+		facility.setCommunity(community);
+		facility.setDistrict(district);
+		facility.setRegion(region);
+		facilityService.persist(facility);
+		
+		return new RDCF(region, district, community, facility);
+	}
+	
+	public class RDCF {
+		public Region region;
+		public District district;
+		public Community community;
+		public Facility facility;
+		
+		public RDCF(Region region, District district, Community community, Facility facility) {
+			this.region = region;
+			this.district = district;
+			this.community = community;
+			this.facility = facility;
+		}
+	}
+
+}
--- sormas-backend/src/test/java/de/symeda/sormas/backend/caze/CaseFacadeEjbTest.java
+++ sormas-backend/src/test/java/de/symeda/sormas/backend/caze/CaseFacadeEjbTest.java
@@ -3,9 +3,7 @@ package de.symeda.sormas.backend.caze;
 import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
 
 import org.junit.Before;
@@ -20,43 +18,34 @@ import de.symeda.sormas.api.contact.ContactDto;
 import de.symeda.sormas.api.contact.ContactFacade;
 import de.symeda.sormas.api.contact.FollowUpStatus;
 import de.symeda.sormas.api.facility.FacilityFacade;
-import de.symeda.sormas.api.facility.FacilityType;
-import de.symeda.sormas.api.hospitalization.HospitalizationFacade;
 import de.symeda.sormas.api.hospitalization.PreviousHospitalizationDto;
 import de.symeda.sormas.api.person.PersonDto;
-import de.symeda.sormas.api.person.PersonFacade;
 import de.symeda.sormas.api.region.CommunityFacade;
-import de.symeda.sormas.api.region.DistrictFacade;
-import de.symeda.sormas.api.region.RegionFacade;
 import de.symeda.sormas.api.task.TaskContext;
 import de.symeda.sormas.api.task.TaskDto;
 import de.symeda.sormas.api.task.TaskFacade;
 import de.symeda.sormas.api.task.TaskStatus;
 import de.symeda.sormas.api.task.TaskType;
 import de.symeda.sormas.api.user.UserDto;
-import de.symeda.sormas.api.user.UserFacade;
 import de.symeda.sormas.api.user.UserRole;
-import de.symeda.sormas.api.utils.DataHelper;
 import de.symeda.sormas.backend.MockProducer;
+import de.symeda.sormas.backend.TestDataCreator;
+import de.symeda.sormas.backend.TestDataCreator.RDCF;
 import de.symeda.sormas.backend.caze.CaseFacadeEjb.CaseFacadeEjbLocal;
 import de.symeda.sormas.backend.contact.ContactFacadeEjb.ContactFacadeEjbLocal;
-import de.symeda.sormas.backend.facility.Facility;
 import de.symeda.sormas.backend.facility.FacilityFacadeEjb;
 import de.symeda.sormas.backend.facility.FacilityService;
-import de.symeda.sormas.backend.hospitalization.HospitalizationFacadeEjb;
 import de.symeda.sormas.backend.person.PersonFacadeEjb;
-import de.symeda.sormas.backend.region.Community;
 import de.symeda.sormas.backend.region.CommunityFacadeEjb;
 import de.symeda.sormas.backend.region.CommunityService;
-import de.symeda.sormas.backend.region.District;
 import de.symeda.sormas.backend.region.DistrictFacadeEjb;
 import de.symeda.sormas.backend.region.DistrictService;
-import de.symeda.sormas.backend.region.Region;
 import de.symeda.sormas.backend.region.RegionFacadeEjb;
 import de.symeda.sormas.backend.region.RegionService;
 import de.symeda.sormas.backend.task.TaskFacadeEjb;
 import de.symeda.sormas.backend.user.UserFacadeEjb.UserFacadeEjbLocal;
 import de.symeda.sormas.backend.util.DateHelper8;
+import de.symeda.sormas.backend.visit.VisitFacadeEjb;
 import info.novatec.beantest.api.BaseBeanTest;
 
 public class CaseFacadeEjbTest extends BaseBeanTest {
@@ -71,33 +60,18 @@ public class CaseFacadeEjbTest extends BaseBeanTest {
 
 	@Test
 	public void testDiseaseChangeUpdatesContacts() {
-
-		// TODO create provider for facades (probably best to add a SormasBeanTest class)
-		UserFacade userFacade = getBean(UserFacadeEjbLocal.class);
-		PersonFacade personFacade = getBean(PersonFacadeEjb.class);
 		CaseFacade caseFacade = getBean(CaseFacadeEjbLocal.class);
 		ContactFacade contactFacade = getBean(ContactFacadeEjbLocal.class);
 
-		RDCF rdcf = createRDCF("Region", "District", "Community", "Facility");
-		UserDto user = createSurveillanceSupervisor(userFacade, rdcf.region.getUuid(), rdcf.district.getUuid());
-		PersonDto cazePerson = createCasePerson(personFacade);
-		CaseDataDto caze = createCase(caseFacade, user, cazePerson, rdcf);
-
-		PersonDto contactPerson = new PersonDto();
-		cazePerson.setUuid(DataHelper.createUuid());
-		contactPerson.setFirstName("Steff");
-		contactPerson.setLastName("Hansen");
-		contactPerson = personFacade.savePerson(contactPerson);
-
-		ContactDto contact = new ContactDto();
-		contact.setUuid(DataHelper.createUuid());
-		contact.setReportDateTime(new Date());
-		contact.setReportingUser(user);
-		contact.setContactOfficer(user);
-		contact.setPerson(contactPerson);
-		contact.setCaze(caze);
-		contact.setLastContactDate(new Date());
-		contact = contactFacade.saveContact(contact);
+		TestDataCreator creator = createTestDataCreator();
+	
+		RDCF rdcf = creator.createRDCF("Region", "District", "Community", "Facility");
+		UserDto user = creator.createUser(rdcf.region.getUuid(), rdcf.district.getUuid(), "Surv", "Sup", UserRole.SURVEILLANCE_SUPERVISOR);
+		PersonDto cazePerson = creator.createPerson("Case", "Person");
+		CaseDataDto caze = creator.createCase(user, cazePerson, Disease.EVD, CaseClassification.PROBABLE,
+				InvestigationStatus.PENDING, new Date(), rdcf);
+		PersonDto contactPerson = creator.createPerson("Contact", "Person");
+		ContactDto contact = creator.createContact(user, user, contactPerson, caze, new Date(), new Date());
 
 		// Follow-up status and duration should be set to the requirements for EVD
 		assertEquals(FollowUpStatus.FOLLOW_UP, contact.getFollowUpStatus());
@@ -115,47 +89,23 @@ public class CaseFacadeEjbTest extends BaseBeanTest {
 
 	@Test
 	public void testMovingCaseUpdatesTaskAssigneeAndCreatesPreviousHospitalization() {
-		
-		// TODO create provider for facades (probably best to add a SormasBeanTest class)
-		UserFacade userFacade = getBean(UserFacadeEjbLocal.class);
 		CaseFacade caseFacade = getBean(CaseFacadeEjbLocal.class);
-		PersonFacade personFacade = getBean(PersonFacadeEjb.class);
 		TaskFacade taskFacade = getBean(TaskFacadeEjb.class);
 		CommunityFacade communityFacade = getBean(CommunityFacadeEjb.class);
 		FacilityFacade facilityFacade = getBean(FacilityFacadeEjb.class);
 
-		RDCF rdcf = createRDCF("Region", "District", "Community", "Facility");
-		UserDto survSupervisor = createSurveillanceSupervisor(userFacade, rdcf.region.getUuid(), rdcf.district.getUuid());
-		PersonDto cazePerson = createCasePerson(personFacade);
-		CaseDataDto caze = createCase(caseFacade, survSupervisor, cazePerson, rdcf);
-
-		UserDto caseOfficer = new UserDto();
-		caseOfficer.setUuid(DataHelper.createUuid());
-		caseOfficer.setFirstName("Case");
-		caseOfficer.setLastName("Officer");
-		caseOfficer.setUserName("CaseOfficer");
-		caseOfficer.setUserRoles(new HashSet<UserRole>(Arrays.asList(UserRole.CASE_OFFICER)));
-		caseOfficer = userFacade.saveUser(caseOfficer);
+		TestDataCreator creator = createTestDataCreator();
 		
-		TaskDto pendingTask = new TaskDto();
-		pendingTask.setUuid(DataHelper.createUuid());
-		pendingTask.setTaskContext(TaskContext.CASE);
-		pendingTask.setTaskType(TaskType.CASE_INVESTIGATION);
-		pendingTask.setTaskStatus(TaskStatus.PENDING);
-		pendingTask.setCaze(caze);
-		pendingTask.setAssigneeUser(survSupervisor);
-		pendingTask = taskFacade.saveTask(pendingTask);
+		RDCF rdcf = creator.createRDCF("Region", "District", "Community", "Facility");
+		UserDto user = creator.createUser(rdcf.region.getUuid(), rdcf.district.getUuid(), "Surv", "Sup", UserRole.SURVEILLANCE_SUPERVISOR);
+		PersonDto cazePerson = creator.createPerson("Case", "Person");
+		CaseDataDto caze = creator.createCase(user, cazePerson, Disease.EVD, CaseClassification.PROBABLE,
+				InvestigationStatus.PENDING, new Date(), rdcf);
+		UserDto caseOfficer = creator.createUser(rdcf.region.getUuid(), rdcf.district.getUuid(), "Case", "Officer", UserRole.CASE_OFFICER);
+		TaskDto pendingTask = creator.createTask(TaskContext.CASE, TaskType.CASE_INVESTIGATION, TaskStatus.PENDING, caze, null, user);
+		TaskDto doneTask = creator.createTask(TaskContext.CASE, TaskType.CASE_INVESTIGATION, TaskStatus.DONE, caze, null, user);
 		
-		TaskDto doneTask = new TaskDto();
-		doneTask.setUuid(DataHelper.createUuid());
-		doneTask.setTaskContext(TaskContext.CASE);
-		doneTask.setTaskType(TaskType.CASE_INVESTIGATION);
-		doneTask.setTaskStatus(TaskStatus.DONE);
-		doneTask.setCaze(caze);
-		doneTask.setAssigneeUser(survSupervisor);
-		doneTask = taskFacade.saveTask(doneTask);
-		
-		RDCF newRDCF = createRDCF("New Region", "New District", "New Community", "New Facility");
+		RDCF newRDCF = creator.createRDCF("New Region", "New District", "New Community", "New Facility");
 		caseFacade.moveCase(caze, communityFacade.getByUuid(newRDCF.community.getUuid()), facilityFacade.getByUuid(newRDCF.facility.getUuid()), caze.getHealthFacilityDetails(), caseOfficer);
 		
 		caze = caseFacade.getCaseDataByUuid(caze.getUuid());
@@ -170,114 +120,19 @@ public class CaseFacadeEjbTest extends BaseBeanTest {
 		
 		// Pending task should've been reassigned to the case officer, done task should still be assigned to the surveillance supervisor
 		assertEquals(pendingTask.getAssigneeUser().getUuid(), caseOfficer.getUuid());
-		assertEquals(doneTask.getAssigneeUser().getUuid(), survSupervisor.getUuid());
+		assertEquals(doneTask.getAssigneeUser().getUuid(), user.getUuid());
 		
 		// A previous hospitalization with the former facility should have been created
 		List<PreviousHospitalizationDto> previousHospitalizations = caze.getHospitalization().getPreviousHospitalizations();
 		assertEquals(previousHospitalizations.size(), 1);
 	}
 	
-	private UserDto createSurveillanceSupervisor(UserFacade userFacade, String regionUuid, String districtUuid) {
-		RegionFacade regionFacade = getBean(RegionFacadeEjb.class);
-		DistrictFacade districtFacade = getBean(DistrictFacadeEjb.class);
-		
-		// TODO handle user creation at a central place
-		UserDto user = new UserDto();
-		user.setUuid(DataHelper.createUuid());
-		user.setFirstName("Admin");
-		user.setLastName("Symeda");
-		user.setUserName("AdminSymeda");
-		user.setUserRoles(new HashSet<UserRole>(Arrays.asList(UserRole.SURVEILLANCE_SUPERVISOR)));
-		user.setRegion(regionFacade.getRegionByUuid(regionUuid));
-		user.setDistrict(districtFacade.getDistrictByUuid(districtUuid));
-		user = userFacade.saveUser(user);
-		
-		return user;
+	private TestDataCreator createTestDataCreator() {
+		return new TestDataCreator(getBean(UserFacadeEjbLocal.class), getBean(PersonFacadeEjb.class),
+				getBean(CaseFacadeEjbLocal.class), getBean(ContactFacadeEjbLocal.class), getBean(TaskFacadeEjb.class),
+				getBean(VisitFacadeEjb.class), getBean(RegionFacadeEjb.class), getBean(DistrictFacadeEjb.class), getBean(CommunityFacadeEjb.class), 
+				getBean(FacilityFacadeEjb.class), getBean(RegionService.class), getBean(DistrictService.class),
+				getBean(CommunityService.class), getBean(FacilityService.class));
 	}
-	
-	private PersonDto createCasePerson(PersonFacade personFacade) {
-		// TODO add create method to PersonFacade
-		PersonDto cazePerson = new PersonDto();
-		cazePerson.setUuid(DataHelper.createUuid());
-		cazePerson.setFirstName("Tim");
-		cazePerson.setLastName("Kunsen");
-		cazePerson = personFacade.savePerson(cazePerson);
-		
-		return cazePerson;
-	}
-	
-	private CaseDataDto createCase(CaseFacade caseFacade, UserDto user, PersonDto cazePerson, RDCF rdcf) {
-		RegionFacade regionFacade = getBean(RegionFacadeEjb.class);
-		DistrictFacade districtFacade = getBean(DistrictFacadeEjb.class);
-		CommunityFacade communityFacade = getBean(CommunityFacadeEjb.class);
-		FacilityFacade facilityFacade = getBean(FacilityFacadeEjb.class);
-		
-		// TODO add create method to CaseFacade that takes a person
-		CaseDataDto caze = new CaseDataDto();
-		caze.setPerson(cazePerson);
-		caze.setReportDate(new Date());
-		caze.setReportingUser(user);
-		caze.setDisease(Disease.EVD);
-		caze.setCaseClassification(CaseClassification.PROBABLE);
-		caze.setInvestigationStatus(InvestigationStatus.PENDING);
-		caze.setRegion(regionFacade.getRegionByUuid(rdcf.region.getUuid()));
-		caze.setDistrict(districtFacade.getDistrictByUuid(rdcf.district.getUuid()));
-		caze.setCommunity(communityFacade.getByUuid(rdcf.community.getUuid()));
-		caze.setHealthFacility(facilityFacade.getByUuid(rdcf.facility.getUuid()));		
-		
-		caze = caseFacade.saveCase(caze);
-		
-		return caze;
-	}
-	
-	private RDCF createRDCF(String regionName, String districtName, String communityName, String facilityName) {
-		RegionService regionService = getBean(RegionService.class);
-		DistrictService districtService = getBean(DistrictService.class);
-		CommunityService communityService = getBean(CommunityService.class);
-		FacilityService facilityService = getBean(FacilityService.class);
-		
-		Region region = new Region();
-		region.setUuid(DataHelper.createUuid());
-		region.setName(regionName);
-		regionService.persist(region);
-		
-		District district = new District();
-		district.setUuid(DataHelper.createUuid());
-		district.setName(districtName);
-		district.setRegion(region);
-		districtService.persist(district);
-		
-		Community community = new Community();
-		community.setUuid(DataHelper.createUuid());
-		community.setName("New Community");
-		community.setDistrict(district);
-		communityService.persist(community);
-		
-		Facility facility = new Facility();
-		facility.setUuid(DataHelper.createUuid());
-		facility.setName("New Facility");
-		facility.setType(FacilityType.PRIMARY);
-		facility.setCommunity(community);
-		facility.setDistrict(district);
-		facility.setRegion(region);
-		facilityService.persist(facility);
-		
-		return new RDCF(region, district, community, facility);
-	}
-	
-	private class RDCF {
-		public Region region;
-		public District district;
-		public Community community;
-		public Facility facility;
-		
-		public RDCF(Region region, District district, Community community, Facility facility) {
-			this.region = region;
-			this.district = district;
-			this.community = community;
-			this.facility = facility;
-		}
-	}
-	
 
 }
