2df96dcada158716eff58dcfb4fc2cb21bbfc68d
Martin Wahnschaffe <m.wahnschaffe@symeda.de>
2017-12-07 16:18:17 +0100

    Unit test for imported health facilities #430

---
+++ sormas-backend/src/test/java/de/symeda/sormas/backend/facility/FacilityServiceTest.java
@@ -0,0 +1,83 @@
+package de.symeda.sormas.backend.facility;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import java.lang.annotation.Annotation;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import de.symeda.sormas.api.facility.FacilityDto;
+import de.symeda.sormas.backend.common.ConfigService;
+import de.symeda.sormas.backend.region.CommunityService;
+import de.symeda.sormas.backend.region.DistrictService;
+import de.symeda.sormas.backend.region.Region;
+import de.symeda.sormas.backend.region.RegionService;
+import info.novatec.beantest.api.BeanProviderHelper;
+
+public class FacilityServiceTest {
+
+    private static BeanProviderHelper bm;
+    
+    @BeforeClass
+    public static void initilaize() {
+        bm = BeanProviderHelper.getInstance();
+        
+		ConfigService configService = getBean(ConfigService.class);
+		FacilityService facilityService = getBean(FacilityService.class);
+		RegionService regionService = getBean(RegionService.class);
+		DistrictService districtService = getBean(DistrictService.class);
+		CommunityService communityService = getBean(CommunityService.class);
+
+		String countryName = configService.getCountryName();
+
+		List<Region> regions = regionService.getAll();
+
+		regionService.importRegions(countryName, regions);
+		districtService.importDistricts(countryName, regions);
+		communityService.importCommunities(countryName, regions);
+		
+		facilityService.importFacilities(countryName);
+    }
+
+    @AfterClass
+    public static void cleanUp() {
+        bm.shutdown();
+    }
+    
+    protected static <T> T getBean(Class<T> beanClass, Annotation... qualifiers) {
+        return bm.getBean(beanClass, qualifiers);
+    }
+
+	@Test
+	public void testImportHealthFacilities() {
+		FacilityService facilityService = getBean(FacilityService.class);
+		RegionService regionService = getBean(RegionService.class);
+
+		List<Facility> healthFacilitiesByDistrict = facilityService.getHealthFacilitiesByDistrict(regionService.getAll().get(0).getDistricts().get(0), false);
+		// List should have some entries
+		assertThat(healthFacilitiesByDistrict.size(), greaterThan(1));
+	}
+
+	@Test
+	public void testImportSpecialHealthFacilities() {
+		FacilityService facilityService = getBean(FacilityService.class);
+		Facility otherFacility = facilityService.getByUuid(FacilityDto.OTHER_FACILITY_UUID);
+		assertNotNull(otherFacility);
+		Facility noneFacility = facilityService.getByUuid(FacilityDto.NONE_FACILITY_UUID);
+		assertNotNull(noneFacility);
+	}
+
+	@Test
+	public void testImportLaboratories() {
+		FacilityService facilityService = getBean(FacilityService.class);
+
+		List<Facility> allLaboratories = facilityService.getAllLaboratories();
+		// List should have some entries
+		assertThat(allLaboratories.size(), greaterThan(1));
+	}
+}
