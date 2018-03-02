e545e44cbb89b82dd578d3d7c5cde3e920d91b62
Sampson Orson Jackson <sampson.orson@technologyboard.org>
2018-03-02 23:28:12 +0100

    Linked the Event listing page to the real database

--- sormas-app/app/src/main/java/de/symeda/sormas/app/event/list/EventsSearchByEventStatusStrategy.java
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/event/list/EventsSearchByEventStatusStrategy.java
@@ -1,11 +1,9 @@
 package de.symeda.sormas.app.event.list;
 
-import de.symeda.sormas.app.util.MemoryDatabaseHelper;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import de.symeda.sormas.api.event.EventStatus;
+import de.symeda.sormas.app.backend.common.DatabaseHelper;
 import de.symeda.sormas.app.backend.event.Event;
 
 /**
@@ -21,21 +19,15 @@ public class EventsSearchByEventStatusStrategy implements IEventsSearchStrategy 
 
     @Override
     public List<Event> search() {
-        List<Event> list = new ArrayList<>();
+        List<Event> list;
 
         //TODO: Make changes here
         if (status == null) {
-            //return DatabaseHelper.getContactDao().queryForAll(Contact.REPORT_DATE_TIME, false);
-            return new ArrayList<>();
+            list = DatabaseHelper.getEventDao().queryForAll(Event.EVENT_DATE, false);
+        } else {
+            list = DatabaseHelper.getEventDao().queryForEq(Event.EVENT_STATUS, status, Event.EVENT_DATE, false);
         }
 
-        if (status == EventStatus.POSSIBLE) {
-            list = MemoryDatabaseHelper.EVENT.getPossibleEvents(20);
-        } else if (status == EventStatus.CONFIRMED) {
-            list = MemoryDatabaseHelper.EVENT.getConfirmedEvents(20);
-        } else if (status == EventStatus.NO_EVENT) {
-            list = MemoryDatabaseHelper.EVENT.getNoEvents(20);
-        }
         return list;
     }
 }
