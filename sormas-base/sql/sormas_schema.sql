7ed971815bf769ab1a51151069bc265304755ec8
Maté Strysewske <m.strysewske@symeda.de>
2018-03-06 14:07:32 +0100

    merging

--- sormas-base/sql/sormas_schema.sql
+++ sormas-base/sql/sormas_schema.sql
@@ -2177,17 +2177,17 @@ $resultid$  LANGUAGE plpgsql;
 INSERT INTO schema_version (version_number, comment) VALUES (96, 'SQL function to easily add new health facilities to the database #519');
 
 -- 2018-03-05 Add upgrade column to schema_version for backend upgrade logic #402
-
 ALTER TABLE schema_version ADD COLUMN upgradeNeeded boolean NOT NULL DEFAULT false;
 UPDATE schema_version SET upgradeNeeded=true WHERE version_number=95;
 GRANT SELECT, UPDATE ON TABLE schema_version TO sormas_user;
 
 INSERT INTO schema_version (version_number, comment) VALUES (97, 'Add upgrade column to schema_version for backend upgrade logic #402');
--- 2018-03-06 Restrict outbreak mode to CSM for now
 
+-- 2018-03-06 Restrict outbreak mode to CSM for now
 DELETE FROM outbreak WHERE NOT (disease = 'CSM');
 
 INSERT INTO schema_version (version_number, comment) VALUES (98, 'Restrict outbreak mode to CSM for now #489');
+
 -- 2018-03-06 Change export functions #507
 DROP FUNCTION export_database(text, text, text);
 DROP FUNCTION export_database_join(text, text, text, text, text, text);
