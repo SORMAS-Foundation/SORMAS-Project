d01190e9a1df7bd51322dc0e5cb19b4cfa8aab30
Maté Strysewske <m.strysewske@symeda.de>
2018-05-08 15:32:13 +0200

    #541 - needsSorting field for StatisticsCaseAttribute

--- sormas-api/src/main/java/de/symeda/sormas/api/statistics/StatisticsCaseAttribute.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/statistics/StatisticsCaseAttribute.java
@@ -4,29 +4,35 @@ import de.symeda.sormas.api.I18nProperties;
 
 public enum StatisticsCaseAttribute {
 
-	ONSET_TIME(StatisticsCaseSubAttribute.YEAR,StatisticsCaseSubAttribute.QUARTER,StatisticsCaseSubAttribute.MONTH,StatisticsCaseSubAttribute.EPI_WEEK,
+	ONSET_TIME(false, StatisticsCaseSubAttribute.YEAR,StatisticsCaseSubAttribute.QUARTER,StatisticsCaseSubAttribute.MONTH,StatisticsCaseSubAttribute.EPI_WEEK,
 			StatisticsCaseSubAttribute.QUARTER_OF_YEAR,StatisticsCaseSubAttribute.MONTH_OF_YEAR,StatisticsCaseSubAttribute.EPI_WEEK_OF_YEAR,StatisticsCaseSubAttribute.DATE_RANGE),
-	RECEPTION_TIME(StatisticsCaseSubAttribute.YEAR,StatisticsCaseSubAttribute.QUARTER,StatisticsCaseSubAttribute.MONTH,StatisticsCaseSubAttribute.EPI_WEEK,
+	RECEPTION_TIME(false, StatisticsCaseSubAttribute.YEAR,StatisticsCaseSubAttribute.QUARTER,StatisticsCaseSubAttribute.MONTH,StatisticsCaseSubAttribute.EPI_WEEK,
 			StatisticsCaseSubAttribute.QUARTER_OF_YEAR,StatisticsCaseSubAttribute.MONTH_OF_YEAR,StatisticsCaseSubAttribute.EPI_WEEK_OF_YEAR,StatisticsCaseSubAttribute.DATE_RANGE),
-	REPORT_TIME(StatisticsCaseSubAttribute.YEAR,StatisticsCaseSubAttribute.QUARTER,StatisticsCaseSubAttribute.MONTH,StatisticsCaseSubAttribute.EPI_WEEK,
+	REPORT_TIME(false, StatisticsCaseSubAttribute.YEAR,StatisticsCaseSubAttribute.QUARTER,StatisticsCaseSubAttribute.MONTH,StatisticsCaseSubAttribute.EPI_WEEK,
 			StatisticsCaseSubAttribute.QUARTER_OF_YEAR,StatisticsCaseSubAttribute.MONTH_OF_YEAR,StatisticsCaseSubAttribute.EPI_WEEK_OF_YEAR,StatisticsCaseSubAttribute.DATE_RANGE),
-	REGION_DISTRICT(StatisticsCaseSubAttribute.REGION,StatisticsCaseSubAttribute.DISTRICT),
-	SEX,
-	AGE_INTERVAL_1_YEAR,
-	AGE_INTERVAL_5_YEARS,
-	AGE_INTERVAL_CHILDREN_COARSE,
-	AGE_INTERVAL_CHILDREN_FINE,
-	AGE_INTERVAL_CHILDREN_MEDIUM,
-	DISEASE,
-	CLASSIFICATION,
-	OUTCOME;
+	REGION_DISTRICT(true, StatisticsCaseSubAttribute.REGION,StatisticsCaseSubAttribute.DISTRICT),
+	SEX(true),
+	AGE_INTERVAL_1_YEAR(false),
+	AGE_INTERVAL_5_YEARS(false),
+	AGE_INTERVAL_CHILDREN_COARSE(false),
+	AGE_INTERVAL_CHILDREN_FINE(false),
+	AGE_INTERVAL_CHILDREN_MEDIUM(false),
+	DISEASE(true),
+	CLASSIFICATION(true),
+	OUTCOME(true);
 	
+	private final boolean needsSorting;
 	private final StatisticsCaseSubAttribute[] subAttributes;
 
-	StatisticsCaseAttribute(StatisticsCaseSubAttribute ...subAttributes) {
+	StatisticsCaseAttribute(boolean needsSorting, StatisticsCaseSubAttribute ...subAttributes) {
+		this.needsSorting = needsSorting;
 		this.subAttributes = subAttributes;
 	}
 	
+	public boolean isNeedsSorting() {
+		return needsSorting;
+	}
+
 	public StatisticsCaseSubAttribute[] getSubAttributes() {
 		return subAttributes;
 	}
