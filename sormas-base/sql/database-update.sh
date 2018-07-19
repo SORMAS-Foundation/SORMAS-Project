aec8aa954a7a7a839b66462fc7333b7a1596100c
Martin Wahnschaffe <m.wahnschaffe@symeda.de>
2018-07-19 17:04:54 +0200

    Fixed database update script

--- sormas-base/sql/database-update.sh
+++ sormas-base/sql/database-update.sh
@@ -3,9 +3,8 @@
 # ===========================================================
 
 DB_NAME='sormas_db'
-DB_USER='sormas_user'
 
-DB_VERSION=$(psql -U $DB_USER -h localhost -W -d $DB_NAME -c 'SELECT MAX (version_number) FROM schema_version;' | sed -n 's/[^0-9]//g; 3,3p')
+DB_VERSION=$(sudo -i -u postgres psql -d $DB_NAME -c 'SELECT MAX (version_number) FROM schema_version;' | sed -n 's/[^0-9]//g; 3,3p')
 #echo "Client: DB: $DB_NAME   Schema Version: $CM_DB_VERSION"
 
 DB_SCRIPT_NAME=sormas_schema_update_from_${DB_VERSION}.sql
@@ -15,8 +14,8 @@ sed -n "/^INSERT INTO schema_version.*($DB_VERSION,/,\$p" sormas_schema.sql | se
 read -p "Update database $DB_NAME with $DB_SCRIPT_NAME? " -t 30 -ei 'J' DO_UPDATE
 
 if [ ${DO_UPDATE} = 'J' ]; then
-		psql -U $DB_USER -h localhost -W -d $DB_NAME < ${DB_SCRIPT_NAME}
-		echo done: "psql -U $DB_USER -h localhost -W -d $DB_NAME < ${DB_SCRIPT_NAME}"
+		sudo -i -u postgres psql -d $DB_NAME < ${DB_SCRIPT_NAME}
+		echo done: "sudo -i -u postgres psql -d $DB_NAME < ${DB_SCRIPT_NAME}"
 else
-		echo bypassed: "psql -U $DB_USER -h localhost -W -d $DB_NAME < ${DB_SCRIPT_NAME}"
+		echo bypassed: "sudo -i -u postgres psql -d $DB_NAME < ${DB_SCRIPT_NAME}"
 fi
