ac5218f5756f0f9583595ab164317e8676988d90
Maté Strysewske <m.strysewske@symeda.de>
2018-11-16 15:31:49 +0100

    #881 - added technical manual to about page

--- sormas-ui/src/main/java/de/symeda/sormas/ui/AboutView.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/AboutView.java
@@ -56,11 +56,18 @@ public class AboutView extends VerticalLayout implements View {
 
 		}
 
-		Button dataDictionaryButton = new Button("Data Dictionary (xlsx)");
+		Button dataDictionaryButton = new Button("Data Dictionary (XLSX)");
 		CssStyles.style(dataDictionaryButton, ValoTheme.BUTTON_LINK, CssStyles.BUTTON_NO_PADDING, CssStyles.BUTTON_NO_UPPERCASE);
 		documentsLayout.addComponent(dataDictionaryButton);
 		FileDownloader dataDictionaryDownloader = new FileDownloader(new ClassResource("/doc/SORMAS_Data_Dictionary.xlsx"));
 		dataDictionaryDownloader.extend(dataDictionaryButton);
+		
+		Button technicalManualButton = new Button("Technical Manual (PDF)");
+		CssStyles.style(technicalManualButton, ValoTheme.BUTTON_LINK, CssStyles.BUTTON_NO_PADDING, CssStyles.BUTTON_NO_UPPERCASE);
+		documentsLayout.addComponent(technicalManualButton);
+		technicalManualButton.addClickListener(e -> {
+			getUI().getPage().open("https://github.com/hzi-braunschweig/SORMAS-Project/files/2585973/SORMAS_Technical_Manual_Webversion_20180911.pdf", "_self");
+		});
 
 		setSizeFull();
 		setStyleName("about-view");
