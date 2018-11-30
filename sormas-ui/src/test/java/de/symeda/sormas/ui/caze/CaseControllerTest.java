d5dfce456eb6b9484c9656c29a9136e9e168e519
Martin Wahnschaffe <m.wahnschaffe@symeda.de>
2018-11-30 14:55:47 +0100

    #880 comment out prototype UI unit test

--- sormas-ui/src/test/java/de/symeda/sormas/ui/caze/CaseControllerTest.java
+++ sormas-ui/src/test/java/de/symeda/sormas/ui/caze/CaseControllerTest.java
@@ -5,60 +5,72 @@ import static org.mockito.Mockito.when;
 import java.security.Principal;
 
 import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.Mockito;
 
+import com.vaadin.data.util.converter.ConverterFactory;
+import com.vaadin.server.Page;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinService;
 import com.vaadin.server.VaadinServletRequest;
-import com.vaadin.ui.Field;
+import com.vaadin.server.VaadinSession;
+import com.vaadin.ui.UI;
 import com.vaadin.util.CurrentInstance;
 
-import de.symeda.sormas.api.Disease;
-import de.symeda.sormas.api.caze.CaseDataDto;
 import de.symeda.sormas.api.user.UserRole;
 import de.symeda.sormas.ui.AbstractBeanTest;
-import de.symeda.sormas.ui.utils.CommitDiscardWrapperComponent;
+import de.symeda.sormas.ui.SormasUI;
+import de.symeda.sormas.ui.utils.SormasDefaultConverterFactory;
 
-
-@RunWith(MockitoJUnitRunner.class)
 public class CaseControllerTest extends AbstractBeanTest {
-	
-	@Mock
-	private VaadinServletRequest request; 
-	
+
+	private SormasUI ui;
+
 	@Before
-    public void initUI() throws Exception {
+	public void initUI() throws Exception {
 
-    	creator.createUser(null, null, null, "ad", "min", UserRole.ADMIN, UserRole.NATIONAL_USER);
+		creator.createUser(null, null, null, "ad", "min", UserRole.ADMIN, UserRole.NATIONAL_USER);
 
-        when(request.getUserPrincipal()).thenReturn(new Principal() {
+		VaadinRequest request = Mockito.mock(VaadinServletRequest.class);
+		when(request.getUserPrincipal()).thenReturn(new Principal() {
 			@Override
 			public String getName() {
 				return "admin";
 			}
 		});
 
-        CurrentInstance.setInheritable(VaadinRequest.class, request);
-        
-        // TODO init UI
-    }
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	public void testGetCaseCreateComponent() {
+		CurrentInstance.setInheritable(VaadinRequest.class, request);
 
-		CaseController controller = new CaseController();
-		CommitDiscardWrapperComponent<CaseCreateForm> caseCreateComponent = controller.getCaseCreateComponent(null,
-				null, null, null);
+		VaadinService service = Mockito.mock(VaadinService.class);
+		CurrentInstance.set(VaadinService.class, service);
 
-		// TODO add UI class and attach form, so everything works as expected
-		CaseCreateForm caseCreateForm = caseCreateComponent.getWrappedComponent();
-		((Field<String>) caseCreateForm.getField(CaseCreateForm.FIRST_NAME)).setValue("Steff");
-		((Field<String>) caseCreateForm.getField(CaseCreateForm.LAST_NAME)).setValue("Steffson");
-		((Field<Disease>) caseCreateForm.getField(CaseDataDto.DISEASE)).setValue(Disease.EVD);
-		
-		caseCreateComponent.commit();
+		VaadinSession session = Mockito.mock(VaadinSession.class);
+		ConverterFactory converterFactory = new SormasDefaultConverterFactory();
+		when(session.getConverterFactory()).thenReturn(converterFactory);
+		when(session.getService()).thenReturn(service);
+		CurrentInstance.set(VaadinSession.class, session);
+
+		ui = new SormasUI();
+		CurrentInstance.setInheritable(UI.class, ui);
+
+		java.lang.reflect.Field pageField = UI.class.getDeclaredField("page");
+		pageField.setAccessible(true);
+		pageField.set(ui, Mockito.mock(Page.class));
 	}
+
+//	@SuppressWarnings("unchecked")
+//	@Test
+//	public void testGetCaseCreateComponent() {
+//
+//		CaseController controller = new CaseController();
+//		CommitDiscardWrapperComponent<CaseCreateForm> caseCreateComponent = controller.getCaseCreateComponent(null,
+//				null, null, null);
+//
+//		// TODO add UI class and attach form, so everything works as expected
+//		CaseCreateForm caseCreateForm = caseCreateComponent.getWrappedComponent();
+//		((Field<String>) caseCreateForm.getField(CaseCreateForm.FIRST_NAME)).setValue("Steff");
+//		((Field<String>) caseCreateForm.getField(CaseCreateForm.LAST_NAME)).setValue("Steffson");
+//		((Field<Disease>) caseCreateForm.getField(CaseDataDto.DISEASE)).setValue(Disease.EVD);
+//
+//		caseCreateComponent.commit();
+//	}
 }
