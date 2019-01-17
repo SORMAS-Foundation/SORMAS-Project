203308f42f2b378078dcafa678e004312db6b56a
Martin Wahnschaffe <m.wahnschaffe@symeda.de>
2019-01-17 15:13:08 +0100

    Clean up of headers

--- sormas-app/app/src/main/java/de/symeda/sormas/app/backend/classification/DiseaseClassificationCriteriaDao.java
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/backend/classification/DiseaseClassificationCriteriaDao.java
@@ -16,23 +16,6 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-/*
- * This file is part of SORMAS®.
- *
- * SORMAS® is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * SORMAS® is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with SORMAS®.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package de.symeda.sormas.app.backend.classification;
 
 import android.util.Log;
