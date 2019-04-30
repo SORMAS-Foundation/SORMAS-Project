7996d6e639683cda919f70e1b1ab935201340265
Maté Strysewske <m.strysewske@symeda.de>
2019-04-30 14:42:21 +0200

    #1075 - Added SessionFilter and SessionFilterBean to REST to provide a guaranteed context during PostConstruct

---
+++ sormas-rest/src/main/java/de/symeda/sormas/rest/SessionFilter.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * SORMAS® - Surveillance Outbreak Response Management & Analysis System
+ * Copyright © 2016-2018 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *******************************************************************************/
+package de.symeda.sormas.rest;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(asyncSupported = true, urlPatterns = "/*")
+public class SessionFilter implements Filter {
+
+	//protected static final Logger LOGGER = LoggerFactory.getLogger(SessionFilter.class);
+	
+	@EJB
+	private SessionFilterBean sessionFilterBean;
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		sessionFilterBean.doFilter(chain, request, response);
+	}
+
+	@Override
+	public void init(FilterConfig cfg) throws ServletException {
+	}
+
+}
