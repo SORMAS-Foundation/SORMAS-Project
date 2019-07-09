f7a6677472159b65e13fc1df128d9b54742e2453
Joshua Opata <Jol22@bitbucket.org>
2019-07-09 11:48:47 +0000

    Android App changes * View Pathogen List * Add and Edit Pathogen * Added PAthogen Activities * Modified PahtogenTest CLass to include missing fields * Modified string.xml to include Pathogen strings

---
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/pathogentest/list/PathogenTestListAdapter.java
@@ -0,0 +1,118 @@
+/*
+ * SORMAS® - Surveillance Outbreak Response Management & Analysis System
+ * Copyright © 2016-2018 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package de.symeda.sormas.app.pathogentest.list;
+
+import android.content.res.Resources;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import de.symeda.sormas.api.contact.FollowUpStatus;
+import de.symeda.sormas.api.visit.VisitStatus;
+import de.symeda.sormas.app.R;
+import de.symeda.sormas.app.backend.common.DatabaseHelper;
+import de.symeda.sormas.app.backend.contact.Contact;
+import de.symeda.sormas.app.core.adapter.databinding.BindingPagedListAdapter;
+import de.symeda.sormas.app.core.adapter.databinding.BindingViewHolder;
+import de.symeda.sormas.app.core.enumeration.StatusElaborator;
+import de.symeda.sormas.app.core.enumeration.StatusElaboratorFactory;
+import de.symeda.sormas.app.databinding.RowReadContactListItemLayoutBinding;
+import de.symeda.sormas.app.util.DiseaseConfigurationHelper;
+
+public class PathogenTestListAdapter extends BindingPagedListAdapter<Contact, RowReadContactListItemLayoutBinding> {
+
+    private FollowUpStatus currentListFilter;
+
+    public PathogenTestListAdapter(FollowUpStatus initialListFilter) {
+        super(R.layout.row_read_contact_list_item_layout);
+        this.currentListFilter = initialListFilter;
+    }
+
+    public PathogenTestListAdapter() {
+        super(R.layout.row_read_contact_list_item_layout);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        super.onBindViewHolder(holder, position);
+
+        if (getItemViewType(position) == TYPE_ITEM) {
+            BindingViewHolder<Contact, RowReadContactListItemLayoutBinding> pagedHolder = (BindingViewHolder) holder;
+            Contact item = getItem(position);
+
+            pagedHolder.setOnListItemClickListener(this.mOnListItemClickListener);
+
+            indicateContactClassification(pagedHolder.binding.contactClassificationIcon, item);
+
+            if (item.isModifiedOrChildModified()) {
+                pagedHolder.binding.imgSyncIcon.setVisibility(View.VISIBLE);
+                pagedHolder.binding.imgSyncIcon.setImageResource(R.drawable.ic_sync_blue_24dp);
+            } else {
+                pagedHolder.binding.imgSyncIcon.setVisibility(View.GONE);
+            }
+
+            if (DiseaseConfigurationHelper.getInstance().hasFollowUp(item.getCaseDisease())
+                    && (currentListFilter == null || currentListFilter != FollowUpStatus.NO_FOLLOW_UP)) {
+                int numberOfVisits = DatabaseHelper.getVisitDao().getVisitCount(item, null);
+                int numberOfCooperativeVisits = DatabaseHelper.getVisitDao().getVisitCount(item, VisitStatus.COOPERATIVE);
+
+                pagedHolder.binding.numberOfVisits.setText(String.format(
+                        pagedHolder.binding.getRoot().getResources().getString(R.string.number_of_visits_long_format),
+                        numberOfCooperativeVisits, numberOfVisits - numberOfCooperativeVisits));
+            } else {
+                pagedHolder.binding.numberOfVisits.setVisibility(View.GONE);
+            }
+
+        }
+
+        // TODO #704
+//        updateUnreadIndicator(holder, record);
+    }
+
+//    public void updateUnreadIndicator(DataBoundViewHolder<RowReadContactListItemLayoutBinding> holder, Contact item) {
+//        LayerDrawable backgroundRowItem = (LayerDrawable) ContextCompat.getDrawable(holder.context, R.drawable.background_list_activity_row);
+//        Drawable unreadListItemIndicator = backgroundRowItem.findDrawableByLayerId(R.id.unreadListItemIndicator);
+//
+//        if (item != null) {
+//            if (item.isUnreadOrChildUnread()) {
+//                unreadListItemIndicator.setTint(holder.context.getResources().getColor(R.color.unreadIcon));
+//            } else {
+//                unreadListItemIndicator.setTint(holder.context.getResources().getColor(android.R.color.transparent));
+//            }
+//        }
+//    }
+
+    public void indicateContactClassification(ImageView img, Contact record) {
+        Resources resources = img.getContext().getResources();
+        Drawable drw = ContextCompat.getDrawable(img.getContext(), R.drawable.indicator_status_circle);
+        StatusElaborator elaborator = StatusElaboratorFactory.getElaborator(record.getContactClassification());
+        drw.setColorFilter(resources.getColor(elaborator.getColorIndicatorResource()), PorterDuff.Mode.SRC_OVER);
+        img.setBackground(drw);
+    }
+
+    public void setCurrentListFilter(FollowUpStatus currentListFilter) {
+        this.currentListFilter = currentListFilter;
+    }
+
+}
