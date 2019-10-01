e0ea296baf53c1a6560b7cbe39a1d443277ef467
Christopher Riedel <c.riedel@symeda.de>
2019-10-01 17:07:02 +0200

    #1307: Disable merge and pick button for duplicates in different regions

--- sormas-ui/src/main/java/de/symeda/sormas/ui/caze/MergeCasesGrid.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/caze/MergeCasesGrid.java
@@ -42,10 +42,10 @@ public class MergeCasesGrid extends TreeGrid<CaseIndexDto> {
 	public static final String COLUMN_ACTIONS = "actions";
 	public static final String COLUMN_COMPLETENESS = "completenessValue";
 	public static final String COLUMN_UUID = "uuidLink";
-	
+
 	private CaseCriteria criteria;
 	private boolean ignoreRegion;
-	
+
 	private List<String[]> hiddenUuidPairs;
 
 	@SuppressWarnings("unchecked")
@@ -54,16 +54,18 @@ public class MergeCasesGrid extends TreeGrid<CaseIndexDto> {
 		setSizeFull();
 		setSelectionMode(SelectionMode.NONE);
 
-		Column<CaseIndexDto, String> diseaseColumn = addColumn(caze -> 
-			DiseaseHelper.toString(caze.getDisease(), caze.getDiseaseDetails()));
+		Column<CaseIndexDto, String> diseaseColumn = addColumn(
+				caze -> DiseaseHelper.toString(caze.getDisease(), caze.getDiseaseDetails()));
 		diseaseColumn.setId(COLUMN_DISEASE);
-		
+
 		addComponentColumn(indexDto -> {
 			return buildButtonLayout(indexDto);
 		}).setId(COLUMN_ACTIONS);
-		
+
 		addComponentColumn(indexDto -> {
-			Label label = new Label(indexDto.getCompleteness() != null ? new DecimalFormat("#").format(indexDto.getCompleteness() * 100) + " %" : "-");
+			Label label = new Label(indexDto.getCompleteness() != null
+					? new DecimalFormat("#").format(indexDto.getCompleteness() * 100) + " %"
+					: "-");
 			if (indexDto.getCompleteness() != null) {
 				if (indexDto.getCompleteness() < 0.25f) {
 					CssStyles.style(label, CssStyles.LABEL_CRITICAL);
@@ -77,35 +79,41 @@ public class MergeCasesGrid extends TreeGrid<CaseIndexDto> {
 			}
 			return label;
 		}).setId(COLUMN_COMPLETENESS);
-		
+
 		addComponentColumn(indexDto -> {
-			Link link = new Link(DataHelper.getShortUuid(indexDto.getUuid()), new ExternalResource(SormasUI.get().getPage().getLocation().getRawPath() + "#!" + CaseDataView.VIEW_NAME + "/" + indexDto.getUuid()));
+			Link link = new Link(DataHelper.getShortUuid(indexDto.getUuid()),
+					new ExternalResource(SormasUI.get().getPage().getLocation().getRawPath() + "#!"
+							+ CaseDataView.VIEW_NAME + "/" + indexDto.getUuid()));
 			link.setTargetName("_blank");
 			return link;
 		}).setId(COLUMN_UUID);
-		
-		setColumns(COLUMN_UUID, COLUMN_DISEASE, CaseIndexDto.CASE_CLASSIFICATION, CaseIndexDto.PERSON_FIRST_NAME, CaseIndexDto.PERSON_LAST_NAME, 
-				CaseIndexDto.AGE_AND_BIRTH_DATE, CaseIndexDto.SEX, CaseIndexDto.DISTRICT_NAME, CaseIndexDto.HEALTH_FACILITY_NAME,
-				CaseIndexDto.REPORT_DATE, CaseIndexDto.CREATION_DATE, COLUMN_COMPLETENESS, COLUMN_ACTIONS);
 
-		((Column<CaseIndexDto, Date>) getColumn(CaseIndexDto.REPORT_DATE)).setRenderer(new DateRenderer(DateHelper.getLocalDateTimeFormat()));
-		((Column<CaseIndexDto, Date>) getColumn(CaseIndexDto.CREATION_DATE)).setRenderer(new DateRenderer(DateHelper.getLocalDateTimeFormat()));
+		setColumns(COLUMN_UUID, COLUMN_DISEASE, CaseIndexDto.CASE_CLASSIFICATION, CaseIndexDto.PERSON_FIRST_NAME,
+				CaseIndexDto.PERSON_LAST_NAME, CaseIndexDto.AGE_AND_BIRTH_DATE, CaseIndexDto.SEX,
+				CaseIndexDto.DISTRICT_NAME, CaseIndexDto.HEALTH_FACILITY_NAME, CaseIndexDto.REPORT_DATE,
+				CaseIndexDto.CREATION_DATE, COLUMN_COMPLETENESS, COLUMN_ACTIONS);
+
+		((Column<CaseIndexDto, Date>) getColumn(CaseIndexDto.REPORT_DATE))
+				.setRenderer(new DateRenderer(DateHelper.getLocalDateTimeFormat()));
+		((Column<CaseIndexDto, Date>) getColumn(CaseIndexDto.CREATION_DATE))
+				.setRenderer(new DateRenderer(DateHelper.getLocalDateTimeFormat()));
 
 		for (Column<?, ?> column : getColumns()) {
-			column.setCaption(I18nProperties.getPrefixCaption(
-					CaseIndexDto.I18N_PREFIX, column.getId().toString(), column.getCaption()));
+			column.setCaption(I18nProperties.getPrefixCaption(CaseIndexDto.I18N_PREFIX, column.getId().toString(),
+					column.getCaption()));
 		}
 		getColumn(COLUMN_ACTIONS).setCaption("");
 		getColumn(COLUMN_UUID).setCaption(I18nProperties.getPrefixCaption(CaseIndexDto.I18N_PREFIX, CaseIndexDto.UUID));
-		getColumn(COLUMN_COMPLETENESS).setCaption(I18nProperties.getPrefixCaption(CaseIndexDto.I18N_PREFIX, CaseIndexDto.COMPLETENESS));
+		getColumn(COLUMN_COMPLETENESS)
+				.setCaption(I18nProperties.getPrefixCaption(CaseIndexDto.I18N_PREFIX, CaseIndexDto.COMPLETENESS));
 		getColumn(COLUMN_COMPLETENESS).setSortable(false);
-		
+
 		this.setStyleGenerator(new StyleGenerator<CaseIndexDto>() {
 			@Override
 			public String apply(CaseIndexDto item) {
 				TreeDataProvider<CaseIndexDto> dataProvider = (TreeDataProvider<CaseIndexDto>) getDataProvider();
 				TreeData<CaseIndexDto> data = dataProvider.getTreeData();
-				
+
 				if (data.getRootItems().contains(item)) {
 					return "v-treegrid-parent-row";
 				} else {
@@ -114,7 +122,7 @@ public class MergeCasesGrid extends TreeGrid<CaseIndexDto> {
 			}
 		});
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	private HorizontalLayout buildButtonLayout(CaseIndexDto caze) {
 		HorizontalLayout layout = new HorizontalLayout();
@@ -124,65 +132,65 @@ public class MergeCasesGrid extends TreeGrid<CaseIndexDto> {
 		Button btnPick = new Button(I18nProperties.getCaption(Captions.actionPick));
 		btnPick.setIcon(VaadinIcons.CHECK);
 		Button btnHide = null;
-		
+
 		TreeDataProvider<CaseIndexDto> dataProvider = (TreeDataProvider<CaseIndexDto>) getDataProvider();
 		TreeData<CaseIndexDto> data = dataProvider.getTreeData();
-		
+
 		btnMerge.addClickListener(e -> {
-			VaadinUiUtil.showConfirmationPopup(
-					I18nProperties.getString(Strings.headingConfirmChoice),
+			VaadinUiUtil.showConfirmationPopup(I18nProperties.getString(Strings.headingConfirmChoice),
 					new Label(I18nProperties.getString(Strings.confirmationMergeCaseAndDeleteOther)),
-					I18nProperties.getCaption(Captions.actionConfirm),
-					I18nProperties.getCaption(Captions.actionCancel),
-					640,
-					confirmed -> {
+					I18nProperties.getCaption(Captions.actionConfirm), I18nProperties.getCaption(Captions.actionCancel),
+					640, confirmed -> {
 						if (confirmed.booleanValue()) {
-							CaseIndexDto caseToMergeAndDelete = data.getParent(caze) != null ? data.getParent(caze) : data.getChildren(caze).get(0);
+							CaseIndexDto caseToMergeAndDelete = data.getParent(caze) != null ? data.getParent(caze)
+									: data.getChildren(caze).get(0);
 							FacadeProvider.getCaseFacade().mergeCase(caze.getUuid(), caseToMergeAndDelete.getUuid());
-							FacadeProvider.getCaseFacade().deleteCaseAsDuplicate(caseToMergeAndDelete.getUuid(), caze.getUuid(), UserProvider.getCurrent().getUuid());
-							
+							FacadeProvider.getCaseFacade().deleteCaseAsDuplicate(caseToMergeAndDelete.getUuid(),
+									caze.getUuid(), UserProvider.getCurrent().getUuid());
+
 							if (FacadeProvider.getCaseFacade().isDeleted(caseToMergeAndDelete.getUuid())) {
 								reload();
-								new Notification(I18nProperties.getString(Strings.messageCasesMerged), Type.TRAY_NOTIFICATION).show(Page.getCurrent());
+								new Notification(I18nProperties.getString(Strings.messageCasesMerged),
+										Type.TRAY_NOTIFICATION).show(Page.getCurrent());
 							} else {
-								new Notification(I18nProperties.getString(Strings.errorCaseMerging), Type.ERROR_MESSAGE).show(Page.getCurrent());
+								new Notification(I18nProperties.getString(Strings.errorCaseMerging), Type.ERROR_MESSAGE)
+										.show(Page.getCurrent());
 							}
 						}
-					}
-			);
+					});
 		});
-		
+
 		btnPick.addClickListener(e -> {
-			VaadinUiUtil.showConfirmationPopup(
-					I18nProperties.getString(Strings.headingConfirmChoice),
+			VaadinUiUtil.showConfirmationPopup(I18nProperties.getString(Strings.headingConfirmChoice),
 					new Label(I18nProperties.getString(Strings.confirmationPickCaseAndDeleteOther)),
-					I18nProperties.getCaption(Captions.actionConfirm),
-					I18nProperties.getCaption(Captions.actionCancel),
-					640,
-					confirmed -> {
+					I18nProperties.getCaption(Captions.actionConfirm), I18nProperties.getCaption(Captions.actionCancel),
+					640, confirmed -> {
 						if (confirmed.booleanValue()) {
-							CaseIndexDto caseToDelete = data.getParent(caze) != null ? data.getParent(caze) : data.getChildren(caze).get(0);
-							FacadeProvider.getCaseFacade().deleteCaseAsDuplicate(caseToDelete.getUuid(), caze.getUuid(), UserProvider.getCurrent().getUuid());
-							
+							CaseIndexDto caseToDelete = data.getParent(caze) != null ? data.getParent(caze)
+									: data.getChildren(caze).get(0);
+							FacadeProvider.getCaseFacade().deleteCaseAsDuplicate(caseToDelete.getUuid(), caze.getUuid(),
+									UserProvider.getCurrent().getUuid());
+
 							if (FacadeProvider.getCaseFacade().isDeleted(caseToDelete.getUuid())) {
 								data.removeItem(data.getParent(caze) == null ? caze : data.getParent(caze));
 								dataProvider.refreshAll();
-								new Notification(I18nProperties.getString(Strings.messageCaseDuplicateDeleted), Type.TRAY_NOTIFICATION).show(Page.getCurrent());
+								new Notification(I18nProperties.getString(Strings.messageCaseDuplicateDeleted),
+										Type.TRAY_NOTIFICATION).show(Page.getCurrent());
 							} else {
-								new Notification(I18nProperties.getString(Strings.errorCaseDuplicateDeletion), Type.ERROR_MESSAGE).show(Page.getCurrent());
+								new Notification(I18nProperties.getString(Strings.errorCaseDuplicateDeletion),
+										Type.ERROR_MESSAGE).show(Page.getCurrent());
 							}
 						}
-					}
-			);
+					});
 		});
-		
-		if (data.getParent(caze) == null) {        
+
+		if (data.getParent(caze) == null) {
 			CssStyles.style(btnMerge, CssStyles.HSPACE_RIGHT_5, ValoTheme.BUTTON_PRIMARY);
 			CssStyles.style(btnPick, CssStyles.HSPACE_RIGHT_5, ValoTheme.BUTTON_PRIMARY);
 			btnHide = new Button(I18nProperties.getCaption(Captions.actionHide));
 			btnHide.setIcon(VaadinIcons.CLOSE);
 			btnHide.addClickListener(e -> {
-				hiddenUuidPairs.add(new String[]{caze.getUuid(), data.getChildren(caze).get(0).getUuid()});
+				hiddenUuidPairs.add(new String[] { caze.getUuid(), data.getChildren(caze).get(0).getUuid() });
 				dataProvider.getTreeData().removeItem(caze);
 				dataProvider.refreshAll();
 			});
@@ -190,48 +198,54 @@ public class MergeCasesGrid extends TreeGrid<CaseIndexDto> {
 			CssStyles.style(btnMerge, ValoTheme.BUTTON_LINK);
 			CssStyles.style(btnPick, ValoTheme.BUTTON_LINK);
 		}
-	
+
+		if (ignoreRegion) {
+			btnMerge.setEnabled(false);
+			btnPick.setEnabled(false);
+		}
 		layout.addComponent(btnMerge);
 		layout.addComponent(btnPick);
+		
 		if (btnHide != null) {
 			layout.addComponent(btnHide);
 		}
-		
+
 		return layout;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public void reload() {
 		TreeDataProvider<CaseIndexDto> dataProvider = (TreeDataProvider<CaseIndexDto>) getDataProvider();
 		TreeData<CaseIndexDto> data = dataProvider.getTreeData();
 		data.clear();
-		
+
 		if (hiddenUuidPairs == null) {
 			hiddenUuidPairs = new ArrayList<>();
 		}
-		
-		List<CaseIndexDto[]> casePairs = FacadeProvider.getCaseFacade()
-				.getCasesForDuplicateMerging(criteria, UserProvider.getCurrent().getUuid(), ignoreRegion);
+
+		List<CaseIndexDto[]> casePairs = FacadeProvider.getCaseFacade().getCasesForDuplicateMerging(criteria,
+				UserProvider.getCurrent().getUuid(), ignoreRegion);
 		for (CaseIndexDto[] casePair : casePairs) {
 			boolean uuidPairExists = false;
 			for (String[] hiddenUuidPair : hiddenUuidPairs) {
-				if (hiddenUuidPair[0].equals(casePair[0].getUuid()) && hiddenUuidPair[1].equals(casePair[1].getUuid())) {
+				if (hiddenUuidPair[0].equals(casePair[0].getUuid())
+						&& hiddenUuidPair[1].equals(casePair[1].getUuid())) {
 					uuidPairExists = true;
 				}
 			}
-			
+
 			if (uuidPairExists) {
 				continue;
 			}
-			
+
 			data.addItem(null, casePair[0]);
 			data.addItem(casePair[0], casePair[1]);
 			expand(casePair[0]);
 		}
-		
+
 		dataProvider.refreshAll();
 	}
-	
+
 	public void reload(boolean ignoreRegion) {
 		this.ignoreRegion = ignoreRegion;
 		reload();
@@ -241,15 +255,15 @@ public class MergeCasesGrid extends TreeGrid<CaseIndexDto> {
 	public void calculateCompletenessValues() {
 		TreeDataProvider<CaseIndexDto> dataProvider = (TreeDataProvider<CaseIndexDto>) getDataProvider();
 		TreeData<CaseIndexDto> data = dataProvider.getTreeData();
-		
+
 		for (CaseIndexDto parent : data.getRootItems()) {
 			FacadeProvider.getCaseFacade().updateCompleteness(parent.getUuid());
 			FacadeProvider.getCaseFacade().updateCompleteness(data.getChildren(parent).get(0).getUuid());
 		}
-		
+
 		reload();
 	}
-	
+
 	public void setCriteria(CaseCriteria criteria) {
 		this.criteria = criteria;
 	}
