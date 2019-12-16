a543e5a69db449c5612523cfb21b200289d467f9
MartinWahnschaffeSymeda <m.wahnschaffe@symeda.de>
2019-12-16 12:40:39 +0100

    #1415 renamed

--- sormas-api/src/test/java/de/symeda/sormas/api/user/UserRoleConfigGenerator.java
+++ sormas-api/src/test/java/de/symeda/sormas/api/user/UserRoleConfigGenerator.java
@@ -6,29 +6,32 @@ import de.symeda.sormas.api.utils.DataHelper;
 
 public class UserRoleConfigGenerator {
 
+	/**
+	 * Prints the SQL needed to create the userrolesconfig for a user role with it's default user rights
+	 */
+	@SuppressWarnings("unused")
 	@Test
-	public void generateDefaultUserRoleConfig() {
-		UserRole userRole = UserRole.HOSPITAL_INFORMANT;
-		
-		StringBuilder sqlLineBuilder = new StringBuilder();
-		sqlLineBuilder.append("INSERT INTO userrolesconfig VALUES (nextval('entity_seq'), ")
-			.append("'").append(DataHelper.createUuid()).append("', ")
-			.append("now(), now(), ")
-			.append("'").append(userRole.name()).append("');");
+	public void printDefaultUserRoleConfig() {
+		UserRole userRole = null;// UserRole.HOSPITAL_INFORMANT;
 
-		System.out.println(sqlLineBuilder.toString());
+		if (userRole != null) {
+			StringBuilder sqlLineBuilder = new StringBuilder();
+			sqlLineBuilder.append("INSERT INTO userrolesconfig VALUES (nextval('entity_seq'), ").append("'")
+					.append(DataHelper.createUuid()).append("', ").append("now(), now(), ").append("'")
+					.append(userRole.name()).append("');");
 
-		for (UserRight userRight : userRole.getDefaultUserRights()) {
-			sqlLineBuilder = new StringBuilder();
-			sqlLineBuilder.append("INSERT INTO userroles_userrights (userright, userrole_id) VALUES (")
-			.append("'").append(userRight.name()).append("', ")
-			.append("(SELECT id FROM userrolesconfig WHERE userrole = '").append(userRole.name()).append("'));");
-			
 			System.out.println(sqlLineBuilder.toString());
+
+			for (UserRight userRight : userRole.getDefaultUserRights()) {
+				sqlLineBuilder = new StringBuilder();
+				sqlLineBuilder.append("INSERT INTO userroles_userrights (userright, userrole_id) VALUES (").append("'")
+						.append(userRight.name()).append("', ")
+						.append("(SELECT id FROM userrolesconfig WHERE userrole = '").append(userRole.name())
+						.append("'));");
+
+				System.out.println(sqlLineBuilder.toString());
+			}
 		}
-		
-		
-		
 	}
-	
+
 }
