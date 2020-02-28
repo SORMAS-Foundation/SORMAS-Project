e347dc6835c2bf8748915b50739ec73eee83d2c7
Holger Reise <holger.reise@vitasystems.de>
2020-02-28 11:02:44 +0100

    #1168 Initial release vis Network Vaadin JS Component

---
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/dashboard/visualisation/NetworkDiagramState.java
@@ -0,0 +1,7 @@
+package de.symeda.sormas.ui.dashboard.visualisation;
+import com.vaadin.shared.ui.JavaScriptComponentState;
+
+public class NetworkDiagramState extends JavaScriptComponentState {
+	private static final long serialVersionUID = 1L;
+	public String jsonData;
+}
