a8d59fadc86c9284e60d9e67267c8a75503e80f7
Daniel Schulze <legolas38@arcor.de>
2020-04-02 12:07:35 +0200

    helper methods to get table content

--- Keywords/com/hzi/Table.groovy
+++ Keywords/com/hzi/Table.groovy
@@ -35,7 +35,7 @@ class Table {
 
 		return attribute.toInteger() - 1
 	}
-	
+
 	/**
 	 * Get number of rows by attribute "aria-rowcount" for a static testObject found by the same attribute. Is a convenience method.
 	 * @return
@@ -45,4 +45,56 @@ class Table {
 		TestObject testObject = Helper.createTestObjectWithXPath('//table[@aria-rowcount]')
 		return table.getTableRowsByAttribute(testObject)
 	}
+	
+	static TableContent getVisibleTableContent() {
+		TestObject tableObject = Helper.createTestObjectWithXPath('//table')
+		
+		WebElement tableElement = WebUiBuiltInKeywords.findWebElement(tableObject)
+		
+		// WebElement headElement = tableElement.findElements(By.tagName("thead"))
+		WebElement bodyElement = tableElement.findElement(By.tagName("tbody"))
+		
+		List<String> headers = Table.getTableColumnNames()
+		
+		List<WebElement> rowElements = bodyElement.findElements(By.tagName("tr"))
+		println('rows:' + rowElements.size())
+		
+		TableContent tc = new TableContent()
+		tc.setTableHeaders(headers)
+		for (int row = 0; row < rowElements.size(); row++) {
+			List<String> tableData = new ArrayList<String>()
+			List<WebElement> columnElements = rowElements.get(row).findElements(By.tagName('td'))
+			 
+			println('columns:' + columnElements.size())
+			 
+			for (int column = 0; column < columnElements.size(); column++) {
+				String celltext = columnElements.get(column).getText()
+				tableData.add(celltext)
+			}
+			tc.addRowData(tableData)
+		}
+		
+		return tc
+	}
+		
+	static String[] getTableColumnNames() {
+		TestObject tableObject = Helper.createTestObjectWithXPath('//table')
+		
+		WebElement tableElement = WebUiBuiltInKeywords.findWebElement(tableObject)
+		WebElement headElement = tableElement.findElement(By.tagName("thead"))
+		
+		List<WebElement> rowElements = headElement.findElements(By.tagName("tr"))
+		
+		List<String> columnNames = []
+		for (int row = 0; row < rowElements.size(); row++) {
+			List<WebElement> headerElements = rowElements.get(row).findElements(By.tagName('th'))
+			 
+			println('columns:' + headerElements.size())
+			for (int column = 0; column < headerElements.size(); column++) {
+				String headerText = headerElements.get(column).getText()
+				columnNames.add(headerText)
+			}
+		}
+		return columnNames
+	}	
 }
---
+++ Keywords/com/hzi/TableContent.groovy
@@ -0,0 +1,30 @@
+package com.hzi
+
+public class TableContent {
+	
+	List<String> tableHeaders
+	List<List<String>> tableRows
+		
+	def void addRowData(List<String> rowData) {
+		getTableRows().add(rowData)
+	}
+	
+	def String getRowData(int rowIndex, int columnIndex) {
+		return tableRows.get(rowIndex).get(columnIndex)
+	}
+	
+	def List<String> getTableHeaders() {
+		return tableHeaders;
+	}
+	
+	def void setTableHeaders(List<String> tableHeaders) {
+		this.tableHeaders = tableHeaders;
+	}
+	
+	def List<List<String>> getTableRows(){
+		if (tableRows == null) {
+			tableRows = new ArrayList<List<String>>()
+		}
+		return tableRows
+	}
+}
