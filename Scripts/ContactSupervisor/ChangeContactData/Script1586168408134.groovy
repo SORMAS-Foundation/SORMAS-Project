a7fa1d18a67c26e4997e3b3f8ce70e0d0f6718a9
Daniel Schulze <legolas38@arcor.de>
2020-04-06 13:49:14 +0200

    adjusted contact-change-data test - with check of quarantine date fields deleted unnecessary objects

--- Scripts/ContactSupervisor/ChangeContactData/Script1586168408134.groovy
+++ Scripts/ContactSupervisor/ChangeContactData/Script1586168408134.groovy
@@ -1,8 +1,12 @@
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
+import org.openqa.selenium.Dimension
+import org.openqa.selenium.WebDriver
+
 import com.kms.katalon.core.exception.StepFailedException
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.webui.driver.DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 // PREPARE
@@ -12,6 +16,11 @@ WebUI.callTestCase(findTestCase('ContactSupervisor/partials/switchToContacts'), 
 
 WebUI.callTestCase(findTestCase('ContactSupervisor/partials/searchAndSelectContact'), [('key') : 'userName-B'], FailureHandling.STOP_ON_FAILURE)
 
+String quarantineStart = '02/02/2020'
+String quarantineEnd = '03/05/2020'
+
+WebDriver driver = DriverFactory.getWebDriver()
+driver.manage().window().setSize(new Dimension(2500, 2500))
 
 // TESTCASE
 WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/div_Responsible region_v-filterselect-button'))
@@ -28,25 +37,19 @@ WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/sp
 
 WebUI.setText(findTestObject('Contacts/ContactInformationView/ChangeContactData/textarea_Description of how contact took pl_3ad3bd'), 
     'this is a test')
+WebUI.delay(0.5)
 
 WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/div_Quarantine_v-filterselect-button'))
 
 WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/span_Home'))
 
-WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/button_Quarantine start_v-datefield-button'))
+WebUI.setText(findTestObject('Contacts/ContactInformationView/ChangeContactData/input_Quarantine start_gwt-uid-228'), quarantineStart)
 
-WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/button_'))
-
-WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/span_27'))
-
-WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/button_Quarantine start_v-datefield-button'))
-
-WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/button__1'))
-
-WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/span_1'))
+WebUI.setText(findTestObject('Contacts/ContactInformationView/ChangeContactData/input_Quarantine end_gwt-uid-226'), quarantineEnd)
 
 WebUI.setText(findTestObject('Contacts/ContactInformationView/ChangeContactData/textarea_Follow-up status comment_followUpComment'), 
     'this is a test')
+WebUI.delay(0.5)
 
 WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/div_Responsible contact officer_v-filtersel_edee33'))
 
@@ -56,6 +59,7 @@ WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/di
 
 // CHECK
 WebUI.refresh()
+driver.manage().window().setSize(new Dimension(2500, 2500))
 WebUI.delay(1)
 
 String region = WebUI.getAttribute(findTestObject('Contacts/ContactInformationView/ChangeContactData/input_Responsible region_gwt-uid-207'), 'value')
@@ -80,6 +84,12 @@ if (!quarantine.contains("Home")) {
 	throw new StepFailedException('The qurantine should contain "Home". quarantine: ' + quarantine)
 }
 
+String startDate = WebUI.getAttribute(findTestObject('Contacts/ContactInformationView/ChangeContactData/input_Quarantine start_gwt-uid-228'), 'value')
+String endDate = WebUI.getAttribute(findTestObject('Contacts/ContactInformationView/ChangeContactData/input_Quarantine end_gwt-uid-226'), 'value')
+if (!startDate.contains(quarantineStart) || !endDate.contains(quarantineEnd)) {
+	throw new StepFailedException('The qurantine start- and/or end-Date are wrong". startDate: ' + startDate + ' endDate:' + endDate)
+}
+
 String comment = WebUI.getAttribute(findTestObject('Contacts/ContactInformationView/ChangeContactData/textarea_Follow-up status comment_followUpComment'), 'value')
 if (!comment.contains("this is a test")) {
 	throw new StepFailedException('The comment should contain "this is a test". comment: ' + comment)
@@ -91,7 +101,6 @@ if (!contact.contains("Berlin CONTACT - Surveillance Officer, Contact Officer"))
 }
 
 // CLEANUP
-
 WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/div_Responsible region_v-filterselect-button'))
 
 WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/td_'))
@@ -116,25 +125,4 @@ WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/td
 
 WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/div_Save'))
 
-
-
-//WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/input_Responsible region_gwt-uid-207'))
-
-//WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/input_Responsible district_gwt-uid-203'))
-
-//WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/input_Relationship with case_gwt-uid-195'))
-
-//WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/input_Quarantine_gwt-uid-205'))
-
-//WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/div_Quarantine_v-filterselect-button'))
-
-//WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/td_Home'))
-
-//WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/input_Quarantine start_gwt-uid-228'))
-
-//WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/input_Quarantine end_gwt-uid-226'))
-
-//WebUI.click(findTestObject('Contacts/ContactInformationView/ChangeContactData/input_Responsible contact officer_gwt-uid-199'))
-
-//WebUI.closeBrowser()
 
