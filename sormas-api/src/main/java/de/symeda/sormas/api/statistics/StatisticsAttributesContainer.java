f28e9f85759fa5dfd8a3c178fa18a92c30dab8c7
Fahtihi Ghazzali <faatihi@yahoo.com>
2020-04-08 10:32:46 +0000

    use a container to be used to get an attribute given its enum constant

---
+++ sormas-api/src/main/java/de/symeda/sormas/api/statistics/StatisticsAttributesContainer.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * SORMAS® - Surveillance Outbreak Response Management & Analysis System
+ * Copyright © 2016-2018 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *******************************************************************************/
+package de.symeda.sormas.api.statistics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.symeda.sormas.api.i18n.I18nProperties;
+
+public class StatisticsAttributesContainer {
+	
+	private final List<StatisticsAttributeGroup> groups;
+	
+	public StatisticsAttributesContainer (List<StatisticsAttributeGroup> groups) {
+		this.groups = groups;
+	}
+	
+	public List<StatisticsAttributeGroup> values () {
+		return groups;
+	}
+	
+	public StatisticsAttribute get (StatisticsAttributeEnum _enum) {
+		return groups.stream()
+					 .flatMap(n -> n.getAttributes().stream())
+					 .filter(n -> n.getBaseEnum() == _enum)
+					 .findFirst().orElse(null);
+	}
+	
+	public StatisticsSubAttribute get (StatisticsSubAttributeEnum _enum) {
+		return groups.stream()
+					 .flatMap(n -> n.getAttributes().stream())
+					 .flatMap(n -> n.getSubAttributes().stream())
+					 .filter(n -> n.getBaseEnum() == _enum)
+					 .findFirst().orElse(null);
+	}
+}
