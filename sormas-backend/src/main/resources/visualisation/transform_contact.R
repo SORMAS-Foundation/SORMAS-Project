a3191bf2b189ac7ec32b7fa55e8368409bf6b802
Holger Reise <holger.reise@vitasystems.de>
2020-04-14 18:35:09 +0200

    #1563 Revised disease network diagram
    - Computation of nodes and edges in database
    - Enums with prefixes for i18n
    - Short case uuid as node label
    - Option for hierarchical layout
    - Tweaked physics
    - Refactoring

--- sormas-backend/src/main/resources/visualisation/transform_contact.R
+++ sormas-backend/src/main/resources/visualisation/transform_contact.R
@@ -6,14 +6,13 @@ envDefaults = c(
 	"DB_USER" = "sormas_user", "DB_PASS" = "sormas_db",
 	"DB_HOST" = "127.0.0.1", "DB_PORT" = "5432", "DB_NAME" = "sormas_db", 
 	"CONTACT_IDS" = "",
-	"OUTFILE" = "sormas_contact.html"
+	"OUTFILE" = "sormas_contact.html",
+	"HIERARCHICAL" = "FALSE"
 )
 sysEnv = Sys.getenv(names(envDefaults), NA);
 sysEnv = subset(sysEnv, sysEnv == sysEnv)
 
-
 env = c(sysEnv, envDefaults)[names(envDefaults)]
-	
 
 DB_USER = env["DB_USER"]
 DB_PASS = env["DB_PASS"]
@@ -23,7 +22,22 @@ DB_PORT = env["DB_PORT"]
 
 OUTFILE = env["OUTFILE"]
 CONTACT_IDS = env["CONTACT_IDS"]
+HIERARCHICAL = env["HIERARCHICAL"]
 
+caseClass = paste("Classification", c(
+	"HEALTHY",
+	"NOT_CLASSIFIED",
+	"SUSPECT",
+	"PROBABLE",
+	"CONFIRMED"), sep=("."))
+caseClassColor = c(
+	"#17bd27", 
+	"#706c67", 
+	"#a88732", 
+	"#db890f", 
+	"#f70707")
+
+hierarchical= "T" == substr(HIERARCHICAL, 1, 1)
 defaultFont="font-family:'Open Sans', sans-serif, 'Source Sans Pro'"
 mainStyle = paste(defaultFont, "color: #6591C4", ";font-weight: 600", "font-size: 1.6em", "text-align:center;", sep="; ")
 submainStyle = paste(defaultFont, "text-align:center;", sep="; ")
@@ -39,123 +53,136 @@ if (CONTACT_IDS == "") {
 } else {
 	idContString = CONTACT_IDS
 }
-
-sql_fmt_idCont = "select id, contactproximity, caze_id, person_id, contactstatus,resultingcase_id
-                         from public.contact
-                         where id in (%s) and deleted = FALSE and contactclassification != 'NO_CONTACT' and contactstatus != 'DROPPED' "
-contactTable = dbGetQuery( con, sprintf(sql_fmt_idCont, idContString) )
-
-#query case table selecting only cases thet have id in the contact set
-idCase = unique(as.character(c(contactTable$caze_id, na.omit(contactTable$resultingcase_id)))) # unique id of cases that belong to the set of contacts and also contact that became ceses but have not contact
-idCaseString = toString(idCase)
 #guards against syntax exception due to empty id list
-if (idCaseString == "") idCaseString = "NULL"
-sql_fmt_idCase = "select  id, caseclassification, person_id from public.cases where id in (%s) and caseclassification != 'NO_CASE' and deleted = FALSE"
-caseTable = dbGetQuery( con, sprintf(sql_fmt_idCase, idCaseString) )
+if (idContString == "") idContString = "NULL"
 
-## querying person table and keep only persons id that belong to either case or contact table
-idPersonCase = unique(as.character(c(caseTable$person_id, contactTable$person_id))) # uniqur persons in either case or contact table
-idPersonCaseString = toString(idPersonCase)
-#guards against syntax exception due to empty id list
-if (idPersonCaseString == "") idPersonCaseString = "NULL"
-sql_fmt_idPerson = "select  id from public.person where id in (%s)"
-personTable = dbGetQuery(con, sprintf(sql_fmt_idPerson, idPersonCaseString) )
+#query all relevant contacts
+sql_edge = "select distinct cs.person_id case_pid, ct.person_id contact_pid,
+	'ContactProximity.' || ct.contactproximity as contactproximity, 'ContactStatus.' || ct.contactstatus as contactstatus
+from public.contact ct 
+	join public.cases cs on ct.caze_id = cs.id
+where ct.id in (%s)
+	and ct.deleted = FALSE 
+	and ct.contactclassification != 'NO_CONTACT' 
+	and ct.contactstatus != 'DROPPED'
+	and cs.caseclassification != 'NO_CASE' 
+	and cs.deleted = FALSE"
+
+edgeTable = dbGetQuery( con, sprintf(sql_edge, idContString) )
+
+#query all relevant persons with oldest relevant case
+sql_node = "with clean_ct as (
+	select *
+	from public.contact
+	where id in (%s)
+		and deleted = FALSE
+),
+clean_cs as (
+	select *
+	from public.cases
+	where deleted = FALSE 
+		and caseclassification != 'NO_CASE'
+),
+node as (
+	--contact / resulting case
+	select p.id as person_id, rcs.reportdate, rcs.uuid, rcs.caseclassification
+	from public.person p
+		join clean_ct ct on ct.person_id = p.id
+		left join clean_cs rcs on ct.resultingcase_id = rcs.id 
+	WHERE ct.resultingcase_id is not null 
+		or (contactclassification != 'NO_CONTACT' 
+			and contactstatus != 'DROPPED'
+		)
+union
+	--caze
+	select distinct p.id, cs.reportdate, cs.uuid, cs.caseclassification
+	from clean_ct ct 
+		join clean_cs cs on ct.caze_id = cs.id
+		join public.person p on cs.person_id = p.id
+	WHERE contactclassification != 'NO_CONTACT' 
+		and contactstatus != 'DROPPED'
+)
+--XXX take data from earliest case
+select distinct on (person_id)
+	person_id, uuid,  upper(left(uuid, 6)) as short_uuid, 'Classification.' || caseclassification as caseclassification
+from node
+order by person_id, uuid is null, reportdate, uuid"
+
+nodeTable = dbGetQuery( con, sprintf(sql_node, idContString) )
 
 dbDisconnect(con)
 
+elist = data.frame(from=edgeTable$case_pid, to=edgeTable$contact_pid, contactproximity=edgeTable$contactproximity)
 
-case_id = as.character(caseTable$id)
-case_idCont = as.character(contactTable$caze_id)
 
-#adding person id of cases in contact table
-pIdCase = rep(NA, nrow(contactTable))
-personIdCase = caseTable$person_id
-
-for(i in 1:length(case_idCont))
-{
-  temp = case_idCont[i]
-  for(j in 1:length(case_id))
-  {
-    if(temp == case_id[j] )
-    {
-      pIdCase[i] = personIdCase[j]
-    }
-    
-  }
-  
-}
-elist = data.frame(pIdCase, contactTable$person_id, contactTable$contactproximity)
-colnames(elist) = c("from", "to", "contactproximity")
-
-#status of each node or person in line list
-Classification = rep("HEALTHY",nrow(personTable))
-personId = personTable$id
-casPersonId = caseTable$person_id
-personClass = caseTable$caseclassification
-
-for( i in 1:length(Classification))
-{
-  for (j in 1:nrow(caseTable))
-  {
-    if(personId[i] == casPersonId[j])
-    {
-      Classification[i] = personClass[j]
-    }
-  }
-}
-
-personTable = data.frame(personTable, Classification)
+nlist = data.frame(id=nodeTable$person_id, uuid=nodeTable$uuid, label=nodeTable$short_uuid, Classification=factor(nodeTable$caseclassification, levels=caseClass), stringsAsFactors=FALSE)
+# "healthy"
+nlist$Classification[is.na(nlist$Classification)] <- caseClass[1]
 
 ## defining contact categories based on proximity
+highRiskProximity = paste("ContactProximity", c(
+		"FACE_TO_FACE_LONG",
+		"TOUCHED_FLUID",
+		"MEDICAL_UNSAVE",
+		"CLOTHES_OR_OTHER",
+		"PHYSICAL_CONTACT"), sep=("."))
 elist$label = NA
-elist$label[elist$contactproximity %in% c("FACE_TO_FACE_LONG","TOUCHED_FLUID","MEDICAL_UNSAVE","CLOTHES_OR_OTHER","PHYSICAL_CONTACT" )] = 1 
-elist$label[!(elist$contactproximity %in% c("FACE_TO_FACE_LONG","TOUCHED_FLUID","MEDICAL_UNSAVE","CLOTHES_OR_OTHER","PHYSICAL_CONTACT" ))] = 2
+elist$label[elist$contactproximity %in% highRiskProximity] = 1 
+elist$label[!(elist$contactproximity %in% highRiskProximity)] = 2
 
 ## defining plotting parameters  
 
-nodesS = personTable
+nodesS = nlist
 edgesS = elist
 
-#deleting duplicate edges
-edgesS = distinct(edgesS, from, to, .keep_all = T)
 # deleting edges linking a node to itselt
 edgesS = edgesS[edgesS$from != edgesS$to,]
 
 # defining edge attributes
-#edgesS$label = edgesS$label
-edgesS$smooth = TRUE
 edgesS$dashes = TRUE
 edgesS$dashes[edgesS$label == 1] = FALSE #  using broken lines for high risk contacts
-edgesS$arrows = "to"
 
 # defining node attributes
 nodesS$group = nodesS$Classification
-nodesS$label = nodesS$id
-nodesS$value = 1
-nodesS$shape = c("icon")
-nodesS$code = c("f007")
+#nodesS$shape = c("icon")
+#nodesS$code = c("f007")
 #nodesS$shadow = F, 
 
 # defining legend
-addNodesS <- data.frame(label = c("Legend", "Healthy","Not_classified" ,"Suspected", "Probable", "Confirmed", "1 = High risk", "2 = Low risk"), shape = "icon",
-                        icon.code = c("f0c0","f007", "f007", "f007", "f007", "f007", "f178", "f178"),
-                        icon.size = c(0.1,25, 25, 25, 25, 25,25,25), icon.color = c("#0d0c0c","#17bd27", "#706c67", "#a88732", "#db890f", "#f70707", "#0d0c0c", "#0d0c0c"))
+addNodesS <- data.frame(
+		label = c("Legend", caseClass, "1 = High risk", "2 = Low risk"), 
+		shape = "icon",
+    	icon.code = c("f0c0", rep("f007", times = length(caseClass)), "f178", "f178"),
+		icon.size = c(0.1, rep(25, times = length(caseClass)), 25, 25), 
+		icon.color = c("#0d0c0c", caseClassColor, "#0d0c0c", "#0d0c0c"))
+				
 #plotting
-g= visNetwork(nodesS, edgesS,  main = list(text = "Disease network diagram", style = mainStyle),
+avertarIcon <- function(color, code = c( "f007")) {
+	list( face ='FontAwesome', code = code, color=color)
+}
+g = visNetwork(nodesS, edgesS,  main = list(text = "Disease network diagram", style = mainStyle),
               submain = list(text = "The arrows indicate the direction of transmission", style = submainStyle), 
               footer = list(text = "Double click on the icon to open the associated case or contact data", style = footerStyle), 
               background = "white", annot = T, width = "100%" ) %>%
-  visEdges(arrows = "to", color = "black") %>% 
-  visOptions(selectedBy = "Classification",highlightNearest = TRUE,nodesIdSelection = TRUE) %>% 
-  visGroups(groupname = "SUSPECT", size = 10, shape = "icon", icon = list( face ='FontAwesome', code = c( "f007"), color="#a88732")) %>%
-  visGroups(groupname = "PROBABLE", size = 10, shape = "icon", icon = list( face ='FontAwesome', code = c( "f007"), color="#db890f")) %>%
-  visGroups(groupname = "CONFIRMED", size = 10, shape = "icon", icon = list( face ='FontAwesome', code = c( "f007"), color="#f70707")) %>%
-  visGroups(groupname = "NOT_CLASSIFIED", size = 10, shape = "icon", icon = list( face ='FontAwesome', code = c( "f007"), color="#706c67" )) %>%
-  visGroups(groupname = "HEALTHY", size = 10, shape = "icon", icon = list( face ='FontAwesome', code = c( "f007"), color="#17bd27")) %>%
+  visEdges(arrows = "to", color = "black", smooth = TRUE) %>% 
+  visOptions(selectedBy = "Classification", highlightNearest = TRUE, nodesIdSelection = FALSE)
+for (i in 1:length(caseClass)) {
+	  g = visGroups(graph = g, groupname = caseClass[i], size = 10, shape = "icon", icon = avertarIcon(caseClassColor[i]))
+}
+g = g %>% 
   addFontAwesome() %>%
-  visLegend(addNodes = addNodesS, useGroups = F, position = "left", width = 0.2, ncol = 1, stepX = 100, stepY = 50) %>%  
-  visPhysics(stabilization = F) %>%
-  visInteraction(dragNodes = T, dragView = T, zoomView = T)
+  visLegend(addNodes = addNodesS, useGroups = F, position = "left", width = 0.2, ncol = 1, stepX = 100, stepY = 50)
+  
+  if (hierarchical) {
+    g = g %>% 
+    visHierarchicalLayout() %>%
+    visPhysics(hierarchicalRepulsion = list(damping=0.26))
+  } else {
+    g = g %>% 
+  	visPhysics(solver = "barnesHut", barnesHut = list(damping=0.26))
+  }	
 
+  g = g %>% 
+  visInteraction(dragNodes = T, dragView = T, zoomView = T)
 
 visNetwork::visSave(g, OUTFILE, selfcontained = FALSE)
