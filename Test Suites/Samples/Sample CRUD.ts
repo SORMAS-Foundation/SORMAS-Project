a44092639cadad74f023b7823347c0466b1e3f54
Daniel Schulze <legolas38@arcor.de>
2020-04-15 14:00:04 +0200

    removed createNewSample from old TestSuite

--- Test Suites/Samples/Sample CRUD.ts
+++ Test Suites/Samples/Sample CRUD.ts
@@ -10,4 +10,28 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>d05d7a14-8808-42f5-b242-100c50b01137</testSuiteGuid>
+   <testCaseLink>
+      <guid>fdaeb7b8-1e3e-40c5-a6f0-4deb76893c70</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Samples/CreateNewSampleInCaseView</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>f6da25fe-6938-44f2-b3a4-243fe6e92324</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>870f7a56-a1ca-4d0d-873c-a2a48381346f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Samples/CreateNewTestResult</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>66018d1d-337d-4d77-aa59-70005db020cd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Samples/CancelNewTestResult</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
