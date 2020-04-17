b0f00feb8494241e7bd7e1f6c151360e4bb456b0
Daniel Schulze <legolas38@arcor.de>
2020-04-17 11:09:20 +0200

    test test suite for jenkins

---
+++ Test Suites/JenkinsSuite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>testsuite for jenkins test</description>
+   <name>JenkinsSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0f066089-9d51-49c7-b8aa-6c2c03776030</testSuiteGuid>
+</TestSuiteEntity>
