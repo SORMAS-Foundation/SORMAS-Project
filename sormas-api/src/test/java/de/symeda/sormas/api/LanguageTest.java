397293914db099462ba22838845951e96e3de67a
Holger Reise <holger.reise@vitasystems.de>
2020-05-04 23:03:56 +0200

    #1957 unit tests for LanguageTest::fromLocaleString

---
+++ sormas-api/src/test/java/de/symeda/sormas/api/LanguageTest.java
@@ -0,0 +1,44 @@
+package de.symeda.sormas.api;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+public class LanguageTest {
+
+	@Test
+	public void testFromLocaleString() throws Exception {
+		assertThat(Language.fromLocaleString("en"), is(Language.EN));
+		assertThat(Language.fromLocaleString("EN"), is(Language.EN));
+		
+		assertThat(Language.fromLocaleString("en-NG"), is(Language.EN_NG));
+		assertThat(Language.fromLocaleString("EN_ng"), is(Language.EN_NG));
+		
+		assertThat(Language.fromLocaleString("en-GH"), is(Language.EN_GH));
+		assertThat(Language.fromLocaleString("EN_gh"), is(Language.EN_GH));
+
+		assertThat(Language.fromLocaleString("de-DE"), is(Language.DE));
+		assertThat(Language.fromLocaleString("DE_de"), is(Language.DE));
+		
+		assertThat(Language.fromLocaleString("es-EC"), is(Language.ES_EC));
+
+		assertThat(Language.fromLocaleString("fi-FI"), is(Language.FI));
+	
+		// at the moment only defined fallbacks are supported
+
+		assertThat(Language.fromLocaleString("de"), is(Language.DE));
+		assertThat(Language.fromLocaleString("DE"), is(Language.DE));
+		assertThat(Language.fromLocaleString("fi"), is(Language.FI));
+		assertThat(Language.fromLocaleString("es"), is(Language.ES_EC));
+		
+		//default fallback is en
+		assertThat(Language.fromLocaleString(""), is(Language.EN));
+		assertThat(Language.fromLocaleString("tlh"), is(Language.EN));
+		assertThat(Language.fromLocaleString("en-GB"), is(Language.EN));
+		assertThat(Language.fromLocaleString("de-CH"), is(Language.EN));
+
+
+	}
+
+}
