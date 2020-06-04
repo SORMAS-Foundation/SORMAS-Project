3b71e510c1ec2d97fccdd50474d0b6ff77d6b245
Christopher Riedel <c.riedel@symeda.de>
2020-06-04 18:19:34 +0200

    #1950: fix bug in R script

--- sormas-backend/src/main/resources/visualisation/transform_contact.R
+++ sormas-backend/src/main/resources/visualisation/transform_contact.R
@@ -51,7 +51,7 @@ con = dbConnect(PostgreSQL(), user=DB_USER, dbname=DB_NAME, password = DB_PASS, 
 #query contact table and ratin only contacts parsed from Sys.getenv
 if (CONTACT_IDS == "") {
 	#for testing: get all valid contacts
-	idCont = as.character(dbGetQuery(con, "select ct.id 
+	idContString = as.character(dbGetQuery(con, "select ct.id
 from public.contact ct
 	join public.cases cs on (ct.caze_id = cs.id)
 where ct.deleted = FALSE and ct.contactclassification != 'NO_CONTACT'
