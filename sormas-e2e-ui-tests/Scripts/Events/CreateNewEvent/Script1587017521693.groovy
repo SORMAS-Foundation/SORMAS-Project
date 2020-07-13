55ed2e7575f94d44cee874cbabf796470f35dbfd
raghupola-vg <raghavender.pola@vitasystems.de>
2020-07-13 17:59:05 +0200

    #2469 - Changed input date fields and added other required elements for create events.

--- sormas-e2e-ui-tests/Scripts/Events/CreateNewEvent/Script1587017521693.groovy
+++ sormas-e2e-ui-tests/Scripts/Events/CreateNewEvent/Script1587017521693.groovy
@@ -1,20 +1,10 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
+
 import org.openqa.selenium.Keys as Keys
+
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 WebUI.callTestCase(findTestCase('Login/partials/LoginAsSurveillanceSupervisor'), [:], FailureHandling.STOP_ON_FAILURE)
 
@@ -31,6 +21,13 @@ WebUI.click(findTestObject('Events/NewEventView/div_Disease_v-filterselect-butto
 WebUI.click(findTestObject('Events/NewEventView/td_Disease_COVID-19'))
 
 WebUI.setText(findTestObject('Events/NewEventView/textarea_EventDescription_TArea'), 'Sensless Event')
+
+//WebUI.click(findTestObject('Events/NewEventView/div_Source type_v-filterselect-button'))
+
+//WebUI.click(findTestObject('Events/NewEventView/td_HotlinePerson'))
+//Sending text instead of selecting from drop down due to issue #2447
+WebUI.setText(findTestObject('Events/NewEventView/SourceType_Input'), 'Hotline/Person')
+WebUI.sendKeys(findTestObject('Events/NewEventView/SourceType_Input'), Keys.chord(Keys.ENTER))
 
 WebUI.setText(findTestObject('Events/NewEventView/input_SourceFirstName_inputBox'), 'Karl')
 
