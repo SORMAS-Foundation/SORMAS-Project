64cbf789bca509cae4e11a0f1f8f3a170abbf0e4
Holger Reise <holger.reise@vitagroup.ag>
2020-07-29 16:04:58 +0200

    #2370 cleanup
    - fixed some warnings concerning Generics etc.
    - keep SormasBeanFieldGroup as implementation detail
    - cleanup of SormasBeanFieldGroup

--- sormas-ui/src/main/java/de/symeda/sormas/ui/utils/AbstractForm.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/utils/AbstractForm.java
@@ -21,22 +21,21 @@ import de.symeda.sormas.api.FacadeProvider;
 
 public abstract class AbstractForm<T> extends CustomField<T> {
 
-	private static final Logger logger = LoggerFactory.getLogger(AbstractForm.class);
+	private static final long serialVersionUID = -3816438104870246080L;
 
-	private static final long serialVersionUID = -4362630675613167165L;
+	private static final Logger logger = LoggerFactory.getLogger(AbstractForm.class);
 
 	protected final String propertyI18nPrefix;
 	private final SormasBeanFieldGroup<T> fieldGroup;
 	private Class<T> type;
 	private List<Field<?>> customFields = new ArrayList<>();
 
-	@SuppressWarnings("serial")
 	protected AbstractForm(Class<T> type, String propertyI18nPrefix, SormasFieldGroupFieldFactory fieldFactory, boolean addFields) {
 
 		this.type = type;
 		this.propertyI18nPrefix = propertyI18nPrefix;
 
-		fieldGroup = new SormasBeanFieldGroup<T>(type);
+		fieldGroup = new SormasBeanFieldGroup<>(type);
 
 		fieldGroup.setFieldFactory(fieldFactory);
 		setHeightUndefined();
@@ -72,7 +71,7 @@ public abstract class AbstractForm<T> extends CustomField<T> {
 		return layout;
 	}
 
-	public SormasBeanFieldGroup<T> getFieldGroup() {
+	public BeanFieldGroup<T> getFieldGroup() {
 		return this.fieldGroup;
 	}
 
@@ -102,35 +101,31 @@ public abstract class AbstractForm<T> extends CustomField<T> {
 		}
 	}
 
-	@SuppressWarnings({
-		"rawtypes",
-		"hiding" })
-	protected <T extends Field> T addField(FieldConfiguration configuration) {
+	@SuppressWarnings("rawtypes")
+	protected <F extends Field> F addField(FieldConfiguration configuration) {
 		return addField(getContent(), configuration);
 	}
 
 	@SuppressWarnings({
 		"unchecked",
-		"rawtypes",
-		"hiding" })
-	protected <T extends Field> T addField(CustomLayout layout, FieldConfiguration configuration) {
+		"rawtypes" })
+	protected <F extends Field> F addField(CustomLayout layout, FieldConfiguration configuration) {
 		Field field = addField(layout, configuration.getPropertyId());
 
 		applyFieldConfiguration(configuration, field);
 
-		return (T) field;
+		return (F) field;
 	}
 
 	@SuppressWarnings({
 		"rawtypes",
-		"unchecked",
-		"hiding" })
-	protected <T extends Field> T addField(CustomLayout layout, Class<T> fieldType, FieldConfiguration configuration) {
+		"unchecked" })
+	protected <F extends Field> F addField(CustomLayout layout, Class<F> fieldType, FieldConfiguration configuration) {
 		Field field = addField(layout, configuration.getPropertyId(), fieldType);
 
 		applyFieldConfiguration(configuration, field);
 
-		return (T) field;
+		return (F) field;
 	}
 
 	@SuppressWarnings("rawtypes")
@@ -152,19 +147,15 @@ public abstract class AbstractForm<T> extends CustomField<T> {
 		}
 	}
 
-	@SuppressWarnings({
-		"rawtypes",
-		"hiding" })
-	protected <T extends Field> void addFields(Class<T> fieldType, String... properties) {
+	@SuppressWarnings("rawtypes")
+	protected <F extends Field> void addFields(Class<F> fieldType, String... properties) {
 		for (String property : properties) {
 			addField(property, fieldType);
 		}
 	}
 
-	@SuppressWarnings({
-		"rawtypes",
-		"hiding" })
-	protected <T extends Field> void addFields(CustomLayout layout, Class<T> fieldType, String... properties) {
+	@SuppressWarnings("rawtypes")
+	protected <F extends Field> void addFields(CustomLayout layout, Class<F> fieldType, String... properties) {
 		for (String property : properties) {
 			addField(layout, property, fieldType);
 		}
@@ -172,32 +163,26 @@ public abstract class AbstractForm<T> extends CustomField<T> {
 
 	@SuppressWarnings({
 		"unchecked",
-		"rawtypes",
-		"hiding" })
-	protected <T extends Field> T addField(String propertyId) {
-		return (T) addField(propertyId, Field.class);
+		"rawtypes" })
+	protected <F extends Field> F addField(String propertyId) {
+		return (F) addField(propertyId, Field.class);
 	}
 
 	@SuppressWarnings({
 		"unchecked",
-		"rawtypes",
-		"hiding" })
-	protected <T extends Field> T addField(CustomLayout layout, String propertyId) {
-		return (T) addField(layout, propertyId, Field.class);
+		"rawtypes" })
+	protected <F extends Field> F addField(CustomLayout layout, String propertyId) {
+		return (F) addField(layout, propertyId, Field.class);
 	}
 
-	@SuppressWarnings({
-		"rawtypes",
-		"hiding" })
-	protected <T extends Field> T addField(String propertyId, Class<T> fieldType) {
+	@SuppressWarnings("rawtypes")
+	protected <F extends Field> F addField(String propertyId, Class<F> fieldType) {
 		return addField(getContent(), propertyId, fieldType);
 	}
 
-	@SuppressWarnings({
-		"rawtypes",
-		"hiding" })
-	protected <T extends Field> T addField(CustomLayout layout, String propertyId, Class<T> fieldType) {
-		T field = getFieldGroup().buildAndBind(propertyId, (Object) propertyId, fieldType);
+	@SuppressWarnings("rawtypes")
+	protected <F extends Field> F addField(CustomLayout layout, String propertyId, Class<F> fieldType) {
+		F field = getFieldGroup().buildAndBind(propertyId, (Object) propertyId, fieldType);
 		formatField(field, propertyId);
 		field.setId(propertyId);
 		layout.addComponent(field, propertyId);
@@ -205,8 +190,8 @@ public abstract class AbstractForm<T> extends CustomField<T> {
 		return field;
 	}
 
-	protected <T extends Field<?>> T addCustomField(FieldConfiguration fieldConfiguration, Class<?> dataType, Class<T> fieldType) {
-		T field = addCustomField(fieldConfiguration.getPropertyId(), dataType, fieldType);
+	protected <F extends Field<?>> F addCustomField(FieldConfiguration fieldConfiguration, Class<?> dataType, Class<F> fieldType) {
+		F field = addCustomField(fieldConfiguration.getPropertyId(), dataType, fieldType);
 
 		field.setCaption(fieldConfiguration.getCaption());
 		field.setStyleName(fieldConfiguration.getStyle());
@@ -214,18 +199,14 @@ public abstract class AbstractForm<T> extends CustomField<T> {
 		return field;
 	}
 
-	@SuppressWarnings({
-		"rawtypes",
-		"hiding" })
-	protected <T extends Field> T addField(String propertyId, Class<T> fieldType, FieldWrapper<T> fieldWrapper) {
+	@SuppressWarnings("rawtypes")
+	protected <F extends Field> F addField(String propertyId, Class<F> fieldType, FieldWrapper<F> fieldWrapper) {
 		return addField(getContent(), propertyId, fieldType, fieldWrapper);
 	}
 
-	@SuppressWarnings({
-		"rawtypes",
-		"hiding" })
-	protected <T extends Field> T addField(CustomLayout layout, String propertyId, Class<T> fieldType, FieldWrapper<T> fieldWrapper) {
-		T field = getFieldGroup().buildAndBind(propertyId, (Object) propertyId, fieldType);
+	@SuppressWarnings("rawtypes")
+	protected <F extends Field> F addField(CustomLayout layout, String propertyId, Class<F> fieldType, FieldWrapper<F> fieldWrapper) {
+		F field = getFieldGroup().buildAndBind(propertyId, (Object) propertyId, fieldType);
 		formatField(field, propertyId);
 		field.setId(propertyId);
 		// Add validators before wrapping field, so the wrapper can access validators
@@ -234,11 +215,9 @@ public abstract class AbstractForm<T> extends CustomField<T> {
 		return field;
 	}
 
-	@SuppressWarnings({
-		"rawtypes",
-		"hiding" })
-	protected <T extends Field> T addCustomField(String fieldId, Class<?> dataType, Class<T> fieldType) {
-		T field = getFieldGroup().getFieldFactory().createField(dataType, fieldType);
+	@SuppressWarnings("rawtypes")
+	protected <F extends Field> F addCustomField(String fieldId, Class<?> dataType, Class<F> fieldType) {
+		F field = getFieldGroup().getFieldFactory().createField(dataType, fieldType);
 		field.setId(fieldId);
 		formatField(field, fieldId);
 		addDefaultAdditionalValidators(field, dataType);
@@ -247,22 +226,18 @@ public abstract class AbstractForm<T> extends CustomField<T> {
 		return field;
 	}
 
-	@SuppressWarnings({
-		"rawtypes",
-		"hiding" })
-	protected <T extends Field> void formatField(T field, String propertyId) {
+	@SuppressWarnings("rawtypes")
+	protected <F extends Field> void formatField(F field, String propertyId) {
 	}
 
-	@SuppressWarnings({
-		"rawtypes",
-		"hiding" })
+	@SuppressWarnings("rawtypes")
 	/**
 	 * @param allowedDaysInFuture
 	 *            How many days in the future the value of this field can be or
 	 *            -1 for no restriction at all
 	 */
-	protected <T extends Field> T addDateField(String propertyId, Class<T> fieldType, int allowedDaysInFuture) {
-		T field = getFieldGroup().buildAndBind(propertyId, (Object) propertyId, fieldType);
+	protected <F extends Field> F addDateField(String propertyId, Class<F> fieldType, int allowedDaysInFuture) {
+		F field = getFieldGroup().buildAndBind(propertyId, (Object) propertyId, fieldType);
 		formatField(field, propertyId);
 		field.setId(propertyId);
 		getContent().addComponent(field, propertyId);
@@ -270,25 +245,31 @@ public abstract class AbstractForm<T> extends CustomField<T> {
 		return field;
 	}
 
-	@SuppressWarnings({
-		"rawtypes",
-		"hiding" })
+	@SuppressWarnings("rawtypes")
 	/**
 	 * @param fieldDataType
 	 *            - must be specified if the current FieldGroup does not know about the field
 	 */
-	protected <T extends Field> T addDefaultAdditionalValidators(T field, Class<?> fieldDataType) {
+	protected <F extends Field> F addDefaultAdditionalValidators(F field, Class<?> fieldDataType) {
 		addLengthValidator(field, fieldDataType);
 		addFutureDateValidator(field, 0);
 		return field;
 	}
 
-	private <T extends Field> void addLengthValidator(T field, Class<?> fieldDataType) {
-		final Class fieldType = field.getClass();
-		final Class<?> typeOfFieldData =
-			fieldDataType != null ? fieldDataType : getFieldGroup().getPropertyTypeById(getFieldGroup().getPropertyId(field));
+	/**
+	 * @param fieldDataType
+	 *            - must be specified if the current FieldGroup does not know about the field
+	 */
+	private <F extends Field<?>> void addLengthValidator(F field, Class<?> fieldDataType) {
+		final Class<?> typeOfFieldData;
+		if (fieldDataType != null) {
+			typeOfFieldData = fieldDataType;
+		} else {
+			typeOfFieldData = fieldGroup.getPropertyType(fieldGroup.getPropertyId(field));
+		}
 
 		if (typeOfFieldData.equals(String.class)) {
+			final Class<?> fieldType = field.getClass();
 			if (fieldType.isAssignableFrom(TextArea.class) || fieldType.isAssignableFrom(com.vaadin.v7.ui.TextArea.class)) {
 				field.addValidator(new MaxLengthValidator(SormasFieldGroupFieldFactory.TEXT_AREA_MAX_LENGTH));
 			} else if (fieldType.isAssignableFrom(TextField.class) || fieldType.isAssignableFrom(com.vaadin.v7.ui.TextField.class)) {
@@ -297,10 +278,8 @@ public abstract class AbstractForm<T> extends CustomField<T> {
 		}
 	}
 
-	@SuppressWarnings({
-		"rawtypes",
-		"hiding" })
-	protected <T extends Field> T addFutureDateValidator(T field, int amountOfDays) {
+	@SuppressWarnings("rawtypes")
+	protected <F extends Field> F addFutureDateValidator(F field, int amountOfDays) {
 		if (amountOfDays < 0) {
 			return field;
 		}
@@ -349,14 +328,17 @@ public abstract class AbstractForm<T> extends CustomField<T> {
 		return FacadeProvider.getConfigFacade().isGermanServer();
 	}
 
-	public static class SormasBeanFieldGroup<T> extends BeanFieldGroup<T> {
+	private static class SormasBeanFieldGroup<T> extends BeanFieldGroup<T> {
 
-		public SormasBeanFieldGroup(Class beanType) {
+		private static final long serialVersionUID = 7271384537227612872L;
+
+		public SormasBeanFieldGroup(Class<T> beanType) {
 			super(beanType);
 		}
 
-		public Class<?> getPropertyTypeById(Object propertyId) {
-			return this.getPropertyType(propertyId);
+		@Override
+		public Class<?> getPropertyType(Object propertyId) {
+			return super.getPropertyType(propertyId);
 		}
 
 		@Override
@@ -367,9 +349,7 @@ public abstract class AbstractForm<T> extends CustomField<T> {
 				field.setEnabled(false);
 			}
 
-			if (field.getPropertyDataSource().isReadOnly()) {
-				field.setReadOnly(true);
-			} else if (isReadOnly()) {
+			if (field.getPropertyDataSource().isReadOnly() || isReadOnly()) {
 				field.setReadOnly(true);
 			}
 		}
