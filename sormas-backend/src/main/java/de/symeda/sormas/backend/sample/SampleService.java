77332898499fbcd1360b7adfc06faf4a135e4807
N.O.R.T.H <yormen1@gmail.com>
2020-09-25 16:15:49 +0000

    Added an external sample purpose condition to the service

--- sormas-backend/src/main/java/de/symeda/sormas/backend/sample/SampleService.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/sample/SampleService.java
@@ -379,6 +379,10 @@ public class SampleService extends AbstractCoreAdoService<Sample> {
 		if (criteria.getReceived() != null) {
 			filter = and(cb, filter, cb.equal(sample.get(Sample.RECEIVED), criteria.getReceived()));
 		}
+		if (criteria.getSamplePurpose() != null) {
+			filter = and(cb, filter, cb.equal(sample.get(Sample.SAMPLE_PURPOSE), criteria.getSamplePurpose()));
+		}
+
 		if (criteria.getReferred() != null) {
 			if (criteria.getReferred().equals(Boolean.TRUE)) {
 				filter = and(cb, filter, cb.isNotNull(sample.get(Sample.REFERRED_TO)));
@@ -410,9 +414,9 @@ public class SampleService extends AbstractCoreAdoService<Sample> {
 		if (criteria.getEventParticipant() != null) {
 			filter = and(cb, filter, cb.equal(joins.getEventParticipant().get(EventParticipant.UUID), criteria.getEventParticipant().getUuid()));
 		}
-		
+
 		filter = createSampleDateFilter(cb, filter, sample, criteria);
-		
+
 		if (criteria.getSpecimenCondition() != null) {
 			filter = and(cb, filter, cb.equal(sample.get(Sample.SPECIMEN_CONDITION), criteria.getSpecimenCondition()));
 		}
@@ -447,31 +451,38 @@ public class SampleService extends AbstractCoreAdoService<Sample> {
 
 		return filter;
 	}
-	
+
 	private Predicate createSampleDateFilter(CriteriaBuilder cb, Predicate filter, From<?, ?> sample, SampleCriteria criteria) {
-		
+
 		String dateProperty = Sample.SAMPLE_DATE_TIME;
 		if (criteria.getSampleDateType() != null) {
 			switch (criteria.getSampleDateType()) {
-				case REPORT: dateProperty = Sample.REPORT_DATE_TIME; break;
-				case SHIPPED: dateProperty = Sample.SHIPMENT_DATE; break;
-				case RECEIVED: dateProperty = Sample.RECEIVED_DATE; break;
-				case RESULT: dateProperty = Sample.PATHOGEN_TEST_RESULT_CHANGE_DATE; break;
-				default: dateProperty = Sample.SAMPLE_DATE_TIME; break;
+			case REPORT:
+				dateProperty = Sample.REPORT_DATE_TIME;
+				break;
+			case SHIPPED:
+				dateProperty = Sample.SHIPMENT_DATE;
+				break;
+			case RECEIVED:
+				dateProperty = Sample.RECEIVED_DATE;
+				break;
+			case RESULT:
+				dateProperty = Sample.PATHOGEN_TEST_RESULT_CHANGE_DATE;
+				break;
+			default:
+				dateProperty = Sample.SAMPLE_DATE_TIME;
+				break;
 			}
 		}
-		
+
 		if (criteria.getSampleDateFrom() != null && criteria.getSampleDateTo() != null) {
-			filter = and(
-				cb,
-				filter,
-				cb.between(sample.get(dateProperty), criteria.getSampleDateFrom(), criteria.getSampleDateTo()));
+			filter = and(cb, filter, cb.between(sample.get(dateProperty), criteria.getSampleDateFrom(), criteria.getSampleDateTo()));
 		} else if (criteria.getSampleDateFrom() != null) {
 			filter = and(cb, filter, cb.greaterThanOrEqualTo(sample.get(dateProperty), criteria.getSampleDateFrom()));
 		} else if (criteria.getSampleDateTo() != null) {
 			filter = and(cb, filter, cb.lessThanOrEqualTo(sample.get(dateProperty), criteria.getSampleDateTo()));
 		}
-		
+
 		return filter;
 	}
 
