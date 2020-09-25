6e5ad87860124cefc57dfb776e7baa3e550296bc
N.O.R.T.H <yormen1@gmail.com>
2020-09-25 16:14:34 +0000

    Added external sample purpose to the count method to be used toquery for only external shippment status

--- sormas-backend/src/main/java/de/symeda/sormas/backend/sample/SampleFacadeEjb.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/sample/SampleFacadeEjb.java
@@ -65,6 +65,7 @@ import de.symeda.sormas.api.sample.SampleDto;
 import de.symeda.sormas.api.sample.SampleExportDto;
 import de.symeda.sormas.api.sample.SampleFacade;
 import de.symeda.sormas.api.sample.SampleIndexDto;
+import de.symeda.sormas.api.sample.SamplePurpose;
 import de.symeda.sormas.api.sample.SampleReferenceDto;
 import de.symeda.sormas.api.sample.SpecimenCondition;
 import de.symeda.sormas.api.user.UserRight;
@@ -814,30 +815,35 @@ public class SampleFacadeEjb implements SampleFacade {
 				.disease(disease)
 				.reportDateBetween(from, to, DateFilterOption.DATE)
 				.specimenCondition(SpecimenCondition.NOT_ADEQUATE));
+
 		long shippedCount = count(
 			(new SampleCriteria()).region(regionRef)
 				.district(districtRef)
 				.disease(disease)
 				.reportDateBetween(from, to, DateFilterOption.DATE)
-				.shipped(true));
+				.shipped(true)
+				.samplePurpose(SamplePurpose.EXTERNAL));
 		long notShippedCount = count(
 			(new SampleCriteria()).region(regionRef)
 				.district(districtRef)
 				.disease(disease)
 				.reportDateBetween(from, to, DateFilterOption.DATE)
-				.shipped(false));
+				.shipped(false)
+				.samplePurpose(SamplePurpose.EXTERNAL));
 		long receivedCount = count(
 			(new SampleCriteria()).region(regionRef)
 				.district(districtRef)
 				.disease(disease)
 				.reportDateBetween(from, to, DateFilterOption.DATE)
-				.received(true));
+				.received(true)
+				.samplePurpose(SamplePurpose.EXTERNAL));
 		long notReceivedCount = count(
 			(new SampleCriteria()).region(regionRef)
 				.district(districtRef)
 				.disease(disease)
 				.reportDateBetween(from, to, DateFilterOption.DATE)
-				.received(false));
+				.received(false)
+				.samplePurpose(SamplePurpose.EXTERNAL));
 
 		Map<SampleCountType, Long> map = new HashMap<SampleCountType, Long>();
 		map.put(SampleCountType.TOTAL, total);
