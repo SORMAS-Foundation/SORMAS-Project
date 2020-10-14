a17822ed2b85622b54a3c3a81a5172a2d342313c
Alex <alexandru.vidrean@fxbits.io>
2020-10-14 10:30:16 +0300

    #2978 Change QuarantineType enums ordering (#3159)

--- sormas-api/src/main/java/de/symeda/sormas/api/contact/QuarantineType.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/contact/QuarantineType.java
@@ -6,14 +6,14 @@ import de.symeda.sormas.api.utils.HideForCountriesExcept;
 
 public enum QuarantineType {
 
+	HOME,
+	INSTITUTIONELL,
 	@HideForCountriesExcept(countries = CountryHelper.COUNTRY_CODE_SWITZERLAND)
 	HOSPITAL,
 	@HideForCountriesExcept(countries = CountryHelper.COUNTRY_CODE_SWITZERLAND)
 	HOTEL,
 	@HideForCountriesExcept(countries = CountryHelper.COUNTRY_CODE_SWITZERLAND)
 	ASYLUM_ACCOMMODATION,
-	INSTITUTIONELL,
-	HOME,
 	NONE,
 	UNKNOWN,
 	OTHER;
