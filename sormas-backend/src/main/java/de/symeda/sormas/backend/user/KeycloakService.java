9af5c4fb9090eedee06d9808fff350fe156efae0
Adrian <adriansorge95@gmail.com>
2020-11-02 17:37:04 +0000

    Update KeycloakService.java

--- sormas-backend/src/main/java/de/symeda/sormas/backend/user/KeycloakService.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/user/KeycloakService.java
@@ -49,6 +49,7 @@ import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -109,13 +110,18 @@ public class KeycloakService {
 
 		try {
 			JSONObject json = JSONObjectUtils.parse(oidcJson.get());
-
+			ResteasyClient RestClient = null
+                        if (configFacade.getProxyHost() != null && configFacade.getProxyPort() != null && configFacade.getProxyProtocol() != null){
+				RestClient = new ResteasyClientBuilder().defaultProxy(configFacade.getProxyHost(), configFacade.getProxyPort(), configFacade.getProxyProtocol()).build()
+			} else {
+				RestClient = new ResteasyClientBuilder().build()
+			}
 			keycloak = KeycloakBuilder.builder()
 				.realm(json.getAsString(OIDC_REALM))
 				.serverUrl(json.getAsString(OIDC_SERVER_URL))
 				.clientId("sormas-backend")
 				.clientSecret(JSONObjectUtils.getJSONObject(json, OIDC_CREDENTIALS).getAsString(OIDC_SECRET))
-				.resteasyClient(new ResteasyClientBuilder().defaultProxy(configFacade.getProxyHost(), configFacade.getProxyPort(), configFacade.getProxyProtocol()).build())
+				.resteasyClient(RestClient)
 				.grantType(OAuth2Constants.CLIENT_CREDENTIALS)
 				.build();
 
