99bcf71888ad267f998dd49f17b86a0d7d439929
Adrian <adriansorge95@gmail.com>
2020-11-02 17:30:38 +0000

    Update KeycloakService.java

--- sormas-backend/src/main/java/de/symeda/sormas/backend/user/KeycloakService.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/user/KeycloakService.java
@@ -115,7 +115,7 @@ public class KeycloakService {
 				.serverUrl(json.getAsString(OIDC_SERVER_URL))
 				.clientId("sormas-backend")
 				.clientSecret(JSONObjectUtils.getJSONObject(json, OIDC_CREDENTIALS).getAsString(OIDC_SECRET))
-				.resteasyClient(new ResteasyClientBuilder().defaultProxy("10.135.160.196", 3128, "http").build())
+				.resteasyClient(new ResteasyClientBuilder().defaultProxy(configFacade.getProxyHost(), configFacade.getProxyPort(), configFacade.getProxyProtocol()).build())
 				.grantType(OAuth2Constants.CLIENT_CREDENTIALS)
 				.build();
 
