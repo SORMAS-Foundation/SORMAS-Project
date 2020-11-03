8f226a0d9616984571cd4837f909c27fb853660d
vlad.ciucescu@fxbits.io <vlad.ciucescu@fxbits.io>
2020-11-03 17:23:25 +0200

    #2993 add Country to app

---
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/rest/CountryFacadeRetro.java
@@ -0,0 +1,22 @@
+package de.symeda.sormas.app.rest;
+
+import java.util.List;
+
+import de.symeda.sormas.api.region.CountryDto;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface CountryFacadeRetro {
+
+	@GET("countries/all/{since}")
+	Call<List<CountryDto>> pullAllSince(@Path("since") long since);
+
+	@POST("countries/query")
+	Call<List<CountryDto>> pullByUuids(@Body List<String> uuids);
+
+	@GET("countries/uuids")
+	Call<List<String>> pullUuids();
+}
