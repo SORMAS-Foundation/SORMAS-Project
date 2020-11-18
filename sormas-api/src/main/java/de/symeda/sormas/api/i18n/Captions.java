abbf1bc9955000c3cf04ef534a29ee399f5fd357
Thomas Broyer <tbr@atolcd.com>
2020-11-18 10:02:27 +0100

    Regenerate i18n constants
    Change-Id: Ie78f7ddbc6ebefb0d38e6b619599317e26d0cd1c

--- sormas-api/src/main/java/de/symeda/sormas/api/i18n/Captions.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/i18n/Captions.java
@@ -1,17 +1,3 @@
-/*
- * SORMAS® - Surveillance Outbreak Response Management & Analysis System
- * Copyright © 2016-2020 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
 package de.symeda.sormas.api.i18n;
 
 import javax.annotation.Generated;
@@ -720,7 +706,6 @@ public interface Captions {
 	String districtAllDistricts = "districtAllDistricts";
 	String districtArchivedDistricts = "districtArchivedDistricts";
 	String districtName = "districtName";
-	String documentUploadDocument = "documentUploadDocument";
 	String documentNoDocuments = "documentNoDocuments";
 	String DocumentTemplate = "DocumentTemplate";
 	String DocumentTemplate_documentTemplateGuide = "DocumentTemplate.documentTemplateGuide";
@@ -731,6 +716,7 @@ public interface Captions {
 	String DocumentTemplate_QuarantineOrder_create = "DocumentTemplate.QuarantineOrder.create";
 	String DocumentTemplate_QuarantineOrder_templates = "DocumentTemplate.QuarantineOrder.templates";
 	String DocumentTemplate_uploadTemplate = "DocumentTemplate.uploadTemplate";
+	String documentUploadDocument = "documentUploadDocument";
 	String EpiData = "EpiData";
 	String EpiData_animalCondition = "EpiData.animalCondition";
 	String EpiData_animalVaccinationStatus = "EpiData.animalVaccinationStatus";
--- sormas-api/src/main/java/de/symeda/sormas/api/i18n/Strings.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/i18n/Strings.java
@@ -1,17 +1,3 @@
-/*
- * SORMAS® - Surveillance Outbreak Response Management & Analysis System
- * Copyright © 2016-2020 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
 package de.symeda.sormas.api.i18n;
 
 import javax.annotation.Generated;
@@ -176,8 +162,8 @@ public interface Strings {
 	String errorCampaignDiagramTotalsCalculationError = "errorCampaignDiagramTotalsCalculationError";
 	String errorCaseDuplicateDeletion = "errorCaseDuplicateDeletion";
 	String errorCaseMerging = "errorCaseMerging";
-	String errorDeletingDocumentTemplate = "errorDeletingDocumentTemplate";
 	String errorDeletingDocument = "errorDeletingDocument";
+	String errorDeletingDocumentTemplate = "errorDeletingDocumentTemplate";
 	String errorDocumentGeneration = "errorDocumentGeneration";
 	String errorEntityOutdated = "errorEntityOutdated";
 	String errorFieldValidationFailed = "errorFieldValidationFailed";
@@ -191,8 +177,8 @@ public interface Strings {
 	String errorProblemOccurred = "errorProblemOccurred";
 	String errorProcessingTemplate = "errorProcessingTemplate";
 	String errorQuarantineOnlyCaseAndContacts = "errorQuarantineOnlyCaseAndContacts";
-	String errorReadingTemplate = "errorReadingTemplate";
 	String errorReadingDocument = "errorReadingDocument";
+	String errorReadingTemplate = "errorReadingTemplate";
 	String errorSetFeverRequired = "errorSetFeverRequired";
 	String errorSormasToSormasCertNotGenerated = "errorSormasToSormasCertNotGenerated";
 	String errorSormasToSormasConnection = "errorSormasToSormasConnection";
