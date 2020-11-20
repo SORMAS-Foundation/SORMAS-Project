1ac63b0c169dbcf5e0f3ac6c709d013c18efee22
alexandre-gille <72074103+alexandre-gille@users.noreply.github.com>
2020-11-20 08:37:56 +0100

    Warns if the assigned user is missing contact info at task creation/update if notifications are enabled #3273 (#3432)
    * Warns if the assigned user is missing contact info at task creation/update if notifications are enabled #3273
    
    * Rename checkIfAssigneeEmailIsProvided to checkIfAssigneeEmailOrPhoneIsProvided #3273
    
    * Set the task's assignee missing info label to full width #3273

--- sormas-backend/src/main/java/de/symeda/sormas/backend/common/ConfigFacadeEjb.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/common/ConfigFacadeEjb.java
@@ -530,4 +530,10 @@ public class ConfigFacadeEjb implements ConfigFacade {
 	public static class ConfigFacadeEjbLocal extends ConfigFacadeEjb {
 
 	}
+
+	@Override
+	public boolean isSmsServiceSetUp() {
+		return !StringUtils.isAnyBlank(getProperty(SMS_AUTH_KEY, null), getProperty(SMS_AUTH_SECRET, null));
+	}
+
 }
