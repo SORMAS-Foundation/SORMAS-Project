731be0156ae09ca71b26d56518b58a250127ccc6
lgal <Valamim1>
2020-11-23 12:01:31 +0200

    #3388 fix failing pseudonymization test

--- sormas-backend/src/test/java/de/symeda/sormas/backend/caze/CaseFacadeEjbPseudonymizationTest.java
+++ sormas-backend/src/test/java/de/symeda/sormas/backend/caze/CaseFacadeEjbPseudonymizationTest.java
@@ -158,7 +158,9 @@ public class CaseFacadeEjbPseudonymizationTest extends AbstractBeanTest {
 		creator.createContact(user2.toReference(), createPerson().toReference(), caze1);
 		CaseDataDto caze2 = createCase(rdcf2, user2);
 
-		List<CaseIndexDto> indexList = getCaseFacade().getIndexList(new CaseCriteria(), null, null, Collections.emptyList());
+		CaseCriteria caseCriteria = new CaseCriteria();
+		caseCriteria.setIncludeCasesFromOtherJurisdictions(true);
+		List<CaseIndexDto> indexList = getCaseFacade().getIndexList(caseCriteria, null, null, Collections.emptyList());
 
 		CaseIndexDto caseIndex1 = indexList.stream().filter(c -> c.getUuid().equals(caze1.getUuid())).findFirst().get();
 
