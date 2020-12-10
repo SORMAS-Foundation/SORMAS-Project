04984a017e99176e8cc37dfc59ffee28db98d77c
Christopher Riedel <42139870+ChristopherRiedel@users.noreply.github.com>
2020-12-10 15:16:38 +0100

    #3486: Add LabMessage to backend

---
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/labmessage/LabMessageFacadeEjb.java
@@ -0,0 +1,107 @@
+package de.symeda.sormas.backend.labmessage;
+
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.validation.constraints.NotNull;
+
+import de.symeda.sormas.api.labmessage.LabMessageDto;
+import de.symeda.sormas.api.labmessage.LabMessageFacade;
+import de.symeda.sormas.backend.util.DtoHelper;
+
+@Stateless(name = "LabMessageFacade")
+public class LabMessageFacadeEjb implements LabMessageFacade {
+
+	@EJB
+	LabMessageService labMessageService;
+
+	private LabMessage fromDto(@NotNull LabMessageDto source, LabMessage target) {
+
+		if (target == null) {
+			target = new LabMessage();
+			target.setUuid(source.getUuid());
+		}
+
+		DtoHelper.validateDto(source, target);
+
+		target.setLabMessageDetails(source.getLabMessageDetails());
+		target.setLabSampleId(source.getLabSampleId());
+		target.setPersonBirthDateDD(source.getPersonBirthDateDD());
+		target.setPersonBirthDateMM(source.getPersonBirthDateMM());
+		target.setPersonBirthDateYYYY(source.getPersonBirthDateYYYY());
+		target.setPersonCity(source.getPersonCity());
+		target.setPersonFirstName(source.getPersonFirstName());
+		target.setPersonHouseNumber(source.getPersonHouseNumber());
+		target.setPersonLastName(source.getPersonLastName());
+		target.setPersonPostalCode(source.getPersonPostalCode());
+		target.setPersonSex(source.getPersonSex());
+		target.setPersonStreet(source.getPersonStreet());
+		target.setProcessed(source.isProcessed());
+		target.setSampleDateTime(source.getSampleDateTime());
+		target.setSampleMaterial(source.getSampleMaterial());
+		target.setSampleReceivedDate(source.getSampleReceivedDate());
+		target.setSpecimenCondition(source.getSpecimenCondition());
+		target.setTestDateTime(source.getTestDateTime());
+		target.setTestedDisease(source.getTestedDisease());
+		target.setTestLabCity(source.getTestLabCity());
+		target.setTestLabExternalId(source.getTestLabExternalId());
+		target.setTestLabName(source.getTestLabName());
+		target.setTestLabPostalCode(source.getTestLabPostalCode());
+		target.setTestResult(source.getTestResult());
+		target.setTestType(source.getTestType());
+
+		return target;
+	}
+
+	@Override
+	public void save(LabMessageDto dto) {
+
+		LabMessage labMessage = labMessageService.getByUuid(dto.getUuid());
+
+		labMessage = fromDto(dto, labMessage);
+		labMessageService.ensurePersisted(labMessage);
+	}
+
+	public LabMessageDto toDto(LabMessage source) {
+
+		if (source == null) {
+			return null;
+		}
+		LabMessageDto target = new LabMessageDto();
+		DtoHelper.fillDto(target, source);
+
+		target.setLabMessageDetails(source.getLabMessageDetails());
+		target.setLabSampleId(source.getLabSampleId());
+		target.setPersonBirthDateDD(source.getPersonBirthDateDD());
+		target.setPersonBirthDateMM(source.getPersonBirthDateMM());
+		target.setPersonBirthDateYYYY(source.getPersonBirthDateYYYY());
+		target.setPersonCity(source.getPersonCity());
+		target.setPersonFirstName(source.getPersonFirstName());
+		target.setPersonHouseNumber(source.getPersonHouseNumber());
+		target.setPersonLastName(source.getPersonLastName());
+		target.setPersonPostalCode(source.getPersonPostalCode());
+		target.setPersonSex(source.getPersonSex());
+		target.setPersonStreet(source.getPersonStreet());
+		target.setProcessed(source.isProcessed());
+		target.setSampleDateTime(source.getSampleDateTime());
+		target.setSampleMaterial(source.getSampleMaterial());
+		target.setSampleReceivedDate(source.getSampleReceivedDate());
+		target.setSpecimenCondition(source.getSpecimenCondition());
+		target.setTestDateTime(source.getTestDateTime());
+		target.setTestedDisease(source.getTestedDisease());
+		target.setTestLabCity(source.getTestLabCity());
+		target.setTestLabExternalId(source.getTestLabExternalId());
+		target.setTestLabName(source.getTestLabName());
+		target.setTestLabPostalCode(source.getTestLabPostalCode());
+		target.setTestResult(source.getTestResult());
+		target.setTestType(source.getTestType());
+
+		return target;
+	}
+
+	@LocalBean
+	@Stateless
+	public static class LabMessageFacadeEjbLocal extends LabMessageFacadeEjb {
+
+	}
+}
