72400233753e588575780716550bd56bacc906da
FredrikSchäferVitagroup <fredrik.schaefer@vitagroup.ag>
2021-01-08 11:12:46 +0100

    #3488 fix to match latest state of development

--- sormas-backend/src/main/java/de/symeda/sormas/backend/labmessage/LabMessageService.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/labmessage/LabMessageService.java
@@ -1,7 +1,19 @@
 package de.symeda.sormas.backend.labmessage;
 
+import de.symeda.sormas.api.labmessage.LabMessageCriteria;
+import de.symeda.sormas.backend.common.AdoServiceWithUserFilter;
 import de.symeda.sormas.backend.common.BaseAdoService;
 
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.From;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+@Stateless
+@LocalBean
 public class LabMessageService extends BaseAdoService<LabMessage> {
 
 	public LabMessageService() {
