c4b8713faa58f1596a9eb7973dd341813cd980bc
Stefan Kock <s.kock@symeda.de>
2021-01-14 15:49:06 +0100

    #3768: Prevent tag attribute outbreak
    - Sanitizing the hyperlink to prevent unexpected tag attributes changed
    the behaviour to double quotes by Jsoup

--- sormas-api/src/test/java/de/symeda/sormas/api/utils/HtmlHelperTest.java
+++ sormas-api/src/test/java/de/symeda/sormas/api/utils/HtmlHelperTest.java
@@ -24,15 +24,14 @@ public class HtmlHelperTest {
 	@Test
 	public void testBuildHyperlinkTitle() {
 
-		assertThat(HtmlHelper.buildHyperlinkTitle("hoverText", "visualText"), equalTo("<a title='hoverText'>visualText</a>"));
+		assertThat(HtmlHelper.buildHyperlinkTitle("hoverText", "visualText"), equalTo("<a title=\"hoverText\">visualText</a>"));
 		assertThat(
 			HtmlHelper.buildHyperlinkTitle("transitive relation", "a < b & b < c = a < c"),
-			equalTo("<a title='transitive relation'>a &lt; b &amp; b &lt; c = a &lt; c</a>"));
+			equalTo("<a title=\"transitive relation\">a &lt; b &amp; b &lt; c = a &lt; c</a>"));
+		assertThat(HtmlHelper.buildHyperlinkTitle("not <b>bold</b>", "not <i>italic</i>"), equalTo("<a title=\"not bold\">not italic</a>"));
 		assertThat(
-			HtmlHelper.buildHyperlinkTitle(
-				"not <b>bold</b>",
-				"not <i>italic</i>"),
-			equalTo("<a title='not bold'>not italic</a>"));
+			HtmlHelper.buildHyperlinkTitle("tagContent' href=\"javascript:alert(1);\" class='", "breakout in title tag prevented"),
+			equalTo("<a title=\"tagContent\">breakout in title tag prevented</a>"));
 	}
 
 	@Test
