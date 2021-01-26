1c8ebed952b784fd41ffe465939a593719ceae0c
Seal33 <klausbart@gmx.net>
2021-01-26 19:13:09 +0100

    comment out different parts of the test #4133

--- sormas-backend/src/test/java/de/symeda/sormas/backend/util/PerformanceLoggingInterceptorTest.java
+++ sormas-backend/src/test/java/de/symeda/sormas/backend/util/PerformanceLoggingInterceptorTest.java
@@ -24,11 +24,11 @@ public class PerformanceLoggingInterceptorTest {
 		Method method = mock(Method.class);
 		when(context.getMethod()).thenReturn(method);
 
-		when(context.getTarget()).thenReturn("de.symeda.sormas.backend.region.CommunityService@15301bba");
-		when(context.getMethod().getName()).thenReturn("count");
-		assertThat(PerformanceLoggingInterceptor.getInvokedMethod(context), equalTo("CommunityService.count"));
+		//when(context.getTarget()).thenReturn("de.symeda.sormas.backend.region.CommunityService@15301bba");
+		//when(context.getMethod().getName()).thenReturn("count");
+		//assertThat(PerformanceLoggingInterceptor.getInvokedMethod(context), equalTo("CommunityService.count"));
 
-		//when(context.getTarget()).thenReturn("de.symeda.sormas.backend.facility.FacilityFacadeEjb$FacilityFacadeEjbLocal@630e5556");
-		//assertThat(PerformanceLoggingInterceptor.getInvokedMethod(context), equalTo("FacilityFacadeEjb.count"));
+		when(context.getTarget()).thenReturn("de.symeda.sormas.backend.facility.FacilityFacadeEjb$FacilityFacadeEjbLocal@630e5556");
+		assertThat(PerformanceLoggingInterceptor.getInvokedMethod(context), equalTo("FacilityFacadeEjb.count"));
 	}
 }
