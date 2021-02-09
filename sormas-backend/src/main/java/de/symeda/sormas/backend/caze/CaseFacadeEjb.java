23a9c1afc8107c81786eb1875f9d21ec6a1cd9b7
Bartha Barna <barna.bartha@she.net>
2021-02-09 16:47:30 +0200

    Feature 4304 case completeness count fix (#4310)
    * #4304 - replace count > 0 checks with exists checks for case completeness calculation
    
    * #4304 - remove no longer used methods
    
    * #4304 - do not consider deleted entries for case completeness
    
    Co-authored-by: barnabartha <barnabartha@she.net>

--- sormas-backend/src/main/java/de/symeda/sormas/backend/caze/CaseFacadeEjb.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/caze/CaseFacadeEjb.java
@@ -2055,13 +2055,13 @@ public class CaseFacadeEjb implements CaseFacade {
 		if (!CaseClassification.NOT_CLASSIFIED.equals(caze.getCaseClassification())) {
 			completeness += 0.2f;
 		}
-		if (sampleService.getSampleCountByCase(caze) > 0) {
+		if (sampleService.exists((cb, root) -> cb.and(sampleService.createDefaultFilter(cb, root), cb.equal(root.get(Sample.ASSOCIATED_CASE), caze)))) {
 			completeness += 0.15f;
 		}
 		if (Boolean.TRUE.equals(caze.getSymptoms().getSymptomatic())) {
 			completeness += 0.15f;
 		}
-		if (contactService.getContactCountByCase(caze) > 0) {
+		if (contactService.exists((cb, root) -> cb.and(contactService.createDefaultFilter(cb, root), cb.equal(root.get(Contact.CAZE), caze)))) {
 			completeness += 0.10f;
 		}
 		if (!CaseOutcome.NO_OUTCOME.equals(caze.getOutcome())) {
@@ -3223,21 +3223,13 @@ public class CaseFacadeEjb implements CaseFacade {
 
 	@Override
 	public boolean hasPositiveLabResult(String caseUuid) {
-		final CriteriaBuilder cb = em.getCriteriaBuilder();
-
-		final CriteriaQuery<Sample> query = cb.createQuery(Sample.class);
+		final CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+		final CriteriaQuery<Sample> query = criteriaBuilder.createQuery(Sample.class);
 		final Root<Sample> sampleRoot = query.from(Sample.class);
-
 		final Join<Sample, Case> caseJoin = sampleRoot.join(Sample.ASSOCIATED_CASE, JoinType.INNER);
-
-		query.select(sampleRoot);
-		final Predicate casePredicate = cb.equal(caseJoin.get(AbstractDomainObject.UUID), caseUuid);
-		final Predicate testPositivityPredicate = cb.equal(sampleRoot.get(Sample.PATHOGEN_TEST_RESULT), PathogenTestResultType.POSITIVE);
-		query.where(cb.and(casePredicate, testPositivityPredicate));
-
-		final TypedQuery<Sample> typedQuery = em.createQuery(query);
-
-		return typedQuery.getResultList().size() > 0;
+		final Predicate casePredicate = criteriaBuilder.equal(caseJoin.get(AbstractDomainObject.UUID), caseUuid);
+		final Predicate testPositivityPredicate = criteriaBuilder.equal(sampleRoot.get(Sample.PATHOGEN_TEST_RESULT), PathogenTestResultType.POSITIVE);
+		return sampleService.exists((cb, root) -> cb.and(casePredicate, testPositivityPredicate));
 	}
 
 	@Override
@@ -3514,23 +3506,21 @@ public class CaseFacadeEjb implements CaseFacade {
 
 			Predicate reportDatePredicate;
 
-			if (reportDateThreshold == 0){
+			if (reportDateThreshold == 0) {
 				// threshold is zero: we want to get exact matches
 				reportDatePredicate = cb.equal(
-						cb.function("date", Date.class, caseRoot.get(Case.REPORT_DATE)),
-						cb.function("date", Date.class, cb.literal(searchCaze.getReportDate()))
-				);
-			} else{
+					cb.function("date", Date.class, caseRoot.get(Case.REPORT_DATE)),
+					cb.function("date", Date.class, cb.literal(searchCaze.getReportDate())));
+			} else {
 				// threshold is nonzero: apply time range of threshold to the reportDate
 				Date reportDate = casePerson.getCaze().getReportDate();
 				Date dateBefore = new DateTime(reportDate).minusDays(reportDateThreshold).toDate();
-				Date dateAfter= new DateTime(reportDate).plusDays(reportDateThreshold).toDate();;
+				Date dateAfter = new DateTime(reportDate).plusDays(reportDateThreshold).toDate();;
 
 				reportDatePredicate = cb.between(
-						cb.function("date", Date.class, caseRoot.get(Case.REPORT_DATE)),
-						cb.function("date", Date.class, cb.literal(dateBefore)),
-						cb.function("date", Date.class, cb.literal(dateAfter))
-				);
+					cb.function("date", Date.class, caseRoot.get(Case.REPORT_DATE)),
+					cb.function("date", Date.class, cb.literal(dateBefore)),
+					cb.function("date", Date.class, cb.literal(dateAfter)));
 			}
 
 			combinedPredicate = and(
