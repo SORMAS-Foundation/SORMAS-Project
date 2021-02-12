f729d3389c3c1c721c749db5142068679f9746c9
raghupola-vg <raghavender.pola@vitagroup.ag>
2021-02-12 10:51:10 +0100

    #4355 - changes to few test suites and OR's

--- sormas-e2e-ui-tests/Scripts/Contacts/partials/switchToContacts/Script1586946842050.groovy
+++ sormas-e2e-ui-tests/Scripts/Contacts/partials/switchToContacts/Script1586946842050.groovy
@@ -2,4 +2,4 @@ import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
-WebUI.click(findTestObject('Contacts/MainView/menu_Contacts'))
+WebUI.click(findTestObject('Login/MainView/menu_Contacts'))
