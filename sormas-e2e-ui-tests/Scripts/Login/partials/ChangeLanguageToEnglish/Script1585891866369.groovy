e0a8a80412949b19192b354fc2eaa6ff7c2f441c
raghupola-vg <raghavender.pola@vitagroup.ag>
2021-02-26 18:50:15 +0100

    #4536 - Adjust login times and page load  for Jenkins failures

--- sormas-e2e-ui-tests/Scripts/Login/partials/ChangeLanguageToEnglish/Script1585891866369.groovy
+++ sormas-e2e-ui-tests/Scripts/Login/partials/ChangeLanguageToEnglish/Script1585891866369.groovy
@@ -1,5 +1,6 @@
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 WebUI.scrollToElement(findTestObject('Login/MainView/menu_Settings'), 2)
@@ -12,4 +13,15 @@ WebUI.click(findTestObject('Login/SettingsDialog/td_English'))
 
 WebUI.click(findTestObject('ReusableORs/div_Save'))
 
-WebUI.delay(2)
+WebUI.delay(3)
+
+WebUI.waitForPageLoad(20)
+
+if (WebUI.waitForElementVisible(findTestObject('Login/SettingsDialog/warning popup'), 5, FailureHandling.OPTIONAL)) {
+    WebUI.click(findTestObject('Login/SettingsDialog/warning popup'))
+
+    WebUI.click(findTestObject('ReusableORs/div_Discard'))
+
+    WebUI.callTestCase(findTestCase('Login/partials/ChangeLanguageToEnglish'), [:])
+}
+
