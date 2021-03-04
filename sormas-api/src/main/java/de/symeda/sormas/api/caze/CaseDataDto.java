0e377efbcdc60c20d414c4c32c258dc23e258d45
Christopher Riedel <42139870+ChristopherRiedel@users.noreply.github.com>
2021-03-04 13:54:09 +0100

    #4332: Copy followup comment when converting contact to case (#4535)
    * #4332: Copy followup comment when converting contact to case
    
    * #4332: fixed test failures
    
    Co-authored-by: Christopher Riedel <42139870+ChristopherRiedelSymeda@users.noreply.github.com>

--- sormas-api/src/main/java/de/symeda/sormas/api/caze/CaseDataDto.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/caze/CaseDataDto.java
@@ -636,6 +636,8 @@ public class CaseDataDto extends PseudonymizableDto implements SormasToSormasEnt
 		caze.setCaseClassification(CaseClassification.NOT_CLASSIFIED);
 		caze.setOutcome(CaseOutcome.NO_OUTCOME);
 		caze.setCaseOrigin(CaseOrigin.IN_COUNTRY);
+		// TODO This is a workaround for transferring the followup comment while converting a contact to a case. This can be removed if the followup for cases is implemented in the mobile app
+		caze.setFollowUpStatus(FollowUpStatus.NO_FOLLOW_UP);
 		return caze;
 	}
 
@@ -655,6 +657,7 @@ public class CaseDataDto extends PseudonymizableDto implements SormasToSormasEnt
 
 	private static void migratesAttributes(ContactDto contact, CaseDataDto cazeData) {
 		cazeData.setEpiData(contact.getEpiData());
+		cazeData.setFollowUpComment(contact.getFollowUpComment());
 	}
 
 	public static CaseDataDto buildFromEventParticipant(EventParticipantDto eventParticipant, PersonDto person, Disease eventDisease) {
