d8d78591f8121a5b8337c309b00d12d74140a4e2
syntakker <frank@boldsolutions.de>
2021-03-17 09:50:06 +0100

    remove unused import (#4050)

--- sormas-api/src/test/java/de/symeda/sormas/api/utils/CharsetHelperTest.java
+++ sormas-api/src/test/java/de/symeda/sormas/api/utils/CharsetHelperTest.java
@@ -22,7 +22,6 @@ import static org.junit.Assert.assertTrue;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
