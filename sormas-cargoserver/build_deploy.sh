5b1f14f025c83c4528f43ee00b4d4a0fe8be8314
Jonas Cirotzki <jonas.cirotzki@posteo.de>
2021-03-18 11:22:29 +0100

    [#4814] QOL script to build and deploy to cargo

---
+++ sormas-cargoserver/build_deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+mvn cargo:stop
+
+# recompile
+cd ../sormas-base || exit
+
+if [ "$1" = "clean" ]; then
+  cd ../sormas-cargoserver || exit
+  docker-compose down
+  docker volume rm sormas-cargoserver_psqldata_cargoserver
+  docker-compose up -d
+  cd ../sormas-base || exit
+  mvn clean install -DskipTests
+
+elif [ "$1" = "test" ]; then
+  mvn clean install
+
+elif [ "$1" = "fast" ]; then
+  mvn -T 1C install -DskipTests
+
+elif [ "$1" = "verify" ]; then
+  mvn verify -DskipTests
+
+else
+  mvn install -DskipTests
+fi
+
+# restart server
+cd ../sormas-cargoserver || exit
+docker-compose up -d
+mvn cargo:run  -Dcargo.glassfish.domain.debug=true
