4fc407876af0cd005b468a0dc8db18017a0bd655
Seal33 <klausbart@gmx.net>
2021-03-19 10:28:39 +0100

    Remove duplicate comment #4591

--- sormas-app/app/src/main/java/de/symeda/sormas/app/caze/edit/PreviousHospitalizationDialog.java
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/caze/edit/PreviousHospitalizationDialog.java
@@ -12,20 +12,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
-/*
- * SORMAS® - Surveillance Outbreak Response Management & Analysis System
- * Copyright © 2016-2018 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
 
 package de.symeda.sormas.app.caze.edit;
 
@@ -35,11 +21,9 @@ import java.util.List;
 
 import android.content.Context;
 import android.util.Log;
-
 import androidx.databinding.ViewDataBinding;
 import androidx.databinding.library.baseAdapters.BR;
 import androidx.fragment.app.FragmentActivity;
-
 import de.symeda.sormas.api.facility.FacilityType;
 import de.symeda.sormas.api.hospitalization.HospitalizationReasonType;
 import de.symeda.sormas.api.hospitalization.PreviousHospitalizationDto;
