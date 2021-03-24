9bd2cef03e2d71cba92987ff7274aad94f662d42
Simon Schweizer <simon-tobias.schweizer@hs-heilbronn.de>
2021-03-24 12:59:45 +0100

    updated java versions to 1.8 fixes typo

--- sormas-api/src/test/java/de/symeda/sormas/api/utils/DefaultUserHelperTest.java
+++ sormas-api/src/test/java/de/symeda/sormas/api/utils/DefaultUserHelperTest.java
@@ -18,33 +18,33 @@ public class DefaultUserHelperTest {
 
     private static final String DEFAULT_ADMIN_USERNAME = "admin";
     private static final String DEFAULT_ADMIN_PASS = "sadmin";
-    private static final String DEFAUTL_SURV_SUP_USER_PASS = "SurvSup";
-    private static final String DEFAUTL_CASE_SUP_USER_PASS = "CaseSup";
-    private static final String DEFAUTL_CONT_SUP_USER_PASS = "ContSup";
-    private static final String DEFAUTL_POE_SUP_USER_PASS = "PoeSup";
-    private static final String DEFAUTL_LAB_OFF_USER_PASS = "LabOff";
-    private static final String DEFAUTL_EVE_OFF_USER_PASS = "EveOff";
-    private static final String DEFAUTL_NAT_USER_USER_PASS = "NatUser";
-    private static final String DEFAUTL_NAT_CLIN_USER_PASS = "NatClin";
-    private static final String DEFAUTL_SURV_OFF_USER_PASS = "SurvOff";
-    private static final String DEFAUTL_HOSP_INF_USER_PASS = "HospInf";
-    private static final String DEFAUTL_POE_INF_USER_PASS = "PoeInf";
+    private static final String DEFAULT_SURV_SUP_USER_PASS = "SurvSup";
+    private static final String DEFAULT_CASE_SUP_USER_PASS = "CaseSup";
+    private static final String DEFAULT_CONT_SUP_USER_PASS = "ContSup";
+    private static final String DEFAULT_POE_SUP_USER_PASS = "PoeSup";
+    private static final String DEFAULT_LAB_OFF_USER_PASS = "LabOff";
+    private static final String DEFAULT_EVE_OFF_USER_PASS = "EveOff";
+    private static final String DEFAULT_NAT_USER_USER_PASS = "NatUser";
+    private static final String DEFAULT_NAT_CLIN_USER_PASS = "NatClin";
+    private static final String DEFAULT_SURV_OFF_USER_PASS = "SurvOff";
+    private static final String DEFAULT_HOSP_INF_USER_PASS = "HospInf";
+    private static final String DEFAULT_POE_INF_USER_PASS = "PoeInf";
 
     private static final int COUNT_OF_DEFAULT_ACCOUNTS = 12;
 
-    private static final Map<String, String> DEFAULT_USERS  = new HashMap<>() {{
+    private static final Map<String, String> DEFAULT_USERS  = new HashMap<String, String>() {{
         put(DEFAULT_ADMIN_USERNAME, DEFAULT_ADMIN_PASS);
-        put(DEFAUTL_SURV_SUP_USER_PASS, DEFAUTL_SURV_SUP_USER_PASS);
-        put(DEFAUTL_CASE_SUP_USER_PASS, DEFAUTL_CASE_SUP_USER_PASS);
-        put(DEFAUTL_CONT_SUP_USER_PASS, DEFAUTL_CONT_SUP_USER_PASS);
-        put(DEFAUTL_POE_SUP_USER_PASS, DEFAUTL_POE_SUP_USER_PASS);
-        put(DEFAUTL_LAB_OFF_USER_PASS, DEFAUTL_LAB_OFF_USER_PASS);
-        put(DEFAUTL_EVE_OFF_USER_PASS, DEFAUTL_EVE_OFF_USER_PASS);
-        put(DEFAUTL_NAT_USER_USER_PASS, DEFAUTL_NAT_USER_USER_PASS);
-        put(DEFAUTL_NAT_CLIN_USER_PASS, DEFAUTL_NAT_CLIN_USER_PASS);
-        put(DEFAUTL_SURV_OFF_USER_PASS, DEFAUTL_SURV_OFF_USER_PASS);
-        put(DEFAUTL_HOSP_INF_USER_PASS, DEFAUTL_HOSP_INF_USER_PASS);
-        put(DEFAUTL_POE_INF_USER_PASS, DEFAUTL_POE_INF_USER_PASS);
+        put(DEFAULT_SURV_SUP_USER_PASS, DEFAULT_SURV_SUP_USER_PASS);
+        put(DEFAULT_CASE_SUP_USER_PASS, DEFAULT_CASE_SUP_USER_PASS);
+        put(DEFAULT_CONT_SUP_USER_PASS, DEFAULT_CONT_SUP_USER_PASS);
+        put(DEFAULT_POE_SUP_USER_PASS, DEFAULT_POE_SUP_USER_PASS);
+        put(DEFAULT_LAB_OFF_USER_PASS, DEFAULT_LAB_OFF_USER_PASS);
+        put(DEFAULT_EVE_OFF_USER_PASS, DEFAULT_EVE_OFF_USER_PASS);
+        put(DEFAULT_NAT_USER_USER_PASS, DEFAULT_NAT_USER_USER_PASS);
+        put(DEFAULT_NAT_CLIN_USER_PASS, DEFAULT_NAT_CLIN_USER_PASS);
+        put(DEFAULT_SURV_OFF_USER_PASS, DEFAULT_SURV_OFF_USER_PASS);
+        put(DEFAULT_HOSP_INF_USER_PASS, DEFAULT_HOSP_INF_USER_PASS);
+        put(DEFAULT_POE_INF_USER_PASS, DEFAULT_POE_INF_USER_PASS);
     }};
 
     @Test
