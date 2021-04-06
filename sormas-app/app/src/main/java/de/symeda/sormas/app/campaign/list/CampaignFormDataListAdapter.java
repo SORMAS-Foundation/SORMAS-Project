5adbb7057a146f44ec970fd3e050c6bebcc0979d
Alexandru Caruntu <alexandru.caruntu.ext@vitagroup.ag>
2021-04-06 23:41:26 +0300

    #3161 - Fixed a type

--- sormas-app/app/src/main/java/de/symeda/sormas/app/campaign/list/CampaignFormDataListAdapter.java
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/campaign/list/CampaignFormDataListAdapter.java
@@ -1,11 +1,11 @@
 package de.symeda.sormas.app.campaign.list;
 
 import de.symeda.sormas.app.R;
-import de.symeda.sormas.app.backend.caze.Case;
+import de.symeda.sormas.app.backend.campaign.data.CampaignFormData;
 import de.symeda.sormas.app.core.adapter.databinding.BindingPagedListAdapter;
 import de.symeda.sormas.app.databinding.RowCaseListItemLayoutBinding;
 
-public class CampaignFormDataListAdapter extends BindingPagedListAdapter<Case, RowCaseListItemLayoutBinding> {
+public class CampaignFormDataListAdapter extends BindingPagedListAdapter<CampaignFormData, RowCaseListItemLayoutBinding> {
     public CampaignFormDataListAdapter() {
         super(R.layout.row_campaign_list_item_layout);
     }
