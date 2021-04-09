90355e65373399dd7800ab08ec6ea771ee77cbbb
barnabartha <barnabartha@she.net>
2021-04-09 13:42:10 +0300

    #2001 - campaign form data read fragment with dynamic fields

--- sormas-app/app/src/main/java/de/symeda/sormas/app/campaign/read/CampaignFormDataReadFragment.java
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/campaign/read/CampaignFormDataReadFragment.java
@@ -18,22 +18,29 @@
 
 package de.symeda.sormas.app.campaign.read;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
-
-import androidx.annotation.Nullable;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import de.symeda.sormas.api.campaign.data.CampaignFormDataEntry;
+import de.symeda.sormas.api.campaign.form.CampaignFormElement;
+import de.symeda.sormas.api.campaign.form.CampaignFormElementType;
 import de.symeda.sormas.app.BaseReadFragment;
 import de.symeda.sormas.app.R;
 import de.symeda.sormas.app.backend.campaign.data.CampaignFormData;
 import de.symeda.sormas.app.backend.campaign.form.CampaignFormMeta;
 import de.symeda.sormas.app.backend.common.DatabaseHelper;
+import de.symeda.sormas.app.component.controls.ControlPropertyField;
 import de.symeda.sormas.app.component.controls.ControlTextReadField;
 import de.symeda.sormas.app.databinding.FragmentCampaignDataReadLayoutBinding;
 
@@ -47,8 +54,72 @@ public class CampaignFormDataReadFragment extends BaseReadFragment<FragmentCampa
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+
+        final LinearLayout dynamicLayout = view.findViewById(R.id.dynamicLayout);
+
+        final CampaignFormMeta campaignFormMeta = DatabaseHelper.getCampaignFormMetaDao().queryForId(record.getCampaignFormMeta().getId());
+        final List<CampaignFormDataEntry> formValues = record.getFormValues();
+        final Map<String, String> formValuesMap = new HashMap<>();
+        formValues.forEach(campaignFormDataEntry -> formValuesMap.put(campaignFormDataEntry.getId(), campaignFormDataEntry.getValue().toString()));
+
+        for (CampaignFormElement campaignFormElement : campaignFormMeta.getCampaignFormElements()) {
+            CampaignFormElementType type = CampaignFormElementType.fromString(campaignFormElement.getType());
+
+            if (type != CampaignFormElementType.SECTION && type != CampaignFormElementType.LABEL) {
+                String value = formValuesMap.get(campaignFormElement.getId());
+                if (value != null) {
+                    ControlPropertyField dynamicField = createControlTextReadField(campaignFormElement);
+                    dynamicField.setShowCaption(true);
+                    if (type == CampaignFormElementType.YES_NO) {
+                        ControlTextReadField.setValue((ControlTextReadField) dynamicField, Boolean.valueOf(value), null, null);
+                    } else {
+                        ControlTextReadField.setValue((ControlTextReadField) dynamicField, value, null, null, null);
+                    }
+                    dynamicLayout.addView(dynamicField, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                } else {
+                    Log.e(getClass().getName(), "No form value for element id : " + campaignFormElement.getId());
+                }
+            }
+        }
+        return view;
+    }
+
+    private ControlTextReadField createControlTextReadField(CampaignFormElement campaignFormElement) {
+        return new ControlTextReadField(requireContext()) {
+            @Override
+            protected String getPrefixDescription() {
+                return campaignFormElement.getCaption();
+            }
+
+            @Override
+            protected String getPrefixCaption() {
+                return campaignFormElement.getCaption();
+            }
+
+            @Override
+            public int getTextAlignment() {
+                return View.TEXT_ALIGNMENT_VIEW_START;
+            }
+
+            @Override
+            public int getGravity() {
+                return Gravity.CENTER_VERTICAL;
+            }
+
+            @Override
+            public int getMaxLines() {
+                return 1;
+            }
+
+            @Override
+            protected void inflateView(Context context, AttributeSet attrs, int defStyle) {
+                super.inflateView(context, attrs, defStyle);
+                initLabel();
+                initTextView();
+            }
+        };
     }
 
     @Override
@@ -59,20 +130,6 @@ public class CampaignFormDataReadFragment extends BaseReadFragment<FragmentCampa
     @Override
     protected void onLayoutBinding(FragmentCampaignDataReadLayoutBinding contentBinding) {
         contentBinding.setData(record);
-
-        final View view = getRootView();
-        if (view != null) {
-            final LinearLayout dynamicLayout = view.findViewById(R.id.dynamicLayout);
-            final CampaignFormMeta campaignFormMeta = DatabaseHelper.getCampaignFormMetaDao().queryForId(record.getCampaignFormMeta().getId());
-            final List<CampaignFormDataEntry> formValues = record.getFormValues();
-            final Map<String, Object> formValuesMap = new HashMap<>();
-            formValues.forEach(campaignFormDataEntry -> formValuesMap.put(campaignFormDataEntry.getId(), campaignFormDataEntry.getValue()));
-            campaignFormMeta.getCampaignFormElements().forEach(campaignFormElement -> {
-                ControlTextReadField controlTextReadField = new ControlTextReadField(getActivity());
-                controlTextReadField.setValue(formValuesMap.get(campaignFormElement.getId()));
-                dynamicLayout.addView(controlTextReadField);
-            });
-        }
     }
 
     @Override
