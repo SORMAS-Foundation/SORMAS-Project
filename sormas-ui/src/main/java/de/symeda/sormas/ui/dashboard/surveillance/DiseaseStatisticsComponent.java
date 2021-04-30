3da025faa68c05d8bedea8e08cb62d78310ce395
Alexandru Caruntu <alexandru.caruntu.ext@vitagroup.ag>
2021-04-30 14:18:36 +0300

    #3004 - Created disease summary component

--- sormas-ui/src/main/java/de/symeda/sormas/ui/dashboard/surveillance/DiseaseStatisticsComponent.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/dashboard/surveillance/DiseaseStatisticsComponent.java
@@ -19,19 +19,11 @@ package de.symeda.sormas.ui.dashboard.surveillance;
 
 import com.vaadin.ui.CustomLayout;
 
-import de.symeda.sormas.api.FacadeProvider;
-import de.symeda.sormas.api.feature.FeatureType;
-import de.symeda.sormas.api.i18n.I18nProperties;
-import de.symeda.sormas.api.i18n.Strings;
-import de.symeda.sormas.api.utils.DataHelper;
 import de.symeda.sormas.ui.dashboard.DashboardDataProvider;
-import de.symeda.sormas.ui.dashboard.statistics.DashboardStatisticsSubComponent;
 import de.symeda.sormas.ui.dashboard.surveillance.components.statistics.CaseStatisticsComponent;
 import de.symeda.sormas.ui.dashboard.surveillance.components.statistics.EventStatisticsComponent;
 import de.symeda.sormas.ui.dashboard.surveillance.components.statistics.TestResultsStatisticsComponent;
-import de.symeda.sormas.ui.dashboard.surveillance.components.statistics.summary.DiseaseSummaryElementComponent;
-import de.symeda.sormas.ui.dashboard.surveillance.components.statistics.summary.FatalitiesSummaryElementComponent;
-import de.symeda.sormas.ui.utils.CssStyles;
+import de.symeda.sormas.ui.dashboard.surveillance.components.statistics.summary.DiseaseSummaryComponent;
 import de.symeda.sormas.ui.utils.LayoutUtil;
 
 public class DiseaseStatisticsComponent extends CustomLayout {
@@ -41,21 +33,7 @@ public class DiseaseStatisticsComponent extends CustomLayout {
 	private final DashboardDataProvider dashboardDataProvider;
 
 	private final CaseStatisticsComponent caseStatisticsComponent;
-
-	// "Outbreak Districts" elements
-	private DiseaseSummaryElementComponent lastReportedDistrict;
-	private DiseaseSummaryElementComponent outbreakDistrictCount;
-
-	//"cases in quarantine" elements 
-	private DiseaseSummaryElementComponent casesInQuarantineByDate;
-	private DiseaseSummaryElementComponent casesPlacedInQuarantineByDate;
-
-	// "Contacts converted to cases"
-	private DiseaseSummaryElementComponent contactsConvertedToCase;
-
-	// "Case Fatality" elements
-	private FatalitiesSummaryElementComponent fatalitiesSummaryElementComponent;
-
+	private final DiseaseSummaryComponent diseaseSummaryComponent;
 	private final EventStatisticsComponent eventStatisticsComponent;
 	private final TestResultsStatisticsComponent testResultsStatisticsComponent;
 
@@ -75,84 +53,20 @@ public class DiseaseStatisticsComponent extends CustomLayout {
 				LayoutUtil.fluidColumn(6, 0, 12, 0, LayoutUtil.fluidRowLocs(EVENT_LOC, SAMPLE_LOC))));
 
 		caseStatisticsComponent = new CaseStatisticsComponent();
+		diseaseSummaryComponent = new DiseaseSummaryComponent();
 		eventStatisticsComponent = new EventStatisticsComponent();
 		testResultsStatisticsComponent = new TestResultsStatisticsComponent();
 
 		addComponent(caseStatisticsComponent, CASE_LOC);
-		addComponent(createOutbreakDistrictAndCaseFatalityLayout(), OUTBREAK_LOC);
+		addComponent(diseaseSummaryComponent, OUTBREAK_LOC);
 		addComponent(eventStatisticsComponent, EVENT_LOC);
 		addComponent(testResultsStatisticsComponent, SAMPLE_LOC);
 	}
 
-	private DashboardStatisticsSubComponent createOutbreakDistrictAndCaseFatalityLayout() {
-		DashboardStatisticsSubComponent layout = new DashboardStatisticsSubComponent();
-
-		fatalitiesSummaryElementComponent = new FatalitiesSummaryElementComponent();
-		layout.addComponent(fatalitiesSummaryElementComponent);
-
-		lastReportedDistrict =
-			new DiseaseSummaryElementComponent(Strings.headingLastReportedDistrict, I18nProperties.getString(Strings.none).toUpperCase());
-		layout.addComponent(lastReportedDistrict);
-
-		if (FacadeProvider.getFeatureConfigurationFacade().isFeatureEnabled(FeatureType.OUTBREAKS)) {
-			outbreakDistrictCount = new DiseaseSummaryElementComponent(Strings.headingOutbreakDistricts);
-			layout.addComponent(outbreakDistrictCount);
-		}
-
-		casesInQuarantineByDate = new DiseaseSummaryElementComponent(Strings.headingCasesInQuarantine);
-		layout.addComponent(casesInQuarantineByDate);
-
-		casesPlacedInQuarantineByDate = new DiseaseSummaryElementComponent(Strings.headingCasesPlacedInQuarantine);
-		layout.addComponent(casesPlacedInQuarantineByDate);
-
-		contactsConvertedToCase = new DiseaseSummaryElementComponent(Strings.headingContactsConvertedToCase);
-		layout.addComponent(contactsConvertedToCase);
-
-		layout.addStyleName(CssStyles.VSPACE_TOP_4);
-
-		return layout;
-	}
-
 	public void refresh() {
-		updateCaseComponent();
-		updateCaseFatalityComponent();
-		updateLastReportedDistrictComponent();
-		if (FacadeProvider.getFeatureConfigurationFacade().isFeatureEnabled(FeatureType.OUTBREAKS)) {
-			updateOutbreakDistrictComponent();
-		}
-		updateEventComponent();
-		updateTestResultComponent();
-		updateCasesInQuarantineData();
-	}
-
-	private void updateCaseComponent() {
 		caseStatisticsComponent.update(dashboardDataProvider.getCases());
-	}
-
-	private void updateCaseFatalityComponent() {
-		fatalitiesSummaryElementComponent.update(dashboardDataProvider.getCases(), dashboardDataProvider.getPreviousCases());
-	}
-
-	private void updateLastReportedDistrictComponent() {
-		String district = dashboardDataProvider.getLastReportedDistrict();
-		lastReportedDistrict.updateTotalLabel(DataHelper.isNullOrEmpty(district) ? I18nProperties.getString(Strings.none).toUpperCase() : district);
-	}
-
-	private void updateOutbreakDistrictComponent() {
-		outbreakDistrictCount.updateTotalLabel(dashboardDataProvider.getOutbreakDistrictCount().toString());
-	}
-
-	private void updateEventComponent() {
+		diseaseSummaryComponent.update(dashboardDataProvider);
 		eventStatisticsComponent.update(dashboardDataProvider.getEventCountByStatus());
-	}
-
-	private void updateTestResultComponent() {
 		testResultsStatisticsComponent.update(dashboardDataProvider.getTestResultCountByResultType());
-	}
-
-	private void updateCasesInQuarantineData() {
-		casesInQuarantineByDate.updateTotalLabel(dashboardDataProvider.getCasesInQuarantineCount().toString());
-		casesPlacedInQuarantineByDate.updateTotalLabel(dashboardDataProvider.getCasesPlacedInQuarantineCount().toString());
-		contactsConvertedToCase.updateTotalLabel(dashboardDataProvider.getContactsConvertedToCaseCount().toString());
 	}
 }
