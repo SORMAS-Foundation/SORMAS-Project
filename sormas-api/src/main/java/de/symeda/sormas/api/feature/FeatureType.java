6a3ebd55d723ed7188a0b3caab2e358924e5c6dd
Aurélien PONÇON <apo@atolcd.com>
2021-05-12 11:00:21 +0200

    Fix of the VIEW_TABS feature types #4733

--- sormas-api/src/main/java/de/symeda/sormas/api/feature/FeatureType.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/feature/FeatureType.java
@@ -64,14 +64,38 @@ public enum FeatureType {
 			CASE_SURVEILANCE }),
 
 	// SHOW/HIDE VIEW TAB FEATURES
-	VIEW_TAB_CASES_HOSPITALIZATION(false, true, null),
-	VIEW_TAB_CASES_SYMPTOMS(false, true, null),
-	VIEW_TAB_CASES_EPIDEMIOLOGICAL_DATA(false, true, null),
-	VIEW_TAB_CASES_THERAPY(false, true, null),
-	VIEW_TAB_CASES_FOLLOW_UP(false, true, null),
-	VIEW_TAB_CASES_CLINICAL_COURSE(false, true, null),
-	VIEW_TAB_CONTACTS_EPIDEMIOLOGICAL_DATA(false, true, null),
-	VIEW_TAB_CONTACTS_FOLLOW_UP_VISITS(false, true, null),
+	VIEW_TAB_CASES_HOSPITALIZATION(true,
+		true,
+		new FeatureType[] {
+			CASE_SURVEILANCE }),
+	VIEW_TAB_CASES_SYMPTOMS(true,
+		true,
+		new FeatureType[] {
+			CASE_SURVEILANCE }),
+	VIEW_TAB_CASES_EPIDEMIOLOGICAL_DATA(true,
+		true,
+		new FeatureType[] {
+			CASE_SURVEILANCE }),
+	VIEW_TAB_CASES_THERAPY(true,
+		true,
+		new FeatureType[] {
+			CASE_SURVEILANCE }),
+	VIEW_TAB_CASES_FOLLOW_UP(true,
+		true,
+		new FeatureType[] {
+			CASE_SURVEILANCE }),
+	VIEW_TAB_CASES_CLINICAL_COURSE(true,
+		true,
+		new FeatureType[] {
+			CASE_SURVEILANCE }),
+	VIEW_TAB_CONTACTS_EPIDEMIOLOGICAL_DATA(true,
+		true,
+		new FeatureType[] {
+			CONTACT_TRACING }),
+	VIEW_TAB_CONTACTS_FOLLOW_UP_VISITS(true,
+		true,
+		new FeatureType[] {
+			CONTACT_TRACING }),
 
 	// ADDITIONAL FEATURES
 	GDPR_CONSENT_POPUP(true, false, null),
