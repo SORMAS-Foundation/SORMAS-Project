28c6200ab201b7c384a56d439e38d38914e3717f
Alexandru Pirvu <pirvu.alexandru@outlook.com>
2021-05-19 10:30:21 +0300

    #5233 Save the person in a separate transaction

--- sormas-backend/src/main/java/de/symeda/sormas/backend/person/PersonFacadeEjb.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/person/PersonFacadeEjb.java
@@ -34,6 +34,8 @@ import java.util.stream.Stream;
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
@@ -389,6 +391,7 @@ public class PersonFacadeEjb implements PersonFacade {
 		return convertToDto(person, Pseudonymizer.getDefault(userService::hasRight), existingPerson == null || isPersonInJurisdiction(person));
 	}
 
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
 	public PersonDto savePerson(PersonDto source) throws ValidationRuntimeException {
 		Person person = personService.getByUuid(source.getUuid());
 
