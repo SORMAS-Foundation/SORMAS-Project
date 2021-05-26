1fc49ef9bda7cbca8e30ae23cb984109874ab96e
syntakker <>
2021-05-27 01:18:41 +0200

    LBDS send person to SORMAS (#3522)

---
+++ sormas-app/app/src/main/java/org/hzi/sormas/lbds/messaging/LbdsHelper.java
@@ -0,0 +1,37 @@
+package org.hzi.sormas.lbds.messaging;
+
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import android.content.Intent;
+import android.util.Base64;
+
+public class LbdsHelper {
+
+	public static LbdsPropagateKexToSormasIntent getLbdsPropagateKexToSormasIntent(Intent intent) {
+		return new LbdsPropagateKexToSormasIntent(intent);
+	}
+
+	public static String getX509FromPublicKey(PublicKey publicKey) {
+		return Base64.encodeToString(publicKey.getEncoded(), Base64.DEFAULT);
+	}
+
+	public static PublicKey getPublicKeyFromX509(String publicKeyString) throws NoSuchAlgorithmException, InvalidKeySpecException {
+		X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(Base64.decode(publicKeyString, Base64.DEFAULT));
+		return KeyFactory.getInstance("RSA").generatePublic(x509EncodedKeySpec);
+	}
+
+	public static String getPKCS8FromPrivetKey(PrivateKey privateKey) {
+		return Base64.encodeToString(privateKey.getEncoded(), Base64.DEFAULT);
+	}
+
+	public static PrivateKey getPrivateKeyFromPKCS8(String privateKeyString) throws NoSuchAlgorithmException, InvalidKeySpecException {
+		PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(Base64.decode(privateKeyString, Base64.DEFAULT));
+		return KeyFactory.getInstance("RSA").generatePrivate(pkcs8EncodedKeySpec);
+	}
+}
