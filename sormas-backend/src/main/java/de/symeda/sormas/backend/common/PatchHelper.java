68aa63394eca35d81495962e0e43d43025d17c99
marius <dmarius23@gmail.com>
2021-06-04 15:48:54 +0300

    #4956 - update naming and add comments

--- sormas-backend/src/main/java/de/symeda/sormas/backend/common/PatchHelper.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/common/PatchHelper.java
@@ -13,11 +13,24 @@ import net.minidev.json.JSONObject;
 
 public class PatchHelper {
 
-	public static <T> T postUpdate(JSONObject jsonObject, Class<T> patchObjectClass, T existingObject) {
+	/**
+	 * This method updates the existingObject with the values received in the jsonObject
+	 * 
+	 * @param jsonObject
+	 *            - the jsonObject contains the values that are intended to be updated,
+	 *            representing a subset of the fields of the object that is intended to be updated
+	 * @param updatingObjectClass
+	 *            - the class of the object that is intended to be updated
+	 * @param existingObject
+	 *            - the existingObject that is intended to be updated
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> T postUpdate(JSONObject jsonObject, Class<T> updatingObjectClass, T existingObject) {
 		ObjectMapper mapper = new ObjectMapper();
 
 		try {
-			T patchObject = mapper.readValue(jsonObject.toString(), patchObjectClass);
+			T patchObject = mapper.readValue(jsonObject.toString(), updatingObjectClass);
 
 			Set<String> attributes = jsonObject.keySet();
 
@@ -38,7 +51,7 @@ public class PatchHelper {
 					Logger logger = LoggerFactory.getLogger(patchObject.getClass());
 					logger.error(
 						String.format(
-							"Failed to patch " + patchObject.getClass().getSimpleName() + " attribute name " + attribute + ". Error: "
+							"Failed to update " + patchObject.getClass().getSimpleName() + " attribute name " + attribute + ". Error: "
 								+ e.getMessage()));
 				}
 
