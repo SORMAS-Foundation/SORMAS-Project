a5e69114a2495c4c3fc0a14760896be7dd945071
Alexandru Caruntu <alexandru.caruntu.ext@vitagroup.ag>
2021-07-07 18:48:33 +0300

    #6023 - Main menu entry and page skeleton for Travel entries

---
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/travelentry/TravelEntriesView.java
@@ -0,0 +1,19 @@
+package de.symeda.sormas.ui.travelentry;
+
+import com.vaadin.navigator.ViewChangeListener;
+
+import de.symeda.sormas.ui.utils.AbstractView;
+
+public class TravelEntriesView extends AbstractView {
+
+	public static final String VIEW_NAME = "entries";
+
+	public TravelEntriesView() {
+		super(VIEW_NAME);
+	}
+
+	@Override
+	public void enter(ViewChangeListener.ViewChangeEvent event) {
+
+	}
+}
---
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/travelentry/TravelEntryController.java
@@ -0,0 +1,10 @@
+package de.symeda.sormas.ui.travelentry;
+
+import com.vaadin.navigator.Navigator;
+
+public class TravelEntryController {
+
+	public void registerViews(Navigator navigator) {
+
+	}
+}
