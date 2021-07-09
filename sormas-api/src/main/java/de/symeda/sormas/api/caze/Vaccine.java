debd8e91b97c6bf83e8017b429ebf40f3f49ca72
Maté Strysewske <mate.strysewske@vitagroup.ag>
2021-07-09 10:21:03 +0200

    #6100 - Added combination vaccines for Covid-19

--- sormas-api/src/main/java/de/symeda/sormas/api/caze/Vaccine.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/caze/Vaccine.java
@@ -18,6 +18,7 @@ package de.symeda.sormas.api.caze;
 import javax.annotation.Nullable;
 
 import de.symeda.sormas.api.i18n.I18nProperties;
+import de.symeda.sormas.api.utils.HideForCountriesExcept;
 
 public enum Vaccine {
 
@@ -27,6 +28,10 @@ public enum Vaccine {
 	AD26_COV2_S(VaccineManufacturer.JOHNSON_JOHNSON),
 	NVX_COV_2373(VaccineManufacturer.NOVAVAX),
 	SANOFI_GSK(VaccineManufacturer.SANOFI_GSK),
+	@HideForCountriesExcept
+	ASTRA_ZENECA_COMIRNATY(VaccineManufacturer.ASTRA_ZENECA_BIONTECH_PFIZER),
+	@HideForCountriesExcept
+	ASTRA_ZENECA_MRNA_1273(VaccineManufacturer.ASTRA_ZENECA_MODERNA),
 	UNKNOWN,
 	OTHER;
 
