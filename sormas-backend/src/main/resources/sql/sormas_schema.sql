1fa444cf98839f76078680fb8ece80d284e5093b
Thomas Broyer <tbr@atolcd.com>
2021-07-13 18:17:02 +0200

    Fix schema version

--- sormas-backend/src/main/resources/sql/sormas_schema.sql
+++ sormas-backend/src/main/resources/sql/sormas_schema.sql
@@ -7611,5 +7611,5 @@ INSERT INTO schema_version (version_number, comment) VALUES (383, 'Add disease v
 ALTER TABLE events ADD COLUMN eventidentificationsource varchar(255);
 ALTER TABLE events_history ADD COLUMN eventidentificationsource varchar(255);
 
-INSERT INTO schema_version (version_number, comment) VALUES (383, 'Event identification source (#5526)');
+INSERT INTO schema_version (version_number, comment) VALUES (384, 'Event identification source (#5526)');
 -- *** Insert new sql commands BEFORE this line ***
