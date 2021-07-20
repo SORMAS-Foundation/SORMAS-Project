eaf2f4514ec2c57d4264def3e71998ac22a44246
syntakker <frank@boldsolutions.de>
2021-07-20 05:54:38 +0200

    show resulting case info (#6026)

--- sormas-ui/src/main/java/de/symeda/sormas/ui/caze/CaseInfoLayout.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/caze/CaseInfoLayout.java
@@ -1,20 +1,18 @@
-/*******************************************************************************
+/*
  * SORMAS® - Surveillance Outbreak Response Management & Analysis System
- * Copyright © 2016-2018 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
- *
+ * Copyright © 2016-2021 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
- *******************************************************************************/
+ */
+
 package de.symeda.sormas.ui.caze;
 
 import com.vaadin.ui.HorizontalLayout;
@@ -41,11 +39,17 @@ import de.symeda.sormas.ui.utils.DateFormatHelper;
 public class CaseInfoLayout extends AbstractInfoLayout<CaseDataDto> {
 
 	private final CaseDataDto caseDto;
+	private final boolean showDisease;
 
 	public CaseInfoLayout(CaseDataDto caseDto) {
+		this(caseDto, true);
+	}
+
+	public CaseInfoLayout(CaseDataDto caseDto, boolean showDisease) {
 		super(CaseDataDto.class, UiFieldAccessCheckers.getDefault(caseDto.isPseudonymized()));
 
 		this.caseDto = caseDto;
+		this.showDisease = showDisease;
 		setSpacing(true);
 		setMargin(false);
 		setWidth(100, Unit.PERCENTAGE);
@@ -77,10 +81,10 @@ public class CaseInfoLayout extends AbstractInfoLayout<CaseDataDto> {
 					caseDto.getExternalID(),
 					I18nProperties.getPrefixCaption(CaseDataDto.I18N_PREFIX, CaseDataDto.EXTERNAL_ID)).setDescription(caseDto.getEpidNumber());
 				addDescLabel(
-						leftColumnLayout,
-						CaseDataDto.EXTERNAL_TOKEN,
-						caseDto.getExternalToken(),
-						I18nProperties.getPrefixCaption(CaseDataDto.I18N_PREFIX, CaseDataDto.EXTERNAL_TOKEN)).setDescription(caseDto.getExternalToken());
+					leftColumnLayout,
+					CaseDataDto.EXTERNAL_TOKEN,
+					caseDto.getExternalToken(),
+					I18nProperties.getPrefixCaption(CaseDataDto.I18N_PREFIX, CaseDataDto.EXTERNAL_TOKEN)).setDescription(caseDto.getExternalToken());
 			} else {
 				addDescLabel(
 					leftColumnLayout,
@@ -128,13 +132,17 @@ public class CaseInfoLayout extends AbstractInfoLayout<CaseDataDto> {
 		rightColumnLayout.setMargin(false);
 		rightColumnLayout.setSpacing(true);
 		{
-			addDescLabel(
-				rightColumnLayout,
-				CaseDataDto.DISEASE,
-				caseDto.getDisease() != Disease.OTHER
-					? DiseaseHelper.toString(caseDto.getDisease(), caseDto.getDiseaseDetails(), caseDto.getDiseaseVariant())
-					: DataHelper.toStringNullable(caseDto.getDiseaseDetails()),
-				I18nProperties.getPrefixCaption(CaseDataDto.I18N_PREFIX, CaseDataDto.DISEASE));
+			if (showDisease) {
+				addDescLabel(
+					rightColumnLayout,
+					CaseDataDto.DISEASE,
+					caseDto.getDisease() != Disease.OTHER
+						? DiseaseHelper.toString(caseDto.getDisease(), caseDto.getDiseaseDetails(), caseDto.getDiseaseVariant())
+						: DataHelper.toStringNullable(caseDto.getDiseaseDetails()),
+					I18nProperties.getPrefixCaption(CaseDataDto.I18N_PREFIX, CaseDataDto.DISEASE));
+			} else {
+				addDescLabel(rightColumnLayout, CaseDataDto.DISEASE, "", "");
+			}
 
 			if (UserProvider.getCurrent().hasUserRight(UserRight.CASE_VIEW)) {
 				addDescLabel(
--- sormas-ui/src/main/java/de/symeda/sormas/ui/travelentry/TravelEntryDataView.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/travelentry/TravelEntryDataView.java
@@ -3,8 +3,11 @@ package de.symeda.sormas.ui.travelentry;
 import com.vaadin.ui.CustomLayout;
 
 import de.symeda.sormas.api.FacadeProvider;
+import de.symeda.sormas.api.caze.CaseDataDto;
+import de.symeda.sormas.api.caze.CaseReferenceDto;
 import de.symeda.sormas.api.travelentry.TravelEntryDto;
 import de.symeda.sormas.ui.ControllerProvider;
+import de.symeda.sormas.ui.caze.CaseInfoLayout;
 import de.symeda.sormas.ui.utils.CommitDiscardWrapperComponent;
 import de.symeda.sormas.ui.utils.CssStyles;
 import de.symeda.sormas.ui.utils.DetailSubComponentWrapper;
@@ -15,6 +18,7 @@ public class TravelEntryDataView extends AbstractTravelEntryView {
 
 	public static final String VIEW_NAME = ROOT_VIEW_NAME + "/data";
 	public static final String TRAVEL_ENTRY_LOC = "travelEntry";
+	public static final String CASE_LOC = "case";
 
 	private CommitDiscardWrapperComponent<TravelEntryDataForm> editComponent;
 
@@ -31,7 +35,8 @@ public class TravelEntryDataView extends AbstractTravelEntryView {
 	protected void initView(String params) {
 		setHeightUndefined();
 
-		String htmlLayout = LayoutUtil.fluidRow(LayoutUtil.fluidColumnLoc(8, 0, 12, 0, TRAVEL_ENTRY_LOC));
+		String htmlLayout =
+			LayoutUtil.fluidRow(LayoutUtil.fluidColumnLoc(8, 0, 12, 0, TRAVEL_ENTRY_LOC), LayoutUtil.fluidColumnLoc(4, 0, 6, 0, CASE_LOC));
 
 		DetailSubComponentWrapper container = new DetailSubComponentWrapper(() -> editComponent);
 		container.setWidth(100, Unit.PERCENTAGE);
@@ -54,6 +59,20 @@ public class TravelEntryDataView extends AbstractTravelEntryView {
 		editComponent.addStyleName(CssStyles.MAIN_COMPONENT);
 		layout.addComponent(editComponent, TRAVEL_ENTRY_LOC);
 
+		CaseReferenceDto resultingCase = travelEntryDto.getResultingCase();
+		if (resultingCase != null) {
+			layout.addComponent(createCaseInfoLayout(resultingCase.getUuid()), CASE_LOC);
+		}
+
 		setTravelEntryEditPermission(container);
+	}
+
+	private CaseInfoLayout createCaseInfoLayout(String caseUuid) {
+
+		CaseDataDto caseDto = FacadeProvider.getCaseFacade().getCaseDataByUuid(caseUuid);
+		CaseInfoLayout caseInfoLayout = new CaseInfoLayout(caseDto, false);
+		caseInfoLayout.addStyleName(CssStyles.SIDE_COMPONENT);
+
+		return caseInfoLayout;
 	}
 }
