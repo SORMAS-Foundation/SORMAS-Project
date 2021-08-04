c842a8293d1ac6ae4a55151395b288af3ef1d032
Alexandru Caruntu <alexandru.caruntu.ext@vitagroup.ag>
2021-08-04 23:26:45 +0300

    #4764 - Added facility fields to ImmunizationCriteria

--- sormas-api/src/main/java/de/symeda/sormas/api/immunization/ImmunizationCriteria.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/immunization/ImmunizationCriteria.java
@@ -18,6 +18,9 @@ package de.symeda.sormas.api.immunization;
 import java.io.Serializable;
 
 import de.symeda.sormas.api.Disease;
+import de.symeda.sormas.api.facility.FacilityReferenceDto;
+import de.symeda.sormas.api.facility.FacilityType;
+import de.symeda.sormas.api.facility.FacilityTypeGroup;
 import de.symeda.sormas.api.region.CommunityReferenceDto;
 import de.symeda.sormas.api.region.DistrictReferenceDto;
 import de.symeda.sormas.api.region.RegionReferenceDto;
@@ -30,24 +33,30 @@ public class ImmunizationCriteria extends BaseCriteria implements Serializable, 
 	public static final String BIRTHDATE_YYYY = "birthdateYYYY";
 	public static final String BIRTHDATE_MM = "birthdateMM";
 	public static final String BIRTHDATE_DD = "birthdateDD";
-	public static final String REGION = "region";
-	public static final String DISTRICT = "district";
-	public static final String COMMUNITY = "community";
 	public static final String MEANS_OF_IMMUNIZATION = "meansOfImmunization";
 	public static final String MANAGEMENT_STATUS = "managementStatus";
 	public static final String IMMUNIZATION_STATUS = "immunizationStatus";
+	public static final String REGION = "region";
+	public static final String DISTRICT = "district";
+	public static final String COMMUNITY = "community";
+	public static final String FACILITY_TYPE_GROUP = "facilityTypeGroup";
+	public static final String FACILITY_TYPE = "facilityType";
+	public static final String HEALTH_FACILITY = "healthFacility";
 
 	private Disease disease;
 	private String nameAddressPhoneEmailLike;
 	private Integer birthdateYYYY;
 	private Integer birthdateMM;
 	private Integer birthdateDD;
-	private RegionReferenceDto region;
-	private DistrictReferenceDto district;
-	private CommunityReferenceDto community;
 	private MeansOfImmunization meansOfImmunization;
 	private ImmunizationManagementStatus managementStatus;
 	private ImmunizationStatus immunizationStatus;
+	private RegionReferenceDto region;
+	private DistrictReferenceDto district;
+	private CommunityReferenceDto community;
+	private FacilityTypeGroup facilityTypeGroup;
+	private FacilityType facilityType;
+	private FacilityReferenceDto healthFacility;
 
 	public Disease getDisease() {
 		return disease;
@@ -89,6 +98,30 @@ public class ImmunizationCriteria extends BaseCriteria implements Serializable, 
 		this.birthdateDD = birthdateDD;
 	}
 
+	public MeansOfImmunization getMeansOfImmunization() {
+		return meansOfImmunization;
+	}
+
+	public void setMeansOfImmunization(MeansOfImmunization meansOfImmunization) {
+		this.meansOfImmunization = meansOfImmunization;
+	}
+
+	public ImmunizationManagementStatus getManagementStatus() {
+		return managementStatus;
+	}
+
+	public void setManagementStatus(ImmunizationManagementStatus managementStatus) {
+		this.managementStatus = managementStatus;
+	}
+
+	public ImmunizationStatus getImmunizationStatus() {
+		return immunizationStatus;
+	}
+
+	public void setImmunizationStatus(ImmunizationStatus immunizationStatus) {
+		this.immunizationStatus = immunizationStatus;
+	}
+
 	public RegionReferenceDto getRegion() {
 		return region;
 	}
@@ -113,27 +146,27 @@ public class ImmunizationCriteria extends BaseCriteria implements Serializable, 
 		this.community = community;
 	}
 
-	public MeansOfImmunization getMeansOfImmunization() {
-		return meansOfImmunization;
+	public FacilityTypeGroup getFacilityTypeGroup() {
+		return facilityTypeGroup;
 	}
 
-	public void setMeansOfImmunization(MeansOfImmunization meansOfImmunization) {
-		this.meansOfImmunization = meansOfImmunization;
+	public void setFacilityTypeGroup(FacilityTypeGroup facilityTypeGroup) {
+		this.facilityTypeGroup = facilityTypeGroup;
 	}
 
-	public ImmunizationManagementStatus getManagementStatus() {
-		return managementStatus;
+	public FacilityType getFacilityType() {
+		return facilityType;
 	}
 
-	public void setManagementStatus(ImmunizationManagementStatus managementStatus) {
-		this.managementStatus = managementStatus;
+	public void setFacilityType(FacilityType facilityType) {
+		this.facilityType = facilityType;
 	}
 
-	public ImmunizationStatus getImmunizationStatus() {
-		return immunizationStatus;
+	public FacilityReferenceDto getHealthFacility() {
+		return healthFacility;
 	}
 
-	public void setImmunizationStatus(ImmunizationStatus immunizationStatus) {
-		this.immunizationStatus = immunizationStatus;
+	public void setHealthFacility(FacilityReferenceDto healthFacility) {
+		this.healthFacility = healthFacility;
 	}
 }
