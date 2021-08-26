3d8ff6c62851541a2bcdb9061263171e458f847d
Alexandru Caruntu <alexandru.caruntu.ext@vitagroup.ag>
2021-08-26 16:05:17 +0300

    #5828 - Cleanup

--- sormas-ui/src/main/java/de/symeda/sormas/ui/immunization/immunizationlink/ImmunizationListComponent.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/immunization/immunizationlink/ImmunizationListComponent.java
@@ -1,36 +1,22 @@
 package de.symeda.sormas.ui.immunization.immunizationlink;
 
-import com.vaadin.icons.VaadinIcons;
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.CustomLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.themes.ValoTheme;
 
-import de.symeda.sormas.api.caze.CaseReferenceDto;
-import de.symeda.sormas.api.i18n.Captions;
 import de.symeda.sormas.api.i18n.I18nProperties;
 import de.symeda.sormas.api.i18n.Strings;
 import de.symeda.sormas.api.immunization.ImmunizationCriteria;
 import de.symeda.sormas.api.person.PersonReferenceDto;
-import de.symeda.sormas.api.user.UserRight;
-import de.symeda.sormas.ui.ControllerProvider;
-import de.symeda.sormas.ui.UserProvider;
-import de.symeda.sormas.ui.utils.ButtonHelper;
 import de.symeda.sormas.ui.utils.CssStyles;
 
 public class ImmunizationListComponent extends VerticalLayout {
 
 	public static final String IMMUNIZATION_LOC = "immunizations";
 
-	private ImmunizationList immunizationList;
-
-	private ImmunizationCriteria immunizationCriteria;
-
 	public ImmunizationListComponent(PersonReferenceDto personReferenceDto) {
-		immunizationCriteria = new ImmunizationCriteria();
+		ImmunizationCriteria immunizationCriteria = new ImmunizationCriteria();
 
 		if (personReferenceDto != null) {
 			immunizationCriteria.person(personReferenceDto);
@@ -46,7 +32,7 @@ public class ImmunizationListComponent extends VerticalLayout {
 		componentHeader.setWidth(100, Unit.PERCENTAGE);
 		addComponent(componentHeader);
 
-		immunizationList = new ImmunizationList(immunizationCriteria);
+		ImmunizationList immunizationList = new ImmunizationList(immunizationCriteria);
 		addComponent(immunizationList);
 		immunizationList.reload();
 
