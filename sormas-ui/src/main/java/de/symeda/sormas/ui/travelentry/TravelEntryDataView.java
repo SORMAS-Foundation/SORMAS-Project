2a585b86d9807012bd6269e9858b37b2682d6120
alexcaruntu-vita <76100512+alexcaruntu-vita@users.noreply.github.com>
2021-09-27 11:46:53 +0300

    Bugfix 5513 long document names (#6805)
    * #5513 - Made DocumentListComponent inheriy from SideComponent
    
    * #5513 - Refactorings
    
    * #5513 - Created DocumentList
    
    * #5513 - Truncate long document names

--- sormas-ui/src/main/java/de/symeda/sormas/ui/travelentry/TravelEntryDataView.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/travelentry/TravelEntryDataView.java
@@ -23,6 +23,7 @@ import de.symeda.sormas.ui.utils.CommitDiscardWrapperComponent;
 import de.symeda.sormas.ui.utils.CssStyles;
 import de.symeda.sormas.ui.utils.DetailSubComponentWrapper;
 import de.symeda.sormas.ui.utils.LayoutUtil;
+import de.symeda.sormas.ui.utils.components.sidecomponent.SideComponentLayout;
 
 public class TravelEntryDataView extends AbstractTravelEntryView {
 
@@ -92,8 +93,7 @@ public class TravelEntryDataView extends AbstractTravelEntryView {
 				getReference(),
 				UserRight.TRAVEL_ENTRY_EDIT,
 				travelEntryDto.isPseudonymized());
-			documentList.addStyleName(CssStyles.SIDE_COMPONENT);
-			layout.addComponent(documentList, DOCUMENTS_LOC);
+			layout.addComponent(new SideComponentLayout(documentList), DOCUMENTS_LOC);
 		}
 
 		QuarantineOrderDocumentsComponent.addComponentToLayout(layout, getTravelEntryRef());
