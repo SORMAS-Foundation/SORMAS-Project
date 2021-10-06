99beedae1845fabbe992efc692d61e7030552088
sergiupacurariu <62688603+sergiupacurariu@users.noreply.github.com>
2021-10-06 16:53:51 +0300

    #6828 - Make contact classification required in the API and backend

--- sormas-backend/src/main/resources/sql/sormas_schema.sql
+++ sormas-backend/src/main/resources/sql/sormas_schema.sql
@@ -8293,4 +8293,14 @@ ALTER TABLE travelentry_history ADD COLUMN diseasevariantdetails text;
 
 INSERT INTO schema_version (version_number, comment) VALUES (408, 'Add disease variant details #5935');
 
+-- 2021-09-30 Make contact classification required in the API and backend #6828
+UPDATE contact set contactclassification = 'UNCONFIRMED' where contactclassification IS NULL;
+ALTER TABLE contact ALTER COLUMN contactclassification SET NOT NULL;
+ALTER TABLE contact_history ALTER COLUMN contactclassification SET NOT NULL;
+
+ALTER TABLE cases ALTER COLUMN responsibleRegion_id SET NOT NULL;
+ALTER TABLE cases ALTER COLUMN responsibleDistrict_id SET NOT NULL;
+ALTER TABLE cases ALTER COLUMN healthfacility_id SET NOT NULL;
+
+INSERT INTO schema_version (version_number, comment) VALUES (409, 'Make contact classification required in the API and backend #6828');
 -- *** Insert new sql commands BEFORE this line. Remember to always consider _history tables. ***
