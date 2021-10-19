34843296becbe7b0d4e9d6f9735dd08c7c907b56
Jonas Cirotzki <jonas.cirotzki@posteo.de>
2021-10-19 11:22:32 +0200

    [#6468] use latest change data received from etcd to determine the new latest sync date

--- sormas-backend/src/main/java/de/symeda/sormas/backend/infrastructure/central/CentralInfraSyncFacade.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/infrastructure/central/CentralInfraSyncFacade.java
@@ -2,7 +2,9 @@ package de.symeda.sormas.backend.infrastructure.central;
 
 import de.symeda.sormas.api.EntityDto;
 import de.symeda.sormas.api.InfrastructureDataReferenceDto;
+import de.symeda.sormas.api.feature.FeatureType;
 import de.symeda.sormas.api.infrastructure.InfrastructureBaseFacade;
+import de.symeda.sormas.api.infrastructure.area.AreaDto;
 import de.symeda.sormas.api.infrastructure.community.CommunityDto;
 import de.symeda.sormas.api.infrastructure.continent.ContinentDto;
 import de.symeda.sormas.api.infrastructure.country.CountryDto;
@@ -14,6 +16,8 @@ import de.symeda.sormas.api.systemevents.SystemEventType;
 import de.symeda.sormas.api.utils.criteria.BaseCriteria;
 import de.symeda.sormas.backend.central.EtcdCentralClient;
 import de.symeda.sormas.backend.common.ConfigFacadeEjb;
+import de.symeda.sormas.backend.feature.FeatureConfigurationFacadeEjb;
+import de.symeda.sormas.backend.infrastructure.area.AreaFacadeEjb;
 import de.symeda.sormas.backend.infrastructure.community.CommunityFacadeEjb;
 import de.symeda.sormas.backend.infrastructure.continent.ContinentFacadeEjb;
 import de.symeda.sormas.backend.infrastructure.country.CountryFacadeEjb;
@@ -30,6 +34,7 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -41,49 +46,62 @@ public class CentralInfraSyncFacade {
 	public static final String CONTINENT = "continent";
 	public static final String SUBCONTINENT = "subcontinent";
 	public static final String COUNTRY = "country";
+	public static final String AREA = "area";
 	public static final String REGION = "region";
 	public static final String DISTRICT = "district";
 	public static final String COMMUNITY = "community";
+	public static final String CENTRAL_LOCATION_TEMPLATE = "/central/location/%s/";
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	@EJB
-	ContinentFacadeEjb.ContinentFacadeEjbLocal continentFacadeEjb;
+	private ContinentFacadeEjb.ContinentFacadeEjbLocal continentFacadeEjb;
 	@EJB
-	SubcontinentFacadeEjb.SubcontinentFacadeEjbLocal subcontinentFacadeEjb;
+	private SubcontinentFacadeEjb.SubcontinentFacadeEjbLocal subcontinentFacadeEjb;
 	@EJB
-	CountryFacadeEjb.CountryFacadeEjbLocal countryFacadeEjb;
+	private CountryFacadeEjb.CountryFacadeEjbLocal countryFacadeEjb;
 	@EJB
-	RegionFacadeEjb.RegionFacadeEjbLocal regionFacadeEjb;
+	private AreaFacadeEjb.AreaFacadeEjbLocal areaFacadeEjb;
 	@EJB
-	DistrictFacadeEjb.DistrictFacadeEjbLocal districtFacadeEjb;
+	private RegionFacadeEjb.RegionFacadeEjbLocal regionFacadeEjb;
 	@EJB
-	CommunityFacadeEjb.CommunityFacadeEjbLocal communityFacadeEjb;
+	private DistrictFacadeEjb.DistrictFacadeEjbLocal districtFacadeEjb;
+	@EJB
+	private CommunityFacadeEjb.CommunityFacadeEjbLocal communityFacadeEjb;
 	@EJB
 	private SyncFacadeEjb.SyncFacadeEjbLocal syncFacadeEjb;
 	@Inject
 	private EtcdCentralClient centralClient;
 	@EJB
 	private ConfigFacadeEjb.ConfigFacadeEjbLocal configFacade;
+	@EJB
+	private FeatureConfigurationFacadeEjb.FeatureConfigurationFacadeEjbLocal featureConfigurationFacadeEjb;
 
-	private <DTO extends EntityDto, INDEX_DTO extends Serializable, REF_DTO extends InfrastructureDataReferenceDto, CRITERIA extends BaseCriteria> void loadAndStore(
+	private <DTO extends EntityDto, INDEX_DTO extends Serializable, REF_DTO extends InfrastructureDataReferenceDto, CRITERIA extends BaseCriteria> Date loadAndStore(
 		String type,
 		Class<DTO> clazz,
 		InfrastructureBaseFacade<DTO, INDEX_DTO, REF_DTO, CRITERIA> facade,
-		Date lastUpdate) {
+		Date lastSync) {
 		List<DTO> dtos;
 
 		try {
-			dtos = centralClient.getWithPrefix(String.format("/central/location/%s/", type), clazz);
+			dtos = centralClient.getWithPrefix(String.format(CENTRAL_LOCATION_TEMPLATE, type), clazz);
 		} catch (IOException e) {
 			logger.error("Could not load all entities of type {} from central: %s", type, e);
-			return;
+			return lastSync;
 		}
 		logger.info("Loaded {} entities of type {}", dtos.size(), type);
 
-		List<DTO> newDtos = dtos.stream().filter(d -> d.getChangeDate().after(lastUpdate)).collect(Collectors.toList());
+		List<DTO> newDtos = dtos.stream().filter(d -> d.getChangeDate().after(lastSync)).collect(Collectors.toList());
 		logger.info("Importing {} entities of type {}", newDtos.size(), type);
+		if (newDtos.isEmpty()) {
+			return lastSync;
+		}
+
+		// find the newest change date in the received data
+		Date newestChangeDate = newDtos.stream().map(DTO::getChangeDate).max(Date::compareTo).orElse(lastSync);
 
 		newDtos.forEach(facade::save);
+		return newestChangeDate;
 
 	}
 
@@ -95,46 +113,59 @@ public class CentralInfraSyncFacade {
 		logger.info("Syncing all infra data.");
 		try {
 			SystemEventDto currentSync = syncFacadeEjb.startSyncFor(SystemEventType.CENTRAL_SYNC_INFRA);
-			Date lastUpdate = syncFacadeEjb.findLastSyncDateFor(SystemEventType.CENTRAL_SYNC_INFRA);
-			Date syncedAt = new Date();
+			Date lastSync = syncFacadeEjb.findLastSyncDateFor(SystemEventType.CENTRAL_SYNC_INFRA);
+			Date[] latestChangeDates = {
+				loadAndStoreContinents(lastSync),
+				loadAndStoreContinents(lastSync),
+				loadAndStoreSubcontinents(lastSync),
+				loadAndStoreCountries(lastSync),
+				loadAndStoreArea(lastSync),
+				loadAndStoreRegions(lastSync),
+				loadAndStoreDistricts(lastSync),
+				loadAndStoreCommunities(lastSync) };
 
-			loadAndStoreContinents(lastUpdate);
-			loadAndStoreSubcontinents(lastUpdate);
-			loadAndStoreCountries(lastUpdate);
-			loadAndStoreRegions(lastUpdate);
-			loadAndStoreDistricts(lastUpdate);
-			loadAndStoreCommunities(lastUpdate);
+			// we filter for the highest change data received from central. If no newer date is found, we keep the last sync.
+			Date latestSyncDate = Arrays.stream(latestChangeDates).max(Date::compareTo).orElse(lastSync);
+			syncFacadeEjb.reportSuccessfulSyncWithTimestamp(currentSync, latestSyncDate);
 
-			syncFacadeEjb.reportSuccessfulSyncWithTimestamp(currentSync, syncedAt);
 		} catch (Exception e) {
-			// broad clause is necessary here: Cron schedule stability was influenced by uncatched exceptions 
+			// broad clause is necessary here: Cron schedule stability has been influenced by uncatched exceptions
 			logger.error("Could not sync with central: %s", e);
 		}
 
 	}
 
-	private void loadAndStoreContinents(Date lastUpdate) {
-		loadAndStore(CONTINENT, ContinentDto.class, continentFacadeEjb, lastUpdate);
+	private Date loadAndStoreContinents(Date lastUpdate) {
+		return loadAndStore(CONTINENT, ContinentDto.class, continentFacadeEjb, lastUpdate);
 	}
 
-	private void loadAndStoreSubcontinents(Date lastUpdate) {
-		loadAndStore(SUBCONTINENT, SubcontinentDto.class, subcontinentFacadeEjb, lastUpdate);
+	private Date loadAndStoreSubcontinents(Date lastUpdate) {
+		return loadAndStore(SUBCONTINENT, SubcontinentDto.class, subcontinentFacadeEjb, lastUpdate);
 	}
 
-	private void loadAndStoreCountries(Date lastUpdate) {
-		loadAndStore(COUNTRY, CountryDto.class, countryFacadeEjb, lastUpdate);
+	private Date loadAndStoreCountries(Date lastUpdate) {
+		return loadAndStore(COUNTRY, CountryDto.class, countryFacadeEjb, lastUpdate);
 	}
 
-	private void loadAndStoreRegions(Date lastUpdate) {
-		loadAndStore(REGION, RegionDto.class, regionFacadeEjb, lastUpdate);
+	private Date loadAndStoreArea(Date lastUpdate) {
+		if (featureConfigurationFacadeEjb.isFeatureEnabled(FeatureType.INFRASTRUCTURE_TYPE_AREA)) {
+			return loadAndStore(AREA, AreaDto.class, areaFacadeEjb, lastUpdate);
+		} else {
+			logger.info("Skipping sync for Area as feature is disabled.");
+			return lastUpdate;
+		}
 	}
 
-	private void loadAndStoreDistricts(Date lastUpdate) {
-		loadAndStore(DISTRICT, DistrictDto.class, districtFacadeEjb, lastUpdate);
+	private Date loadAndStoreRegions(Date lastUpdate) {
+		return loadAndStore(REGION, RegionDto.class, regionFacadeEjb, lastUpdate);
 	}
 
-	private void loadAndStoreCommunities(Date lastUpdate) {
-		loadAndStore(COMMUNITY, CommunityDto.class, communityFacadeEjb, lastUpdate);
+	private Date loadAndStoreDistricts(Date lastUpdate) {
+		return loadAndStore(DISTRICT, DistrictDto.class, districtFacadeEjb, lastUpdate);
+	}
+
+	private Date loadAndStoreCommunities(Date lastUpdate) {
+		return loadAndStore(COMMUNITY, CommunityDto.class, communityFacadeEjb, lastUpdate);
 	}
 
 }
