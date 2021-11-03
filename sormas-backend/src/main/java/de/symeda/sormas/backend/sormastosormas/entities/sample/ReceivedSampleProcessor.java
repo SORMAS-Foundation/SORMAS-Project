027dd37764892f22f1208b49d226bc418d1628ff
Alex Vidrean <alexandru.vidrean@fxbits.io>
2021-11-03 11:30:46 +0200

    #7014 removed unused import

--- sormas-backend/src/main/java/de/symeda/sormas/backend/sormastosormas/entities/sample/ReceivedSampleProcessor.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/sormastosormas/entities/sample/ReceivedSampleProcessor.java
@@ -37,7 +37,6 @@ import de.symeda.sormas.backend.sormastosormas.data.received.ReceivedDataProcess
 import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Stateless
