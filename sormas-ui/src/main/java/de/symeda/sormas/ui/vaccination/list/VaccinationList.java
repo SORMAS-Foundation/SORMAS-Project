e3688951306787555a4e04009b8e2560b8eb5a49
Alexandru Caruntu <alexandru.caruntu.ext@vitagroup.ag>
2021-12-04 14:16:21 +0200

    #7404 - Reload just the vaccination list after editing

--- sormas-ui/src/main/java/de/symeda/sormas/ui/vaccination/list/VaccinationList.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/vaccination/list/VaccinationList.java
@@ -28,7 +28,6 @@ import de.symeda.sormas.api.i18n.I18nProperties;
 import de.symeda.sormas.api.utils.fieldaccess.UiFieldAccessCheckers;
 import de.symeda.sormas.api.vaccination.VaccinationListEntryDto;
 import de.symeda.sormas.ui.ControllerProvider;
-import de.symeda.sormas.ui.SormasUI;
 import de.symeda.sormas.ui.utils.PaginationList;
 import de.symeda.sormas.ui.vaccination.list.event.VaccinationListChangedEvent;
 
@@ -79,10 +78,7 @@ public class VaccinationList extends PaginationList<VaccinationListEntryDto> {
 					listEntry.getVaccination().getDisease(),
 					UiFieldAccessCheckers.getDefault(listEntry.getVaccination().isPseudonymized()),
 					true,
-					v -> {
-						reload();
-						SormasUI.refreshView();
-					},
+					v -> reload(),
 					() -> {
 						listLayout.removeAllComponents();
 						reload();
