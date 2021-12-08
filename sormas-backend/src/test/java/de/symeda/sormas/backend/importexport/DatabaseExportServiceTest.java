acfe929a1d207a56cb78af371821b3b0fc3e8433
Levente Gal <levente.gal.ext@vitagroup.ag>
2021-12-08 11:05:24 +0200

    #7220 Add missing entities to database tables to the data dictionary and the database export - WIP

--- sormas-backend/src/test/java/de/symeda/sormas/backend/importexport/DatabaseExportServiceTest.java
+++ sormas-backend/src/test/java/de/symeda/sormas/backend/importexport/DatabaseExportServiceTest.java
@@ -1,11 +1,23 @@
 package de.symeda.sormas.backend.importexport;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.isEmptyString;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.persistence.Entity;
+
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
+
+import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
 
 import de.symeda.sormas.api.importexport.DatabaseTable;
 
@@ -35,5 +47,30 @@ public class DatabaseExportServiceTest {
 				assertThat(config.getJoinColumnName(), not(isEmptyString()));
 			}
 		}
+	}
+
+	@Test
+	public void test_all_entities_have_export_configuration() {
+		Set<String> exportableTables =
+			DatabaseExportService.EXPORT_CONFIGS.values().stream().map(DatabaseExportConfiguration::getTableName).collect(Collectors.toSet());
+		Set<String> missingEntities = new HashSet<>();
+
+		JavaClasses classes = new ClassFileImporter().importPackages("de.symeda.sormas.backend");
+
+		for (JavaClass clazz : classes) {
+			if (clazz.isAnnotatedWith(Entity.class)) {
+				Entity entityAnnotation = clazz.getAnnotationOfType(Entity.class);
+				String tableName = entityAnnotation.name();
+				if (StringUtils.isBlank(tableName)) {
+					tableName = clazz.getSimpleName().toLowerCase();
+				}
+
+				if (!exportableTables.contains(tableName)) {
+					missingEntities.add(clazz.getSimpleName());
+				}
+			}
+		}
+
+		assertThat("Entities [" + String.join(", ", missingEntities) + "] are not configured as exportable", missingEntities, hasSize(0));
 	}
 }
