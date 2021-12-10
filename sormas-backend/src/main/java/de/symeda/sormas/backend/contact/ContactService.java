7420d7292b828623fdf2f7d7634e9b8d91967391
Kevel <k.choumene@cloudfacile.com>
2021-12-10 06:24:44 +0100

    temporally undo change due to test failure

--- sormas-backend/src/main/java/de/symeda/sormas/backend/contact/ContactService.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/contact/ContactService.java
@@ -921,7 +921,7 @@ public class ContactService extends AbstractCoreAdoService<Contact> {
 			} else {
 				// Fix bug when there is no filter that matches
 				// Add a default filter that always matches
-				return cb.isNull(contactPath.get(Contact.UUID));
+				return null;
 			}
 		}
 
@@ -966,9 +966,9 @@ public class ContactService extends AbstractCoreAdoService<Contact> {
 
 		// Fix bug when there is no filter that matches
 		// Add a default filter that always matches
-		if(filter == null){
-			filter = cb.isNull(contactPath.get(Contact.UUID));
-		}
+		// if(filter == null){
+		// 	filter = cb.isNull(contactPath.get(Contact.UUID));
+		// }
 		return filter;
 	}
 
