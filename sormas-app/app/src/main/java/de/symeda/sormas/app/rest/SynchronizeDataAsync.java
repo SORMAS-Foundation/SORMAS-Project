a9d260a7613f6aed0d1bdf45dfeb662cb5870112
Maté Strysewske <mate.strysewske@vitagroup.ag>
2021-12-13 11:00:43 +0100

    #7302 - Removed unnecessary pulling logic

--- sormas-app/app/src/main/java/de/symeda/sormas/app/rest/SynchronizeDataAsync.java
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/rest/SynchronizeDataAsync.java
@@ -115,7 +115,7 @@ public class SynchronizeDataAsync extends AsyncTask<Void, Void, Void> {
 				syncModeTrace.start();
 
 				// Prioritize pushing new data
-				pushNewData(true);
+				pushNewData();
 				// Infrastructure always has to be pulled - otherwise referenced data may be lost (e.g. #586)
 				pullInfrastructure();
 				// Pull and remove deleted entities when the last time this has been done is more than 24 hours ago
@@ -137,7 +137,6 @@ public class SynchronizeDataAsync extends AsyncTask<Void, Void, Void> {
 				syncModeTrace = FirebasePerformance.getInstance().newTrace("syncModeCompleteTrace");
 				syncModeTrace.start();
 
-				pushNewData(false);
 				pullInfrastructure(); // do before missing, because we may have a completely empty database
 				pullMissingAndDeleteInvalidInfrastructure();
 				pushNewPullMissingAndDeleteInvalidData();
@@ -247,79 +246,24 @@ public class SynchronizeDataAsync extends AsyncTask<Void, Void, Void> {
 	}
 
 	@AddTrace(name = "pushNewDataTrace")
-	private void pushNewData(boolean pullChanges)
-		throws ServerCommunicationException, ServerConnectionException, DaoException, NoConnectionException {
+	private void pushNewData() throws ServerCommunicationException, ServerConnectionException, DaoException, NoConnectionException {
 
-		PersonDtoHelper personDtoHelper = new PersonDtoHelper();
-		CaseDtoHelper caseDtoHelper = new CaseDtoHelper();
-		ImmunizationDtoHelper immunizationDtoHelper = new ImmunizationDtoHelper();
-		EventDtoHelper eventDtoHelper = new EventDtoHelper();
-		EventParticipantDtoHelper eventParticipantDtoHelper = new EventParticipantDtoHelper();
-		SampleDtoHelper sampleDtoHelper = new SampleDtoHelper();
-		PathogenTestDtoHelper pathogenTestDtoHelper = new PathogenTestDtoHelper();
-		AdditionalTestDtoHelper additionalTestDtoHelper = new AdditionalTestDtoHelper();
-		ContactDtoHelper contactDtoHelper = new ContactDtoHelper();
-		VisitDtoHelper visitDtoHelper = new VisitDtoHelper();
-		TaskDtoHelper taskDtoHelper = new TaskDtoHelper();
-		WeeklyReportDtoHelper weeklyReportDtoHelper = new WeeklyReportDtoHelper();
-		AggregateReportDtoHelper aggregateReportDtoHelper = new AggregateReportDtoHelper();
-		PrescriptionDtoHelper prescriptionDtoHelper = new PrescriptionDtoHelper();
-		TreatmentDtoHelper treatmentDtoHelper = new TreatmentDtoHelper();
-		ClinicalVisitDtoHelper clinicalVisitDtoHelper = new ClinicalVisitDtoHelper();
-
-		boolean personsNeedPull = personDtoHelper.pushEntities(true);
-		boolean casesNeedPull = caseDtoHelper.pushEntities(true);
-		boolean immunizationsNeedPull = immunizationDtoHelper.pushEntities(true);
-		boolean eventsNeedPull = eventDtoHelper.pushEntities(true);
-		boolean eventParticipantsNeedPull = eventParticipantDtoHelper.pushEntities(true);
-		boolean samplesNeedPull = sampleDtoHelper.pushEntities(true);
-		boolean pathogenTestsNeedPull = pathogenTestDtoHelper.pushEntities(true);
-		boolean additionalTestsNeedPull = additionalTestDtoHelper.pushEntities(true);
-		boolean contactsNeedPull = contactDtoHelper.pushEntities(true);
-		boolean visitsNeedPull = visitDtoHelper.pushEntities(true);
-		boolean tasksNeedPull = taskDtoHelper.pushEntities(true);
-		boolean weeklyReportsNeedPull = weeklyReportDtoHelper.pushEntities(true);
-		boolean aggregateReportsNeedPull = aggregateReportDtoHelper.pushEntities(true);
-		boolean prescriptionsNeedPull = prescriptionDtoHelper.pushEntities(true);
-		boolean treatmentsNeedPull = treatmentDtoHelper.pushEntities(true);
-		boolean clinicalVisitsNeedPull = clinicalVisitDtoHelper.pushEntities(true);
-
-		if (pullChanges) {
-			casesNeedPull |= clinicalVisitsNeedPull;
-
-			if (personsNeedPull)
-				personDtoHelper.pullEntities(true);
-			if (casesNeedPull)
-				caseDtoHelper.pullEntities(true);
-			if (immunizationsNeedPull)
-				immunizationDtoHelper.pullEntities(true);
-			if (eventsNeedPull)
-				eventDtoHelper.pullEntities(true);
-			if (eventParticipantsNeedPull)
-				eventParticipantDtoHelper.pullEntities(true);
-			if (samplesNeedPull)
-				sampleDtoHelper.pullEntities(true);
-			if (pathogenTestsNeedPull)
-				pathogenTestDtoHelper.pullEntities(true);
-			if (additionalTestsNeedPull)
-				additionalTestDtoHelper.pullEntities(true);
-			if (contactsNeedPull)
-				contactDtoHelper.pullEntities(true);
-			if (visitsNeedPull)
-				visitDtoHelper.pullEntities(true);
-			if (tasksNeedPull)
-				taskDtoHelper.pullEntities(true);
-			if (weeklyReportsNeedPull)
-				weeklyReportDtoHelper.pullEntities(true);
-			if (aggregateReportsNeedPull)
-				aggregateReportDtoHelper.pullEntities(true);
-			if (prescriptionsNeedPull)
-				prescriptionDtoHelper.pullEntities(true);
-			if (treatmentsNeedPull)
-				treatmentDtoHelper.pullEntities(true);
-			if (clinicalVisitsNeedPull)
-				clinicalVisitDtoHelper.pullEntities(true);
-		}
+		new PersonDtoHelper().pushEntities(true);
+		new CaseDtoHelper().pushEntities(true);
+		new ImmunizationDtoHelper().pushEntities(true);
+		new EventDtoHelper().pushEntities(true);
+		new EventParticipantDtoHelper().pushEntities(true);
+		new SampleDtoHelper().pushEntities(true);
+		new PathogenTestDtoHelper().pushEntities(true);
+		new AdditionalTestDtoHelper().pushEntities(true);
+		new ContactDtoHelper().pushEntities(true);
+		new VisitDtoHelper().pushEntities(true);
+		new TaskDtoHelper().pushEntities(true);
+		new WeeklyReportDtoHelper().pushEntities(true);
+		new AggregateReportDtoHelper().pushEntities(true);
+		new PrescriptionDtoHelper().pushEntities(true);
+		new TreatmentDtoHelper().pushEntities(true);
+		new ClinicalVisitDtoHelper().pushEntities(true);
 	}
 
 	@AddTrace(name = "synchronizeChangedDataTrace")
@@ -627,7 +571,7 @@ public class SynchronizeDataAsync extends AsyncTask<Void, Void, Void> {
 
 		// first push everything that has been CREATED by the user - otherwise this data my lose it's references to other entities.
 		// Example: Case is created using an existing person, meanwhile user loses access to the person
-		pushNewData(false);
+		pushNewData();
 
 		// weekly reports and entries
 		List<String> weeklyReportUuids = executeUuidCall(RetroProvider.getWeeklyReportFacade().pullUuids());
