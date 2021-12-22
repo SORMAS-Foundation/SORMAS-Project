65e53bac25ad5b191a95b0d71da7b6924d9186e4
mario-kuehne <mario.kuehne.ext@vitagroup.ag>
2021-12-22 10:41:01 +0100

    #7472 - adds test for checking disease and newCases counter for COVID-19 after creating a new case and case related person through API calls

---
+++ sormas-e2e-tests/src/test/java/org/sormas/e2etests/steps/web/application/dashboard/surveillance/SurveillanceDashboardSteps.java
@@ -0,0 +1,75 @@
+package org.sormas.e2etests.steps.web.application.dashboard.surveillance;
+
+import cucumber.api.java8.En;
+import javax.inject.Inject;
+import org.assertj.core.api.SoftAssertions;
+import org.sormas.e2etests.helpers.WebDriverHelpers;
+import org.sormas.e2etests.pages.application.dashboard.Surveillance.SurveillanceDashboardPage;
+
+public class SurveillanceDashboardSteps implements En {
+
+  private final WebDriverHelpers webDriverHelpers;
+  private final SoftAssertions softly;
+  private Integer covid19DiseaseCounterIntValueBefore;
+  private Integer covid19DiseaseCounterIntValueAfter;
+  private Integer newCasesCounterIntValueBefore;
+  private Integer newCasesCounterIntValueAfter;
+
+  @Inject
+  public SurveillanceDashboardSteps(WebDriverHelpers webDriverHelpers, SoftAssertions softly) {
+    this.webDriverHelpers = webDriverHelpers;
+    this.softly = softly;
+
+    When(
+        "^I save value for COVID disease counter$",
+        () -> {
+          webDriverHelpers.waitForPageLoadingSpinnerToDisappear(10);
+          String covid19DiseaseCounterRawValue =
+              webDriverHelpers.getTextFromWebElement(
+                  SurveillanceDashboardPage.COVID19_DISEASE_COUNTER);
+          covid19DiseaseCounterIntValueBefore = Integer.valueOf(covid19DiseaseCounterRawValue);
+        });
+
+    Then(
+        "^I check that previous saved counters have been increment$",
+        () -> {
+          webDriverHelpers.waitForPageLoadingSpinnerToDisappear(10);
+
+          String newCasesCounterRawValueAfter =
+              webDriverHelpers.getTextFromWebElement(SurveillanceDashboardPage.CASE_COUNTER);
+          newCasesCounterIntValueAfter = Integer.valueOf(newCasesCounterRawValueAfter);
+
+          String covid19DiseaseCounterRawValueAfter =
+              webDriverHelpers.getTextFromWebElement(
+                  SurveillanceDashboardPage.COVID19_DISEASE_COUNTER);
+          covid19DiseaseCounterIntValueAfter = Integer.valueOf(covid19DiseaseCounterRawValueAfter);
+
+          softly.assertThat(newCasesCounterIntValueBefore).isLessThan(newCasesCounterIntValueAfter);
+          softly
+              .assertThat(covid19DiseaseCounterIntValueBefore)
+              .isLessThan(covid19DiseaseCounterIntValueAfter);
+          softly
+              .assertThat(newCasesCounterIntValueAfter)
+              .isEqualTo(covid19DiseaseCounterIntValueAfter);
+          softly.assertAll();
+        });
+
+    When(
+        "I select {string} in TabSheet of Surveillance Dashboard",
+        (String tabSheetValue) -> {
+          webDriverHelpers.waitForPageLoadingSpinnerToDisappear(10);
+          // webDriverHelpers.clickOnWebElementBySelector(SurveillanceDashboardPage.TAB_SHEET_CAPTION);
+          webDriverHelpers.clickWebElementByText(
+              SurveillanceDashboardPage.TAB_SHEET_CAPTION, tabSheetValue);
+        });
+
+    When(
+        "^I save value for New Cases counter$",
+        () -> {
+          webDriverHelpers.waitForPageLoadingSpinnerToDisappear(10);
+          String newCasesCounterValue =
+              webDriverHelpers.getTextFromWebElement(SurveillanceDashboardPage.CASE_COUNTER);
+          newCasesCounterIntValueBefore = Integer.valueOf(newCasesCounterValue);
+        });
+  }
+}
---
+++ sormas-e2e-tests/src/test/resources/features/sanity/web/Dashboard.feature
@@ -0,0 +1,18 @@
+@UI @Sanity @Dashboard @#7472
+Feature: Dashboard counters
+
+  Scenario: Check disease and new cases counter in Surveillance Dashboard
+    Given I log in with National User
+    When I click on the Dashboard button from navbar and access Surveillance Dashboard
+    When I select "COVID-19" in TabSheet of Surveillance Dashboard
+    When I save value for COVID disease counter
+    When I save value for New Cases counter
+    When API: I create a new person
+    Then API: I check that POST call body is "OK"
+    Then API: I check that POST call status code is 200
+    When API: I create a new case
+    Then API: I check that POST call body is "OK"
+    Then API: I check that POST call status code is 200
+    When I click on the Dashboard button from navbar and access Surveillance Dashboard
+    When I select "COVID-19" in TabSheet of Surveillance Dashboard
+    Then I check that previous saved counters have been increment
