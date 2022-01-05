8003afc2f1bf55711e6152c7ebf31f98d09e511e
Alexandru Caruntu <alexandru.caruntu.ext@vitagroup.ag>
2022-01-05 13:49:03 +0200

    #7534 - PathogenTestListComponent refactoring

--- sormas-ui/src/main/java/de/symeda/sormas/ui/samples/pathogentestlink/PathogenTestListComponent.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/samples/pathogentestlink/PathogenTestListComponent.java
@@ -20,15 +20,8 @@ package de.symeda.sormas.ui.samples.pathogentestlink;
 import java.util.function.BiConsumer;
 import java.util.function.Supplier;
 
-import com.vaadin.icons.VaadinIcons;
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Notification.Type;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.themes.ValoTheme;
 
 import de.symeda.sormas.api.i18n.Captions;
 import de.symeda.sormas.api.i18n.I18nProperties;
@@ -37,49 +30,29 @@ import de.symeda.sormas.api.sample.PathogenTestDto;
 import de.symeda.sormas.api.sample.SampleReferenceDto;
 import de.symeda.sormas.api.user.UserRight;
 import de.symeda.sormas.ui.ControllerProvider;
-import de.symeda.sormas.ui.UserProvider;
-import de.symeda.sormas.ui.utils.ButtonHelper;
-import de.symeda.sormas.ui.utils.CssStyles;
+import de.symeda.sormas.ui.utils.components.sidecomponent.SideComponent;
 
 @SuppressWarnings("serial")
-public class PathogenTestListComponent extends VerticalLayout {
+public class PathogenTestListComponent extends SideComponent {
 
 	private PathogenTestList list;
-	private Button createButton;
 
 	public PathogenTestListComponent(
 		SampleReferenceDto sampleRef,
 		BiConsumer<PathogenTestDto, Runnable> onSavedPathogenTest,
 		Supplier<Boolean> createOrEditAllowedCallback) {
-		setWidth(100, Unit.PERCENTAGE);
+		super(I18nProperties.getString(Strings.headingTests));
 
-		HorizontalLayout componentHeader = new HorizontalLayout();
-		componentHeader.setWidth(100, Unit.PERCENTAGE);
-		addComponent(componentHeader);
+		addCreateButton(I18nProperties.getCaption(Captions.pathogenTestNewTest), UserRight.PATHOGEN_TEST_CREATE, e -> {
+			if (createOrEditAllowedCallback.get()) {
+				ControllerProvider.getPathogenTestController().create(sampleRef, 0, list::reload, onSavedPathogenTest);
+			} else {
+				Notification.show(null, I18nProperties.getString(Strings.messageFormHasErrorsPathogenTest), Type.ERROR_MESSAGE);
+			}
+		});
 
 		list = new PathogenTestList(sampleRef, onSavedPathogenTest, createOrEditAllowedCallback);
 		addComponent(list);
-		list.reload();
-
-		Label testsHeader = new Label(I18nProperties.getString(Strings.headingTests));
-		testsHeader.addStyleName(CssStyles.H3);
-		componentHeader.addComponent(testsHeader);
-
-		if (UserProvider.getCurrent().hasUserRight(UserRight.PATHOGEN_TEST_CREATE)) {
-			createButton = ButtonHelper.createIconButton(Captions.pathogenTestNewTest, VaadinIcons.PLUS_CIRCLE, e -> {
-				if (createOrEditAllowedCallback.get()) {
-					ControllerProvider.getPathogenTestController().create(sampleRef, 0, list::reload, onSavedPathogenTest);
-				} else {
-					Notification.show(null, I18nProperties.getString(Strings.messageFormHasErrorsPathogenTest), Type.ERROR_MESSAGE);
-				}
-			}, ValoTheme.BUTTON_PRIMARY);
-
-			componentHeader.addComponent(createButton);
-			componentHeader.setComponentAlignment(createButton, Alignment.MIDDLE_RIGHT);
-		}
-	}
-
-	public void reload() {
 		list.reload();
 	}
 }
