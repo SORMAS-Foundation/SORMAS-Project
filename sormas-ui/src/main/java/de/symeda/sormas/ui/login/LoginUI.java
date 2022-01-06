f148a34916ab9be6b226802b76690e68f785a937
Alex Vidrean <alex.vidrean@gmail.com>
2022-01-06 16:15:01 +0200

    #4601 Remove unused imports

--- sormas-ui/src/main/java/de/symeda/sormas/ui/login/LoginUI.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/login/LoginUI.java
@@ -48,9 +48,6 @@ import fish.payara.security.openid.api.OpenIdContext;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.HttpConstraint;
-import javax.servlet.annotation.ServletSecurity;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
