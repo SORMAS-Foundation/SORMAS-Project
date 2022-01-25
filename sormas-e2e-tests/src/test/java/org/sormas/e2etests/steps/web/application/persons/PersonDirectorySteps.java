43d3ff8f0082083b45ce3b285d62bc98f3214f8f
Michal Kozakiewicz <michal.kozakiewicz@softgent.com>
2022-01-25 13:23:49 +0100

    Added test scenario for filters in Persons Directory Page

--- sormas-e2e-tests/src/test/java/org/sormas/e2etests/steps/web/application/persons/PersonDirectorySteps.java
+++ sormas-e2e-tests/src/test/java/org/sormas/e2etests/steps/web/application/persons/PersonDirectorySteps.java
@@ -18,29 +18,39 @@
 
 package org.sormas.e2etests.steps.web.application.persons;
 
-import static org.sormas.e2etests.pages.application.persons.EditPersonPage.UUID_INPUT;
-import static org.sormas.e2etests.pages.application.persons.EditPersonPage.getByPersonUuid;
+import static org.sormas.e2etests.pages.application.cases.CaseDirectoryPage.*;
+import static org.sormas.e2etests.pages.application.persons.EditPersonPage.*;
 import static org.sormas.e2etests.pages.application.persons.PersonDirectoryPage.*;
 
+import com.google.common.truth.Truth;
 import cucumber.api.java8.En;
+import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 import javax.inject.Named;
 import org.openqa.selenium.By;
+import org.sormas.e2etests.common.DataOperations;
+import org.sormas.e2etests.enums.CommunityValues;
+import org.sormas.e2etests.enums.DistrictsValues;
+import org.sormas.e2etests.enums.RegionsValues;
+import org.sormas.e2etests.helpers.AssertHelpers;
 import org.sormas.e2etests.helpers.WebDriverHelpers;
+import org.sormas.e2etests.state.ApiState;
 import org.sormas.e2etests.pojo.web.Person;
 import org.sormas.e2etests.state.ApiState;
 import org.sormas.e2etests.steps.web.application.contacts.EditContactPersonSteps;
 import org.sormas.e2etests.steps.web.application.events.EditEventSteps;
+import org.testng.Assert;
 
 public class PersonDirectorySteps implements En {
   private final WebDriverHelpers webDriverHelpers;
-  protected Person createdPerson;
 
   @Inject
   public PersonDirectorySteps(
       WebDriverHelpers webDriverHelpers,
       @Named("ENVIRONMENT_URL") String environmentUrl,
-      ApiState apiState) {
+      ApiState apiState,
+      AssertHelpers assertHelpers,
+      DataOperations dataOperations) {
     this.webDriverHelpers = webDriverHelpers;
 
     // TODO refactor all BDD methods naming to be more explicit regarding where data comes from
@@ -62,6 +72,183 @@ public class PersonDirectorySteps implements En {
           webDriverHelpers.clickOnWebElementBySelector(uuidLocator);
           webDriverHelpers.waitForPageLoadingSpinnerToDisappear(120);
           webDriverHelpers.isElementVisibleWithTimeout(UUID_INPUT, 20);
+        });
+
+    Then(
+        "I choose random value for Year of birth filter",
+        () -> {
+          String yearOfBirth = apiState.getLastCreatedPerson().getBirthdateYYYY().toString();
+          webDriverHelpers.waitForPageLoaded();
+          webDriverHelpers.selectFromCombobox(BIRTH_YEAR_COMBOBOX, yearOfBirth);
+        });
+
+    Then(
+        "I choose random value for Month of birth filter",
+        () -> {
+          String monthOfBirth = apiState.getLastCreatedPerson().getBirthdateMM().toString();
+          webDriverHelpers.selectFromCombobox(BIRTH_MONTH_COMBOBOX, monthOfBirth);
+        });
+    Then(
+        "I choose random value for Day of birth filter",
+        () -> {
+          String dayOfBirth = apiState.getLastCreatedPerson().getBirthdateDD().toString();
+          webDriverHelpers.selectFromCombobox(BIRTH_DAY_COMBOBOX, dayOfBirth);
+        });
+
+    Then(
+        "I choose present condition field from specific range",
+        () -> {
+          String presentCondition = apiState.getLastCreatedPerson().getPresentCondition();
+          String capitalizeWord =
+              presentCondition.substring(0, 1).toUpperCase()
+                  + presentCondition.substring(1).toLowerCase();
+          webDriverHelpers.selectFromCombobox(PRESENT_CONDITION, capitalizeWord);
+        });
+    Then(
+        "I choose random value for Region",
+        () -> {
+          String regionName =
+                  getConvertRegionUuidToName(apiState.getLastCreatedPerson().getAddress().getRegion());
+          webDriverHelpers.selectFromCombobox(REGIONS_COMBOBOX, regionName);
+        });
+
+    Then(
+        "I choose random value for District",
+        () -> {
+          String districtName =
+                  getConvertDistrictUuidToName(apiState.getLastCreatedPerson().getAddress().getDistrict());
+          webDriverHelpers.selectFromCombobox(DISTRICTS_COMBOBOX, districtName);
+        });
+
+    Then(
+        "I choose random value for Community",
+        () -> {
+          String communityName =
+                  getConvertCommunityUuidToName(apiState.getLastCreatedPerson().getAddress().getCommunity());
+          webDriverHelpers.selectFromCombobox(COMMUNITY_PERSON_COMBOBOX, communityName);
+        });
+
+    Then(
+        "I fill a different Community field",
+        () -> {
+          webDriverHelpers.selectFromCombobox(COMMUNITY_PERSON_COMBOBOX, "Community2");
+        });
+
+    Then(
+        "I check the result for UID for second person in grid PERSON ID column",
+        () -> {
+          webDriverHelpers.waitUntilAListOfElementsHasText(
+              CASE_GRID_RESULTS_ROWS, apiState.getLastCreatedPerson().getUuid());
+          assertHelpers.assertWithPoll20Second(
+              () ->
+                  Truth.assertWithMessage(
+                          apiState.getLastCreatedPerson().getUuid()
+                              + " value is not displayed in grid Disease column")
+                      .that(apiState.getLastCreatedPerson().getUuid())
+                      .isEqualTo(
+                          String.valueOf(
+                              webDriverHelpers.getTextFromPresentWebElement(
+                                  CASE_PERSON_ID_COLUMN_HEADERS))));
+        });
+
+    Then(
+        "I check that number of displayed Person results is {int}",
+        (Integer number) ->
+            assertHelpers.assertWithPoll20Second(
+                () ->
+                    Assert.assertEquals(
+                        webDriverHelpers.getNumberOfElements(CASE_GRID_RESULTS_ROWS),
+                        number.intValue(),
+                        "Number of displayed cases is not correct")));
+
+    Then(
+        "I check that number of displayed Persons results is {int}",
+        (Integer number) ->
+            assertHelpers.assertWithPoll20Second(
+                () ->
+                    Assert.assertEquals(
+                        webDriverHelpers.getNumberOfElements(CASE_GRID_RESULTS_ROWS),
+                        number.intValue(),
+                        "Number of displayed cases is not correct")));
+
+    Then(
+        "I change Year of birth filter to {string}",
+        (String yearOfBirth) ->
+            webDriverHelpers.selectFromCombobox(BIRTH_YEAR_COMBOBOX, yearOfBirth));
+
+    Then(
+        "I change Month of birth filter to {string}",
+        (String monthOfBirth) ->
+            webDriverHelpers.selectFromCombobox(BIRTH_MONTH_COMBOBOX, monthOfBirth));
+    Then(
+        "I change Day of birth filter to {string}",
+        (String dayOfBirth) -> webDriverHelpers.selectFromCombobox(BIRTH_DAY_COMBOBOX, dayOfBirth));
+
+    Then(
+        "I change present condition filter to {string}",
+        (String presentCondition) ->
+            webDriverHelpers.selectFromCombobox(PRESENT_CONDITION, presentCondition));
+
+    Then(
+        "I change REGION filter to {string}",
+        (String region) -> webDriverHelpers.selectFromCombobox(REGIONS_COMBOBOX, region));
+
+    Then(
+        "I change DISTRICT filter to {string}",
+        (String district) -> webDriverHelpers.selectFromCombobox(DISTRICTS_COMBOBOX, district));
+    Then(
+        "I change Community filter to {string}",
+        (String community) ->
+            webDriverHelpers.selectFromCombobox(COMMUNITY_PERSON_COMBOBOX, community));
+
+    Then(
+        "I search after last created person from API by {string}",
+        (String searchCriteria) -> {
+          String searchText = "";
+          String personUUID =
+              dataOperations.getPartialUuidFromAssociatedLink(
+                  apiState.getLastCreatedPerson().getUuid());
+          switch (searchCriteria) {
+            case "uuid":
+              searchText = personUUID;
+              break;
+            case "full name":
+              searchText =
+                  apiState.getLastCreatedPerson().getLastName()
+                      + " "
+                      + apiState.getLastCreatedPerson().getFirstName();
+              break;
+            case "phone number":
+              searchText = apiState.getLastCreatedPerson().getPhone();
+              break;
+            case "email":
+              searchText = apiState.getLastCreatedPerson().getEmailAddress();
+              break;
+          }
+
+          webDriverHelpers.fillInWebElement(MULTIPLE_OPTIONS_SEARCH_INPUT, searchText);
+        });
+
+    Then(
+        "I change id,full name,email,phone number, by {string}",
+        (String searchCriteria) -> {
+          String searchText = "";
+          String personUUID = "XCBJ2T-XYUGE3-QZK6PD-CWL6KBER";
+          switch (searchCriteria) {
+            case "uuid":
+              searchText = personUUID;
+              break;
+            case "full name":
+              searchText = "Tom Jerry";
+              break;
+            case "phone number":
+              searchText = "(06713) 6606268";
+              break;
+            case "email":
+              searchText = "Tom.Jerry@person.com";
+              break;
+          }
+          webDriverHelpers.fillInWebElement(MULTIPLE_OPTIONS_SEARCH_INPUT, searchText);
         });
 
     When(
@@ -89,6 +276,24 @@ public class PersonDirectorySteps implements En {
         () -> {
           final String personUuid = EditEventSteps.person.getUuid();
           webDriverHelpers.clickOnWebElementBySelector(getByPersonUuid(personUuid));
+        });
+
+    When(
+        "I apply on the APPLY FILTERS button",
+        () -> {
+          webDriverHelpers.waitUntilIdentifiedElementIsVisibleAndClickable(
+              APPLY_FILTERS_BUTTON, 30);
+          webDriverHelpers.clickOnWebElementBySelector(APPLY_FILTERS_BUTTON);
+          TimeUnit.SECONDS.sleep(10);
+        });
+
+    When(
+        "I click on the RESET FILTERS button",
+        () -> {
+          webDriverHelpers.waitUntilIdentifiedElementIsVisibleAndClickable(
+              RESET_FILTERS_BUTTON, 30);
+          webDriverHelpers.clickOnWebElementBySelector(RESET_FILTERS_BUTTON);
+          TimeUnit.SECONDS.sleep(10);
         });
 
     Then(
@@ -127,4 +332,30 @@ public class PersonDirectorySteps implements En {
     // create generic locators
     // The above method will need to be finished by you (the switch to cover all search criteria)
   }
+    public String getConvertRegionUuidToName(String regionUuid) {
+        for (RegionsValues reg : RegionsValues.values()) {
+            if (reg.getUuid() == regionUuid) {
+                return reg.getName();
+            }
+        }
+        return null;
+    }
+
+    public String getConvertDistrictUuidToName(String districtUuid) {
+        for (DistrictsValues dis : DistrictsValues.values()) {
+            if (dis.getUuid() == districtUuid) {
+                return dis.getName();
+            }
+        }
+        return null;
+    }
+
+    public String getConvertCommunityUuidToName(String communityId) {
+        for (CommunityValues com : CommunityValues.values()) {
+            if (com.getUuid() == communityId) {
+                return com.getName();
+            }
+        }
+        return null;
+    }
 }
