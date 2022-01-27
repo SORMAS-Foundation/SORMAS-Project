0155961a08423bd2b3ee3e22e18e65c68b747e66
FredrikSchäferVitagroup <fredrik.schaefer@vitagroup.ag>
2022-01-27 07:17:38 +0100

    #7209 Map disease variant while processing

---
+++ sormas-api/src/main/java/de/symeda/sormas/api/customizableenum/CustomEnumNotFoundException.java
@@ -0,0 +1,10 @@
+package de.symeda.sormas.api.customizableenum;
+
+public class CustomEnumNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1678125180361258201L;
+
+	public CustomEnumNotFoundException(String message) {
+		super(message);
+	}
+}
--- sormas-ui/src/test/java/de/symeda/sormas/ui/labmessage/LabMessageMapperTest.java
+++ sormas-ui/src/test/java/de/symeda/sormas/ui/labmessage/LabMessageMapperTest.java
@@ -2,7 +2,17 @@ package de.symeda.sormas.ui.labmessage;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.when;
 
+import de.symeda.sormas.api.Disease;
+import de.symeda.sormas.api.FacadeProvider;
+import de.symeda.sormas.api.customizableenum.CustomEnumNotFoundException;
+import de.symeda.sormas.api.customizableenum.CustomizableEnumType;
+import de.symeda.sormas.api.disease.DiseaseVariant;
+import de.symeda.sormas.api.i18n.I18nProperties;
+import de.symeda.sormas.api.sample.PathogenTestDto;
+import de.symeda.sormas.backend.customizableenum.CustomizableEnumFacadeEjb;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
 import org.junit.Test;
 
 import de.symeda.sormas.api.labmessage.LabMessageDto;
@@ -10,8 +20,17 @@ import de.symeda.sormas.api.labmessage.TestReportDto;
 import de.symeda.sormas.api.sample.PathogenTestResultType;
 import de.symeda.sormas.api.sample.SampleDto;
 import de.symeda.sormas.ui.AbstractBeanTest;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class LabMessageMapperTest extends AbstractBeanTest {
+
+	@Mock
+	private CustomizableEnumFacadeEjb customizableEnumFacade;
 
 	@Test
 	public void testHomogenousTestResultTypesInWithNoTestReport() {
@@ -66,5 +85,72 @@ public class LabMessageMapperTest extends AbstractBeanTest {
 		mapper.mapToSample(sample);
 
 		assertNull(sample.getPathogenTestResult());
+	}
+
+	@Test
+	public void testMigrateDiseaseVariant() throws CustomEnumNotFoundException {
+		LabMessageDto labMessage = LabMessageDto.build();
+		labMessage.setTestedDisease(Disease.CORONAVIRUS);
+		TestReportDto testReport = TestReportDto.build();
+
+		LabMessageMapper mapper = LabMessageMapper.forLabMessage(labMessage);
+
+		DiseaseVariant diseaseVariant = new DiseaseVariant();
+		when(customizableEnumFacade.getEnumValue(CustomizableEnumType.DISEASE_VARIANT, "P.2", Disease.CORONAVIRUS)).thenReturn(diseaseVariant);
+		when(customizableEnumFacade.getEnumValue(CustomizableEnumType.DISEASE_VARIANT, "NON-EXISTENT", Disease.CORONAVIRUS))
+			.thenThrow(new CustomEnumNotFoundException("not found"));
+		when(customizableEnumFacade.getEnumValue(CustomizableEnumType.DISEASE_VARIANT, null, Disease.CORONAVIRUS))
+			.thenThrow(new CustomEnumNotFoundException("not found"));
+
+		MockedStatic<FacadeProvider> facadeProvider = Mockito.mockStatic(FacadeProvider.class);
+		facadeProvider.when(FacadeProvider::getCustomizableEnumFacade).thenReturn(customizableEnumFacade);
+
+		// No disease variant set
+		ImmutableTriple<String, DiseaseVariant, String> result = mapper.migrateDiseaseVariant(testReport);
+		assertNull(result.getLeft());
+		assertNull(result.getMiddle());
+		assertNull(result.getRight());
+
+		// Unknown disease variant set
+		testReport.setTestedDiseaseVariant("NON-EXISTENT");
+		result = mapper.migrateDiseaseVariant(testReport);
+		assertEquals(
+			I18nProperties.getPrefixCaption(PathogenTestDto.I18N_PREFIX, PathogenTestDto.TESTED_DISEASE_VARIANT) + ": NON-EXISTENT\n",
+			result.getLeft());
+		assertNull(result.getMiddle());
+		assertNull(result.getRight());
+
+		// Unknown disease variant and disease variant details set
+		testReport.setTestedDiseaseVariantDetails("SOME-DETAILS");
+		result = mapper.migrateDiseaseVariant(testReport);
+		assertEquals(
+			I18nProperties.getPrefixCaption(PathogenTestDto.I18N_PREFIX, PathogenTestDto.TESTED_DISEASE_VARIANT) + ": NON-EXISTENT\n"
+				+ I18nProperties.getPrefixCaption(PathogenTestDto.I18N_PREFIX, PathogenTestDto.TESTED_DISEASE_VARIANT_DETAILS) + ": SOME-DETAILS\n",
+			result.getLeft());
+		assertNull(result.getMiddle());
+		assertNull(result.getRight());
+
+		// Only disease variant details set
+		testReport.setTestedDiseaseVariant(null);
+		result = mapper.migrateDiseaseVariant(testReport);
+		assertEquals(
+			I18nProperties.getPrefixCaption(PathogenTestDto.I18N_PREFIX, PathogenTestDto.TESTED_DISEASE_VARIANT_DETAILS) + ": SOME-DETAILS\n",
+			result.getLeft());
+		assertNull(result.getMiddle());
+		assertNull(result.getRight());
+
+		// Known disease variant and disease variant details set
+		testReport.setTestedDiseaseVariant("P.2");
+		result = mapper.migrateDiseaseVariant(testReport);
+		assertNull(result.getLeft());
+		assertEquals(diseaseVariant, result.getMiddle());
+		assertEquals("SOME-DETAILS", result.getRight());
+
+		// Known disease variant set
+		testReport.setTestedDiseaseVariantDetails(null);
+		result = mapper.migrateDiseaseVariant(testReport);
+		assertNull(result.getLeft());
+		assertEquals(diseaseVariant, result.getMiddle());
+		assertNull(result.getRight());
 	}
 }
