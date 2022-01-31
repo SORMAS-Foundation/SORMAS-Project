84a86a3459ef813ef8f7a2b63bcb18ba893e5758
Pawel Kujawa <pawel.kujawa@softgent.com>
2022-01-31 17:50:39 +0100

    SORQA-34 jenkins-fix

--- sormas-e2e-tests/src/main/java/org/sormas/e2etests/pages/application/cases/EditCasePage.java
+++ sormas-e2e-tests/src/main/java/org/sormas/e2etests/pages/application/cases/EditCasePage.java
@@ -128,7 +128,7 @@ public class EditCasePage {
       By.cssSelector("#nationalLevelDate input");
   public static final By GENERAL_COMMENT_TEXTAREA = By.cssSelector("textarea#additionalDetails");
   public static final By SAVE_BUTTON = By.id("commit");
-  public static final By ACTION_CANCEL = By.id("actionCancel");
+  public static final By ACTION_CANCEL = By.cssSelector(".popupContent #actionCancel");
   public static final By DELETE_BUTTON = By.id("delete");
   public static final By DELETE_POPUP_YES_BUTTON = By.cssSelector(".popupContent #actionConfirm");
   public static final By CASE_SAVED_POPUP = By.cssSelector(".v-Notification-caption");
