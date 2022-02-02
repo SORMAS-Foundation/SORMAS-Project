af818bf17ae69f82543ea362994658515361681c
Alexandru Caruntu <alexandru.caruntu.ext@vitagroup.ag>
2022-02-02 22:26:51 +0200

    #7240 - Automatic deletion label color

--- sormas-ui/src/main/java/de/symeda/sormas/ui/utils/components/automaticdeletion/AutomaticDeletionLabel.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/utils/components/automaticdeletion/AutomaticDeletionLabel.java
@@ -1,5 +1,7 @@
 package de.symeda.sormas.ui.utils.components.automaticdeletion;
 
+import java.util.Date;
+
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.ui.HorizontalLayout;
@@ -8,6 +10,7 @@ import com.vaadin.ui.Label;
 import de.symeda.sormas.api.deletionconfiguration.AutomaticDeletionInfoDto;
 import de.symeda.sormas.api.i18n.I18nProperties;
 import de.symeda.sormas.api.i18n.Strings;
+import de.symeda.sormas.api.utils.DateHelper;
 import de.symeda.sormas.ui.utils.CssStyles;
 import de.symeda.sormas.ui.utils.DateFormatHelper;
 
@@ -32,6 +35,14 @@ public class AutomaticDeletionLabel extends HorizontalLayout {
 		Label infoTextLabel = new Label(infoText, ContentMode.HTML);
 		infoTextLabel.addStyleName(CssStyles.VSPACE_TOP_4);
 		addComponent(infoTextLabel);
+
+		if (DateHelper.getDaysBetween(new Date(), automaticDeletionInfoDto.getDeletionDate()) < 181) {
+			infoIcon.addStyleName(CssStyles.LABEL_CRITICAL);
+			infoTextLabel.addStyleName(CssStyles.LABEL_CRITICAL);
+		} else {
+			infoIcon.addStyleName(CssStyles.LABEL_MINOR);
+			infoTextLabel.addStyleName(CssStyles.LABEL_MINOR);
+		}
 	}
 
 	private String formatDeletionPeriod(int deletionPeriod) {
