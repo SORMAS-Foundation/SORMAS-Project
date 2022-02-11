39eebf3d42c606652977433f874a2f629aea24fd
rdutu-vg <razvan.dutu.ext@vitagroup.ag>
2022-02-11 18:49:41 +0200

    #7747-Fix-Allure-Report : refactored lost changes

---
+++ sormas-e2e-tests/src/main/java/org/sormas/e2etests/envconfig/dto/EnvUser.java
@@ -0,0 +1,37 @@
+/*
+ * SORMAS® - Surveillance Outbreak Response Management & Analysis System
+ * Copyright © 2016-2021 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.sormas.e2etests.envconfig.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+@Value
+@AllArgsConstructor
+@NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
+@Builder(toBuilder = true, builderClassName = "builder")
+public class EnvUser {
+
+    String userRole;
+    String username;
+    String password;
+    String uuid;
+}
---
+++ sormas-e2e-tests/src/main/java/org/sormas/e2etests/envconfig/manager/EnvironmentManager.java
@@ -0,0 +1,84 @@
+/*
+ * SORMAS® - Surveillance Outbreak Response Management & Analysis System
+ * Copyright © 2016-2021 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.sormas.e2etests.envconfig.manager;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.sormas.e2etests.envconfig.configprovider.ConfigFileReader;
+import org.sormas.e2etests.envconfig.dto.EnvUser;
+import org.sormas.e2etests.envconfig.dto.Environment;
+import org.sormas.e2etests.envconfig.dto.Environments;
+
+import java.util.List;
+
+@Slf4j
+public class EnvironmentManager {
+
+    private static EnvironmentManager environmentManager;
+    private ObjectMapper objectMapper;
+    private static Environments environments;
+
+    @SneakyThrows
+    private EnvironmentManager() {
+        objectMapper = new ObjectMapper();
+        environments =
+                objectMapper.readValue(ConfigFileReader.getConfigurationFile(), Environments.class);
+    }
+
+    public static EnvironmentManager getInstance() {
+        if (environmentManager == null) environmentManager = new EnvironmentManager();
+        return environmentManager;
+    }
+
+    @SneakyThrows
+    public static String getEnvironmentUrlForMarket(String market) {
+        try {
+            return environments.getEnvironments().stream()
+                    .filter(env -> env.getLocale().equalsIgnoreCase(market))
+                    .findFirst()
+                    .get()
+                    .getUrl();
+        } catch (NullPointerException e) {
+            throw new Exception(String.format("Unable to get Environment for market: %s", market));
+        }
+    }
+
+    @SneakyThrows
+    public static EnvUser getUserByRole(String market, String role) {
+        try {
+            List<EnvUser> users = getEnvironment(market).getUsers();
+            return users.stream()
+                    .filter(user -> user.getUserRole().equalsIgnoreCase(role))
+                    .findFirst()
+                    .get();
+        } catch (NullPointerException e) {
+            throw new Exception(
+                    String.format(
+                            "Unable to get Environment User for market: %s, and role: %s", market, role));
+        }
+    }
+
+    private static Environment getEnvironment(String market) {
+        return environments.getEnvironments().stream()
+                .filter(env -> env.getLocale().equalsIgnoreCase(market))
+                .findFirst()
+                .get();
+    }
+}
+
