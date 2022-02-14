4053096dcac6c59c244e8b00d9eb1f5567023857
Stefan Kock <stefan.kock@vitagroup.ag>
2022-02-14 12:20:44 +0100

    #7978: Update h2database 1.4.200 -> 2.1.210 (WIP)
    h2database now supports:
    - array_to_string function
    - JsonBinaryType

--- sormas-backend/src/test/java/de/symeda/sormas/backend/ExtendedH2Dialect.java
+++ sormas-backend/src/test/java/de/symeda/sormas/backend/ExtendedH2Dialect.java
@@ -1,12 +1,8 @@
 package de.symeda.sormas.backend;
 
-import java.sql.Types;
-
 import org.hibernate.dialect.H2Dialect;
 import org.hibernate.dialect.function.SQLFunctionTemplate;
 import org.hibernate.dialect.function.StandardSQLFunction;
-
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import org.hibernate.type.StandardBasicTypes;
 
 public class ExtendedH2Dialect extends H2Dialect {
@@ -15,7 +11,6 @@ public class ExtendedH2Dialect extends H2Dialect {
 	public final static String WINDOW_FIRST_VALUE_DESC = "window_first_value_desc";
 	public final static String WINDOW_COUNT = "window_count";
 
-	public final static String ARRAY_TO_STRING = "array_to_string";
 	public final static String ARRAY_AGG = "array_agg";
 	public final static String CONCAT_FUNCTION = "concat_function";
 	public final static String GREATEST = "greatest";
@@ -24,10 +19,8 @@ public class ExtendedH2Dialect extends H2Dialect {
 		super();
 		// needed because of hibernate bug: https://hibernate.atlassian.net/browse/HHH-11938
 		registerFunction("regexp_replace", new StandardSQLFunction("regexp_replace"));
-		registerFunction(ARRAY_TO_STRING, new StandardSQLFunction(ARRAY_TO_STRING));
 		registerFunction(CONCAT_FUNCTION, new StandardSQLFunction("concat"));
 		registerFunction(ARRAY_AGG, new StandardSQLFunction(ARRAY_AGG));
-		registerHibernateType(Types.OTHER, JsonBinaryType.class.getName());
 		// The function unaccent is specific to PostgreSQL
 		// With H2 let's make sure it wont fail by making the function "unaccent" do nothing
 		registerFunction(UNACCENT, new SQLFunctionTemplate(StandardBasicTypes.STRING, "?1"));
