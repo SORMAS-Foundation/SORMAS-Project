ea8a63a1c07cbd5d2b6499728eef8d93b24d7a69
Bartha Barna <brtbarna@gmail.com>
2022-02-15 12:25:10 +0200

    #7815 - hide country specific columns in person selection grid - add generic solution that uses HideForCountries annotations

--- sormas-ui/src/main/java/de/symeda/sormas/ui/person/PersonSelectionGrid.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/person/PersonSelectionGrid.java
@@ -17,20 +17,21 @@
  *******************************************************************************/
 package de.symeda.sormas.ui.person;
 
+import java.util.Arrays;
 import java.util.List;
 
 import com.vaadin.v7.data.util.BeanItemContainer;
 import com.vaadin.v7.data.util.GeneratedPropertyContainer;
 import com.vaadin.v7.shared.ui.grid.HeightMode;
-import com.vaadin.v7.ui.Grid;
 
 import de.symeda.sormas.api.FacadeProvider;
 import de.symeda.sormas.api.i18n.I18nProperties;
 import de.symeda.sormas.api.person.PersonSimilarityCriteria;
 import de.symeda.sormas.api.person.SimilarPersonDto;
+import de.symeda.sormas.ui.utils.SormasGrid;
 
 @SuppressWarnings("serial")
-public class PersonSelectionGrid extends Grid {
+public class PersonSelectionGrid extends SormasGrid {
 
 	/**
 	 * Create a grid of persons listing all persons similar to the given criteria.
@@ -48,22 +49,26 @@ public class PersonSelectionGrid extends Grid {
 		GeneratedPropertyContainer generatedContainer = new GeneratedPropertyContainer(container);
 		setContainerDataSource(generatedContainer);
 
-		setColumns(
-			SimilarPersonDto.FIRST_NAME,
-			SimilarPersonDto.LAST_NAME,
-			SimilarPersonDto.NICKNAME,
-			SimilarPersonDto.AGE_AND_BIRTH_DATE,
-			SimilarPersonDto.SEX,
-			SimilarPersonDto.PRESENT_CONDITION,
-			SimilarPersonDto.PHONE,
-			SimilarPersonDto.DISTRICT_NAME,
-			SimilarPersonDto.COMMUNITY_NAME,
-			SimilarPersonDto.POSTAL_CODE,
-			SimilarPersonDto.CITY,
-			SimilarPersonDto.STREET,
-			SimilarPersonDto.HOUSE_NUMBER,
-			SimilarPersonDto.NATIONAL_HEALTH_ID,
-			SimilarPersonDto.PASSPORT_NUMBER);
+		setColumn(SimilarPersonDto.class, SimilarPersonDto.FIRST_NAME);
+
+		setSormasColumns(
+			SimilarPersonDto.class,
+			Arrays.asList(
+				SimilarPersonDto.FIRST_NAME,
+				SimilarPersonDto.LAST_NAME,
+				SimilarPersonDto.NICKNAME,
+				SimilarPersonDto.AGE_AND_BIRTH_DATE,
+				SimilarPersonDto.SEX,
+				SimilarPersonDto.PRESENT_CONDITION,
+				SimilarPersonDto.PHONE,
+				SimilarPersonDto.DISTRICT_NAME,
+				SimilarPersonDto.COMMUNITY_NAME,
+				SimilarPersonDto.POSTAL_CODE,
+				SimilarPersonDto.CITY,
+				SimilarPersonDto.STREET,
+				SimilarPersonDto.HOUSE_NUMBER,
+				SimilarPersonDto.NATIONAL_HEALTH_ID,
+				SimilarPersonDto.PASSPORT_NUMBER));
 
 		for (Column column : getColumns()) {
 			String propertyId = column.getPropertyId().toString();
@@ -73,6 +78,9 @@ public class PersonSelectionGrid extends Grid {
 
 		getColumn(SimilarPersonDto.FIRST_NAME).setMinimumWidth(150);
 		getColumn(SimilarPersonDto.LAST_NAME).setMinimumWidth(150);
+	}
+
+	private void setColumn(Class<?> clazz, String propertyId) {
 	}
 
 	@SuppressWarnings("unchecked")
