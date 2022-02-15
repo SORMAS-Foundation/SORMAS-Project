ba4ee139cf09b8e4bbbf04ec32ed8bb1446cfbbf
Pawel Kujawa <pawel.kujawa@softgent.com>
2022-02-15 12:05:09 +0100

    sorqa71

--- sormas-e2e-tests/src/test/java/org/sormas/e2etests/steps/web/application/immunizations/ImmunizationDirectorySteps.java
+++ sormas-e2e-tests/src/test/java/org/sormas/e2etests/steps/web/application/immunizations/ImmunizationDirectorySteps.java
@@ -20,7 +20,7 @@ public class ImmunizationDirectorySteps implements En {
         });
 
     When(
-        "I open last created immunization",
+        "I open last created immunization from Immunization tab",
         () -> {
           webDriverHelpers.waitForPageLoadingSpinnerToDisappear(40);
           webDriverHelpers.waitUntilElementIsVisibleAndClickable(FIRST_IMMUNIZATION_ID_BUTTON);
