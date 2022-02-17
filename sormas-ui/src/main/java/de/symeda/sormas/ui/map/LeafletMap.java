db6542259c05927a595db6864bc1cf53c1be515f
Levente Gal <levente.gal.ext@vitagroup.ag>
2022-02-17 14:55:45 +0200

    #7792 Make tile server used for map configurable - remove unnecessary comment

--- sormas-ui/src/main/java/de/symeda/sormas/ui/map/LeafletMap.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/map/LeafletMap.java
@@ -96,17 +96,6 @@ public class LeafletMap extends AbstractJavaScriptComponent {
 		// credit where credit's due
 		String attribution = FacadeProvider.getGeoShapeProvider().loadShapefileAttributions();
 		this.addShapefileAttribution(attribution);
-
-		/*
-		 * var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
-		 * attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC
-		 * BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-		 * subdomains: 'abcd',
-		 * minZoom: 0,
-		 * maxZoom: 20,
-		 * ext: 'png'
-		 * });
-		 */
 	}
 
 	/**
