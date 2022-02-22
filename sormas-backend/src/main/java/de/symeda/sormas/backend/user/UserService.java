be226647ae9461f62eaf1d60cc68178f35d9c4b2
Anthony4m <amoahanthony41@gmail.com>
2022-02-21 23:12:11 -0800

    Added generated password and update Password to user service#12880

--- sormas-backend/src/main/java/de/symeda/sormas/backend/user/UserService.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/user/UserService.java
@@ -1,6 +1,6 @@
 /*
  * SORMAS® - Surveillance Outbreak Response Management & Analysis System
- * Copyright © 2016-2022 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+ * Copyright © 2016-2018 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -646,6 +646,22 @@ public class UserService extends AdoServiceWithUserFilterAndJurisdiction<User> {
 
 		return password;
 	}
+
+	public String generatePassword() {
+		return PasswordHelper.createPass(12);
+	}
+
+	public String updatePassword(String userUuid, String password) {
+		User user = getByUuid(userUuid);
+
+		if (user == null && password == null) {
+			return null;
+		}
+		assert user != null;
+		user.setPassword(PasswordHelper.encodePassword(password, user.getSeed()));
+		ensurePersisted(user);
+		return password;
+	};
 
 	public Predicate buildCriteriaFilter(UserCriteria userCriteria, CriteriaBuilder cb, Root<User> from) {
 
