e501765b8dad7142d78aaae4126926ecb053cf6c
Anthony4m <amoahanthony41@gmail.com>
2022-02-21 23:28:57 -0800

    Added a method to generate password      Added a method to validate password      Added a method to check password strength#12881

--- sormas-backend/src/main/java/de/symeda/sormas/backend/user/UserFacadeEjb.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/user/UserFacadeEjb.java
@@ -141,6 +141,18 @@ import de.symeda.sormas.backend.util.DtoHelper;
 import de.symeda.sormas.backend.util.ModelConstants;
 import de.symeda.sormas.backend.util.QueryHelper;
 import de.symeda.sormas.backend.util.RightsAllowed;
+import edu.vt.middleware.password.CharacterCharacteristicsRule;
+import edu.vt.middleware.password.DigitCharacterRule;
+import edu.vt.middleware.password.LengthRule;
+import edu.vt.middleware.password.LowercaseCharacterRule;
+import edu.vt.middleware.password.NonAlphanumericCharacterRule;
+import edu.vt.middleware.password.Password;
+import edu.vt.middleware.password.PasswordData;
+import edu.vt.middleware.password.PasswordValidator;
+import edu.vt.middleware.password.Rule;
+import edu.vt.middleware.password.RuleResult;
+import edu.vt.middleware.password.UppercaseCharacterRule;
+import edu.vt.middleware.password.WhitespaceRule;
 
 @Stateless(name = "UserFacade")
 public class UserFacadeEjb implements UserFacade {
@@ -851,6 +863,35 @@ public class UserFacadeEjb implements UserFacade {
 		return toDto(userService.getCurrentUser());
 	}
 
+	@PermitAll
+	@AuditIgnore
+	public String updatePassword(String uuid, String password) {
+		String updatePassword = userService.updatePassword(uuid, password);
+		passwordResetEvent.fire(new PasswordResetEvent(userService.getByUuid(uuid)));
+
+		return updatePassword;
+	}
+
+	@Override
+	public String updateUserPassword(String uuid, String newPassword, String currentPassword) {
+		if (!validatePassword(uuid, currentPassword)) {
+			return null;
+		} else {
+			String updatePassword = userService.updatePassword(uuid, newPassword);
+			passwordResetEvent.fire(new PasswordResetEvent(userService.getByUuid(uuid)));
+
+			return updatePassword;
+		}
+	}
+
+	@Override
+	public String generatePassword() {
+		return userService.generatePassword();
+
+	}
+
+	@Override
+	
 	@Override
 	@PermitAll
 	public UserReferenceDto getCurrentUserAsReference() {
@@ -861,7 +902,15 @@ public class UserFacadeEjb implements UserFacade {
 	@PermitAll
 	@AuditIgnore
 	public Set<UserRight> getValidLoginRights(String userName, String password) {
+			User user = userService.getByUserName(userName);
+		if (user != null && user.isActive() && DataHelper.equal(user.getPassword(), PasswordHelper.encodePassword(password, user.getSeed()))) {
+			return new HashSet<>(UserRole.getUserRights(user.getUserRoles()));
+		}
 
+		return null;
+	}
+
+	public Set<UserRole> getValidLoginRoles(String userName, String password) {
 		User user = userService.getByUserName(userName);
 		if (user != null && user.isActive() && DataHelper.equal(user.getPassword(), PasswordHelper.encodePassword(password, user.getSeed()))) {
 			return new HashSet<>(UserRole.getUserRights(user.getUserRoles()));
@@ -872,6 +921,119 @@ public class UserFacadeEjb implements UserFacade {
 
 	@Override
 	@RightsAllowed(UserRight._USER_EDIT)
+	public boolean validatePassword(String uuid, String password) {
+		User user = userService.getCurrentUser();
+		if (user != null) {
+			if (DataHelper.equal(user.getPassword(), PasswordHelper.encodePassword(password, user.getSeed()))) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean validatePasswordPattern(String password) {
+		//Password must be between 8 and
+		LengthRule lengthRule = new LengthRule(8, 16);
+		// don't allow whitespace
+		WhitespaceRule whitespaceRule = new WhitespaceRule();
+		// control allowed characters
+		CharacterCharacteristicsRule charRule = new CharacterCharacteristicsRule();
+		// require at least 1 digit in passwords
+		charRule.getRules().add(new DigitCharacterRule(1));
+		// require at least 1 non-alphanumeric char
+		charRule.getRules().add(new NonAlphanumericCharacterRule(1));
+		// require at least 1 upper case char
+		charRule.getRules().add(new UppercaseCharacterRule(1));
+		// require at least 1 lower case char
+		charRule.getRules().add(new LowercaseCharacterRule(1));
+		// require at least 3 of the previous rules be met
+		charRule.setNumberOfCharacteristics(4);
+
+		List<Rule> ruleList = new ArrayList<Rule>();
+
+		ruleList.add(lengthRule);
+		ruleList.add(whitespaceRule);
+		ruleList.add(charRule);
+		PasswordValidator validator = new PasswordValidator(ruleList);
+		PasswordData passwordData = new PasswordData(new Password(password));
+		RuleResult result = validator.validate(passwordData);
+
+		if (result.isValid()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public String checkPasswordStrength(String password) {
+		//Password must be between 8 and
+		LengthRule strongPasswordlengthRule = new LengthRule(10, 64);
+
+		LengthRule moderatePasswordlengthRule = new LengthRule(8, 16);
+		// don't allow whitespace
+		WhitespaceRule whitespaceRule = new WhitespaceRule();
+		/*-----STrong password-----*/
+		// control allowed characters
+		CharacterCharacteristicsRule strongPassword = new CharacterCharacteristicsRule();
+		// require at least 2 digit in passwords
+		strongPassword.getRules().add(new DigitCharacterRule(2));
+		// require at least 2 non-alphanumeric char
+		strongPassword.getRules().add(new NonAlphanumericCharacterRule(2));
+		// require at least 1 upper case char
+		strongPassword.getRules().add(new UppercaseCharacterRule(1));
+		// require at least 1 lower case char
+		strongPassword.getRules().add(new LowercaseCharacterRule(1));
+
+		// require at least 6 of the previous rules be met
+		strongPassword.setNumberOfCharacteristics(4);
+		//Check to see all rules are met for a strong
+		List<Rule> strongPasswordRuleList = new ArrayList<Rule>();
+		strongPasswordRuleList.add(strongPasswordlengthRule);
+		strongPasswordRuleList.add(whitespaceRule);
+		strongPasswordRuleList.add(strongPassword);
+		PasswordValidator strongPasswordValidator = new PasswordValidator(strongPasswordRuleList);
+		PasswordData strongPasswordData = new PasswordData(new Password(password));
+		RuleResult strongPasswordresult = strongPasswordValidator.validate(strongPasswordData);
+
+		/*-----moderate password-----*/
+		// control allowed characters
+		CharacterCharacteristicsRule moderatePassword = new CharacterCharacteristicsRule();
+		// require at least 1 digit in passwords
+		moderatePassword.getRules().add(new DigitCharacterRule(1));
+		// require at least 1 non-alphanumeric char
+		moderatePassword.getRules().add(new NonAlphanumericCharacterRule(1));
+		// require at least 1 upper case char
+		moderatePassword.getRules().add(new UppercaseCharacterRule(1));
+		// require at least 1 lower case char
+		moderatePassword.getRules().add(new LowercaseCharacterRule(1));
+
+		// require at least 6 of the previous rules be met
+		strongPassword.setNumberOfCharacteristics(3);
+		//Check to see all rules are met for a strong
+		List<Rule> moderatePasswordRuleList = new ArrayList<Rule>();
+		moderatePasswordRuleList.add(moderatePasswordlengthRule);
+		moderatePasswordRuleList.add(whitespaceRule);
+		moderatePasswordRuleList.add(moderatePassword);
+		PasswordValidator moderatePasswordValidator = new PasswordValidator(moderatePasswordRuleList);
+		PasswordData moderatePasswordData = new PasswordData(new Password(password));
+		RuleResult moderatePasswordresult = moderatePasswordValidator.validate(moderatePasswordData);
+
+		String passStrength = "Password Strength is Weak";
+		if (strongPasswordresult.isValid()) {
+			passStrength = "Password Strength is Strong";
+			return passStrength;
+
+		} else if (moderatePasswordresult.isValid()) {
+			passStrength = "Password Strength is Moderate";
+			return passStrength;
+
+		} else
+			return passStrength;
+	};
+
+	@Override
 	public void removeUserAsSurveillanceAndContactOfficer(String userUuid) {
 		CriteriaBuilder cb = em.getCriteriaBuilder();
 		CriteriaQuery<Case> caseQuery = cb.createQuery(Case.class);
