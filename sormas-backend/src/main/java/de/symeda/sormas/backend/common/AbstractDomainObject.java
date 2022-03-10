cbb5e5718b9be368527d511ce4e417c734cafc75
Jonas Cirotzki <jonas.cirotzki@posteo.de>
2022-03-10 16:23:49 +0100

    [#7323] reformat

--- sormas-backend/src/main/java/de/symeda/sormas/backend/common/AbstractDomainObject.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/common/AbstractDomainObject.java
@@ -23,20 +23,31 @@ import java.time.Instant;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import javax.persistence.*;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Version;
 import javax.validation.constraints.Size;
 
-import com.vladmihalcea.hibernate.type.json.JsonType;
-import de.symeda.sormas.api.ConfigFacade;
-import de.symeda.sormas.backend.user.CurrentUserService;
-import de.symeda.sormas.backend.user.User;
-import de.symeda.sormas.backend.user.UserCache;
 import org.hibernate.annotations.TypeDef;
 
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import com.vladmihalcea.hibernate.type.json.JsonType;
 
 import de.symeda.sormas.api.HasUuid;
 import de.symeda.sormas.api.utils.DataHelper;
+import de.symeda.sormas.backend.user.CurrentUserService;
+import de.symeda.sormas.backend.user.User;
 
 /**
  * Note: The hibernate-types article suggests to use JsonBinaryType for Postgres, but doesn't explain why.
