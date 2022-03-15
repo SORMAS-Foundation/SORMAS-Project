02d565a3e6e96e7694bee497635ff083235b6056
Levente Gal <levente.gal.ext@vitagroup.ag>
2022-03-15 16:52:52 +0200

    #445 Add user right & role checks for cases to backend

--- sormas-api/src/main/java/de/symeda/sormas/api/i18n/Strings.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/i18n/Strings.java
@@ -284,6 +284,8 @@ public interface Strings {
 	String errorNoAccessToWeb = "errorNoAccessToWeb";
 	String errorNoPopulationDataFound = "errorNoPopulationDataFound";
 	String errorNoPopulationDataLocations = "errorNoPopulationDataLocations";
+	String errorNoRightsForChangingField = "errorNoRightsForChangingField";
+	String errorNoRightsForChangingMultipleFields = "errorNoRightsForChangingMultipleFields";
 	String errorNotRequiredRights = "errorNotRequiredRights";
 	String errorOccurred = "errorOccurred";
 	String errorProblemOccurred = "errorProblemOccurred";
