f5e45b2f61f5b362b3e41ac17cf0b56dad25d469
Alexandru Caruntu <alexandru.caruntu.ext@vitagroup.ag>
2022-03-17 14:33:47 +0200

    #8191 - Update follow up calculations when deleting a sample

--- sormas-ui/src/main/java/de/symeda/sormas/ui/samples/SampleController.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/samples/SampleController.java
@@ -319,17 +319,7 @@ public class SampleController {
 				FacadeProvider.getSampleFacade().saveSample(changedDto);
 				SormasUI.refreshView();
 
-				final CaseReferenceDto associatedCase = changedDto.getAssociatedCase();
-				if (associatedCase != null) {
-					final CaseDataDto caseDataByUuid = FacadeProvider.getCaseFacade().getCaseDataByUuid(associatedCase.getUuid());
-					FacadeProvider.getCaseFacade().save(caseDataByUuid);
-				}
-
-				final ContactReferenceDto associatedContact = changedDto.getAssociatedContact();
-				if (associatedContact != null) {
-					final ContactDto contactDataByUuid = FacadeProvider.getContactFacade().getByUuid(associatedContact.getUuid());
-					FacadeProvider.getContactFacade().save(contactDataByUuid);
-				}
+				updateAssociationsForSample(changedDto);
 
 				if (changedDto.getSpecimenCondition() != originalDto.getSpecimenCondition()
 					&& changedDto.getSpecimenCondition() == SpecimenCondition.NOT_ADEQUATE
@@ -344,6 +334,7 @@ public class SampleController {
 		if (showDeleteButton && UserProvider.getCurrent().hasUserRight(UserRight.SAMPLE_DELETE)) {
 			editView.addDeleteListener(() -> {
 				FacadeProvider.getSampleFacade().deleteSample(dto.toReference());
+				updateAssociationsForSample(dto);
 				UI.getCurrent().getNavigator().navigateTo(SamplesView.VIEW_NAME);
 			}, I18nProperties.getString(Strings.entitySample));
 		}
@@ -353,6 +344,20 @@ public class SampleController {
 		}
 
 		return editView;
+	}
+
+	private void updateAssociationsForSample(SampleDto sampleDto) {
+		final CaseReferenceDto associatedCase = sampleDto.getAssociatedCase();
+		if (associatedCase != null) {
+			final CaseDataDto caseDataByUuid = FacadeProvider.getCaseFacade().getCaseDataByUuid(associatedCase.getUuid());
+			FacadeProvider.getCaseFacade().save(caseDataByUuid);
+		}
+
+		final ContactReferenceDto associatedContact = sampleDto.getAssociatedContact();
+		if (associatedContact != null) {
+			final ContactDto contactDataByUuid = FacadeProvider.getContactFacade().getByUuid(associatedContact.getUuid());
+			FacadeProvider.getContactFacade().save(contactDataByUuid);
+		}
 	}
 
 	/**
