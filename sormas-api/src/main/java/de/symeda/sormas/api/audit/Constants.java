8afeb79ab3b20977e51b6fd8f403e0f1553b3a2d
Jonas Cirotzki <jonas.cirotzki@posteo.de>
2022-03-18 12:25:33 +0100

    [#8025] make constants unmodifiable

--- sormas-api/src/main/java/de/symeda/sormas/api/audit/Constants.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/audit/Constants.java
@@ -1,51 +1,72 @@
 package de.symeda.sormas.api.audit;
 
+import org.apache.commons.collections.set.UnmodifiableSet;
+
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Constants {
 
-	public static final Set<String> createPrefix = new HashSet<>(Arrays.asList("create", "generate", "build", "clone", "calculate"));
-	public static final Set<String> readPrefix = new HashSet<>(
-		Arrays
-			.asList("count", "get", "is", "has", "does", "validate", "exists", "read", "import", "find", "query", "load", "check", "uses", "fetch"));
-	public static final Set<String> updatePrefix = new HashSet<>(
-		Arrays.asList(
-			"update",
-			"post",
-			"set",
-			"archive",
-			"dearchive",
-			"save",
-			"overwrite",
-			"convert",
-			"link",
-			"bulkAssign",
-			"write",
-			"cleanup",
-			"mark"));
-	public static final Set<String> deletePrefix = new HashSet<>(Arrays.asList("delete", "merge", "remove"));
-	public static final Set<String> executePrefix = new HashSet<>(
-		Arrays.asList(
-			"send",
-			"start",
-			"share",
-			"signAndEncrypt",
-			"decryptAndVerify",
-			"reject",
-			"report",
-			"unlink",
-			"notify",
-			"register",
-			"cancel",
-			"end",
-			"accept",
-			"sync",
-			"request",
-			"revoke",
-			"reset",
-			"enable",
-			"disable"));
+	public static final Set<String> createPrefix =
+		Collections.unmodifiableSet(new HashSet<>(Arrays.asList("create", "generate", "build", "clone", "calculate")));
+	public static final Set<String> readPrefix = Collections.unmodifiableSet(
+		new HashSet<>(
+			Arrays.asList(
+				"count",
+				"get",
+				"is",
+				"has",
+				"does",
+				"validate",
+				"exists",
+				"read",
+				"import",
+				"find",
+				"query",
+				"load",
+				"check",
+				"uses",
+				"fetch")));
+	public static final Set<String> updatePrefix = Collections.unmodifiableSet(
+		new HashSet<>(
+			Arrays.asList(
+				"update",
+				"post",
+				"set",
+				"archive",
+				"dearchive",
+				"save",
+				"overwrite",
+				"convert",
+				"link",
+				"bulkAssign",
+				"write",
+				"cleanup",
+				"mark")));
+	public static final Set<String> deletePrefix = Collections.unmodifiableSet(new HashSet<>(Arrays.asList("delete", "merge", "remove")));
+	public static final Set<String> executePrefix = Collections.unmodifiableSet(
+		new HashSet<>(
+			Arrays.asList(
+				"send",
+				"start",
+				"share",
+				"signAndEncrypt",
+				"decryptAndVerify",
+				"reject",
+				"report",
+				"unlink",
+				"notify",
+				"register",
+				"cancel",
+				"end",
+				"accept",
+				"sync",
+				"request",
+				"revoke",
+				"reset",
+				"enable",
+				"disable")));
 
 }
