a7ea40ab76b72f91468d7d801cef102e5453af23
Jonas Cirotzki <jonas.cirotzki@posteo.de>
2022-03-18 16:36:38 +0100

    [#8025] fix imports

--- sormas-backend/src/main/java/de/symeda/sormas/backend/audit/AuditLogger.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/audit/AuditLogger.java
@@ -31,7 +31,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TimeZone;
-import java.util.concurrent.ConcurrentHashMap;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
