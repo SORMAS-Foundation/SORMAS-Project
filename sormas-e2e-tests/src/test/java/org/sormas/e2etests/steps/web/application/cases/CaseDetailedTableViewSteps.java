7389e8b33a779f39d78a2bcd03df2d066b6054c5
jparzuch <jedrzej.parzuchowski@softgent.com>
2022-03-21 11:48:20 +0100

    Tweaked selector, added sleep

--- sormas-e2e-tests/src/test/java/org/sormas/e2etests/steps/web/application/cases/CaseDetailedTableViewSteps.java
+++ sormas-e2e-tests/src/test/java/org/sormas/e2etests/steps/web/application/cases/CaseDetailedTableViewSteps.java
@@ -15,6 +15,7 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
@@ -206,8 +207,9 @@ public class CaseDetailedTableViewSteps implements En {
         "I double-click on any field in the first row from Case Directory that is not Person ID",
         () -> {
           if (webDriverHelpers.isElementVisibleWithTimeout(
-              By.xpath("//*[contains(text(),'Confirm navigation')]"), 5)) {
+              By.xpath("[contains(text(),'Confirm navigation')]"), 5)) {
             webDriverHelpers.clickOnWebElementBySelector(By.id("actionCancel"));
+            TimeUnit.SECONDS.sleep(2);
           }
           webDriverHelpers.doubleClickOnWebElementBySelector(FIRST_ROW);
         });
