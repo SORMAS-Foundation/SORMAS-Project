25d1ed3161251522eb3b7da2ef4770699de1a530
marius <dmarius23@gmail.com>
2022-03-22 08:44:04 +0200

    #8132 rename endpoint

--- sormas-rest/src/main/java/de/symeda/sormas/rest/ImmunizationResource.java
+++ sormas-rest/src/main/java/de/symeda/sormas/rest/ImmunizationResource.java
@@ -153,7 +153,7 @@ public class ImmunizationResource extends EntityDtoResource {
 	}
 
 	@POST
-	@Path("/pushVaccination")
+	@Path("/vaccination/push")
 	public VaccinationDto postVaccination(@Valid VaccinationDto vaccination) {
 		return FacadeProvider.getVaccinationFacade().save(vaccination);
 	}
