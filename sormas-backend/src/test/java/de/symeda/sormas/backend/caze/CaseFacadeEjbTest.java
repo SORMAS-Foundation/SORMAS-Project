26854aec27a8db61be5c916f76d25bb5ccbe9123
Maté Strysewske <mate.strysewske@vitagroup.ag>
2022-03-24 10:18:36 +0100

    #7942 - Test fixes

--- sormas-backend/src/test/java/de/symeda/sormas/backend/caze/CaseFacadeEjbTest.java
+++ sormas-backend/src/test/java/de/symeda/sormas/backend/caze/CaseFacadeEjbTest.java
@@ -893,14 +893,14 @@ public class CaseFacadeEjbTest extends AbstractBeanTest {
 
 		getCaseFacade().delete(caze.getUuid());
 
-		// Deleted flag should be set for case, contact, sample and pathogen test; Task and additional test should be deleted
+		// Deleted flag should be set for case, sample and pathogen test; Additional test should be deleted; Contact should not have the deleted flag; Task should not be deleted
 		assertTrue(getCaseFacade().getDeletedUuidsSince(since).contains(caze.getUuid()));
-		assertTrue(getContactFacade().getDeletedUuidsSince(since).contains(contact.getUuid()));
+		assertFalse(getContactFacade().getDeletedUuidsSince(since).contains(contact.getUuid()));
 		assertTrue(getSampleFacade().getDeletedUuidsSince(since).contains(sample.getUuid()));
 		assertFalse(getSampleFacade().getDeletedUuidsSince(since).contains(sampleAssociatedToContactAndCase.getUuid()));
 		assertTrue(getSampleTestFacade().getDeletedUuidsSince(since).contains(pathogenTest.getUuid()));
 		assertNull(getAdditionalTestFacade().getByUuid(additionalTest.getUuid()));
-		assertNull(getTaskFacade().getByUuid(task.getUuid()));
+		assertNotNull(getTaskFacade().getByUuid(task.getUuid()));
 	}
 
 	@Test
