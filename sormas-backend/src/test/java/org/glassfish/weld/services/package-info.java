0649093a3c3340e21220185f3b5b4ce0bfd896d5
Stefan Kock <stefan.kock@vitagroup.ag>
2022-03-24 10:04:05 +0100

    #7705: Copied InjectionServicesImpl (payara-5.2021.10)

---
+++ sormas-backend/src/test/java/org/glassfish/weld/services/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Used to create a changed class file to be injected into payara module <em>fish.payara.server.internal.web:weld-integration</em>.
+ * <ol>
+ * <li>https://github.com/payara/Payara/blob/master/appserver/web/weld-integration/src/main/java/org/glassfish/weld/services/InjectionServicesImpl.java</li>
+ * </ol>
+ */
+package org.glassfish.weld.services;
