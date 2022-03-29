410f83a3565e2c495290b90234a8ae674a344bb7
Alexandru Caruntu <alexandru.caruntu.ext@vitagroup.ag>
2022-03-29 17:42:23 +0300

    #8550 - Cleanup

--- sormas-backend/src/main/java/de/symeda/sormas/backend/caze/CaseFacadeEjb.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/caze/CaseFacadeEjb.java
@@ -1618,7 +1618,6 @@ public class CaseFacadeEjb extends AbstractCoreFacadeEjb<Case, CaseDataDto, Case
 			// The samples for case are not persisted yet, so use the samples from contact since they are the same
 			Date untilDate = service.computeFollowUpuntilDate(caze, contact.getSamples());
 			caze.setFollowUpUntil(untilDate);
-			service.ensurePersisted(caze);
 		}
 	}
 
