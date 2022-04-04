66bf6660e96497fcd4bf0f9c5ebf500d174c478d
Levente Gal <levente.gal.ext@vitagroup.ag>
2022-04-04 09:31:12 +0300

    #8508 Add backend checks for contact, visit and missing case-related user rights - split tests

--- sormas-backend/src/test/java/de/symeda/sormas/backend/ArchitectureTest.java
+++ sormas-backend/src/test/java/de/symeda/sormas/backend/ArchitectureTest.java
@@ -1,8 +1,5 @@
 package de.symeda.sormas.backend;
 
-import com.tngtech.archunit.lang.syntax.elements.GivenMethodsConjunction;
-import de.symeda.sormas.backend.caze.caseimport.CaseImportFacadeEjb;
-import de.symeda.sormas.backend.externaljournal.ExternalJournalFacadeEjb;
 import javax.annotation.security.RolesAllowed;
 
 import org.junit.runner.RunWith;
@@ -13,13 +10,16 @@ import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.junit.ArchUnitRunner;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import com.tngtech.archunit.lang.syntax.elements.GivenMethodsConjunction;
 
 import de.symeda.sormas.api.FacadeProvider;
 import de.symeda.sormas.backend.bagexport.BAGExportFacadeEjb;
 import de.symeda.sormas.backend.caze.CaseFacadeEjb;
+import de.symeda.sormas.backend.caze.caseimport.CaseImportFacadeEjb;
 import de.symeda.sormas.backend.caze.surveillancereport.SurveillanceReportFacadeEjb;
 import de.symeda.sormas.backend.clinicalcourse.ClinicalVisitFacadeEjb;
 import de.symeda.sormas.backend.contact.ContactFacadeEjb;
+import de.symeda.sormas.backend.externaljournal.ExternalJournalFacadeEjb;
 import de.symeda.sormas.backend.therapy.PrescriptionFacadeEjb;
 import de.symeda.sormas.backend.therapy.TreatmentFacadeEjb;
 import de.symeda.sormas.backend.visit.VisitFacadeEjb;
@@ -35,17 +35,56 @@ public class ArchitectureTest {
 		ArchRuleDefinition.theClass(FacadeProvider.class).should().onlyBeAccessed().byClassesThat().belongToAnyOf(FacadeProvider.class);
 
 	@ArchTest
-	public void testFacadeEjbAuthorization(JavaClasses classes) {
+	public void testCaseFacadeEjbAuthorization(JavaClasses classes) {
 		assertFacadeEjbAnnotated(CaseFacadeEjb.class, classes);
+	}
+
+	@ArchTest
+	public void testContactFacadeEjbAuthorization(JavaClasses classes) {
+
 		assertFacadeEjbAnnotated(ContactFacadeEjb.class, classes);
+	}
+
+	@ArchTest
+	public void testVisitFacadeEjbAuthorization(JavaClasses classes) {
 		assertFacadeEjbAnnotated(VisitFacadeEjb.class, classes);
+	}
+
+	@ArchTest
+	public void testBAGExportFacadeEjbAuthorization(JavaClasses classes) {
 		assertFacadeEjbAnnotated(BAGExportFacadeEjb.class, classes);
+	}
+
+	@ArchTest
+	public void testCaseImportFacadeEjbAuthorization(JavaClasses classes) {
 		assertFacadeEjbAnnotated(CaseImportFacadeEjb.class, true, classes);
+	}
+
+	@ArchTest
+	public void testExternalJournalFacadeEjbAuthorization(JavaClasses classes) {
 		assertFacadeEjbAnnotated(ExternalJournalFacadeEjb.class, true, classes);
+	}
+
+	@ArchTest
+	public void testPrescriptionFacadeEjbAuthorization(JavaClasses classes) {
 		assertFacadeEjbAnnotated(PrescriptionFacadeEjb.class, classes);
+	}
+
+	@ArchTest
+	public void testTreatmentFacadeEjbAuthorization(JavaClasses classes) {
 		assertFacadeEjbAnnotated(TreatmentFacadeEjb.class, classes);
+
+	}
+
+	@ArchTest
+	public void testSurveillanceReportFacadeEjbAuthorization(JavaClasses classes) {
 		assertFacadeEjbAnnotated(SurveillanceReportFacadeEjb.class, classes);
+	}
+
+	@ArchTest
+	public void testClinicalVisitFacadeEjbAuthorization(JavaClasses classes) {
 		assertFacadeEjbAnnotated(ClinicalVisitFacadeEjb.class, classes);
+
 	}
 
 	private void assertFacadeEjbAnnotated(Class<?> facadeEjbClass, JavaClasses classes) {
@@ -55,21 +94,16 @@ public class ArchitectureTest {
 	private void assertFacadeEjbAnnotated(Class<?> facadeEjbClass, boolean classAuthOnly, JavaClasses classes) {
 		ArchRuleDefinition.theClass(facadeEjbClass).should().beAnnotatedWith(RolesAllowed.class).check(classes);
 
-		GivenMethodsConjunction methods = ArchRuleDefinition.methods()
-				.that()
-				.areDeclaredIn(facadeEjbClass)
-				.and()
-				.arePublic();
+		GivenMethodsConjunction methods = ArchRuleDefinition.methods().that().areDeclaredIn(facadeEjbClass).and().arePublic();
 
-		if(classAuthOnly){
+		if (classAuthOnly) {
 			methods.should().notBeAnnotatedWith(RolesAllowed.class);
 		} else {
-			methods
-					.should()
-					.beAnnotatedWith(RolesAllowed.class)
-					.orShould()
-					.haveNameMatching("^(get|count|is|does|has|validate|to|pseudonymize|convertToReferenceDto|fillOrBuild|convertToDto|fromDto).*")
-					.check(classes);
+			methods.should()
+				.beAnnotatedWith(RolesAllowed.class)
+				.orShould()
+				.haveNameMatching("^(get|count|is|does|has|validate|to|pseudonymize|convertToReferenceDto|fillOrBuild|convertToDto|fromDto).*")
+				.check(classes);
 		}
 	}
 }
