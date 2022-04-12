2177392fa5c3e71a8cfe2ba6bfc4657c9a8d0c7d
dinua <adrian.dinu@she.net>
2022-04-12 17:03:00 +0300

    #8768 add tests

--- sormas-backend/src/test/java/de/symeda/sormas/backend/TestDataCreator.java
+++ sormas-backend/src/test/java/de/symeda/sormas/backend/TestDataCreator.java
@@ -827,6 +827,19 @@ public class TestDataCreator {
 		return createTask(TaskContext.GENERAL, TaskType.OTHER, TaskStatus.PENDING, null, null, null, new Date(), assigneeUser);
 	}
 
+	public TaskDto createTask(TaskContext context, ReferenceDto entityRef, Consumer<TaskDto> customConfig) {
+
+		TaskDto task = TaskDto.build(context, entityRef);
+
+		if (customConfig != null) {
+			customConfig.accept(task);
+		}
+
+		task = beanTest.getTaskFacade().saveTask(task);
+
+		return task;
+	}
+
 	public TaskDto createTask(
 		TaskContext context,
 		TaskType type,
