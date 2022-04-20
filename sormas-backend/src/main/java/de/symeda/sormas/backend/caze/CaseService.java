35f754210a422305167e4897fe70d89e80ee3b1e
Maté Strysewske <mate.strysewske@vitagroup.ag>
2022-04-20 11:55:57 +0200

    #8832 - Assign case jurisdiction to contacts

--- sormas-backend/src/main/java/de/symeda/sormas/backend/caze/CaseService.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/caze/CaseService.java
@@ -987,6 +987,14 @@ public class CaseService extends AbstractCoreAdoService<Case> {
 		// Remove the case as the resulting case and source case from all contacts
 		Optional.ofNullable(caze.getContacts()).ifPresent(cl -> cl.forEach(c -> {
 			c.setCaze(null);
+
+			// Assign the case jurisdiction to the contact if it does not already have one
+			if (c.getDistrict() == null) {
+				c.setRegion(caze.getResponsibleRegion());
+				c.setDistrict(caze.getResponsibleDistrict());
+				c.setCommunity(caze.getResponsibleCommunity());
+			}
+
 			externalJournalService.handleExternalJournalPersonUpdateAsync(c.getPerson().toReference());
 			contactService.ensurePersisted(c);
 		}));
