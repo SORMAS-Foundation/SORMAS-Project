da83fb9a6fa9417a3f968a9d0655183585d4119c
sergiupacurariu <62688603+sergiupacurariu@users.noreply.github.com>
2022-04-28 13:08:22 +0300

    #7876 - Converting a Contact to a Case | Follow-up Comment | Having the Follow-up field filled to it's limit (4096 characters) will produce a server error

--- sormas-ui/src/main/java/de/symeda/sormas/ui/contact/ContactDataForm.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/contact/ContactDataForm.java
@@ -26,9 +26,6 @@ import static de.symeda.sormas.ui.utils.LayoutUtil.fluidRowLocs;
 import static de.symeda.sormas.ui.utils.LayoutUtil.loc;
 import static de.symeda.sormas.ui.utils.LayoutUtil.locCss;
 
-import java.math.BigDecimal;
-import java.math.MathContext;
-import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
