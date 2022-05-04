3d2e8995e8c5a8a8608b887adc9996da37427b77
Michal Kozakiewicz <michal.kozakiewicz@softgent.com>
2022-05-04 15:34:48 +0200

    actualized scenario [SORDEV-9477]

--- sormas-e2e-tests/src/test/java/org/sormas/e2etests/steps/web/application/cases/CreateNewCaseSteps.java
+++ sormas-e2e-tests/src/test/java/org/sormas/e2etests/steps/web/application/cases/CreateNewCaseSteps.java
@@ -492,7 +492,7 @@ public class CreateNewCaseSteps implements En {
     When(
         "^I Pick an existing case in Pick or create person popup in Case entry$",
         () -> {
-          webDriverHelpers.clickOnWebElementBySelector(CREATE_A_NEW_CASE);
+          webDriverHelpers.clickOnWebElementBySelector(CREATE_A_NEW_CASE_CONFIRMATION_BUTTON);
           webDriverHelpers.clickOnWebElementBySelector(SAVE_BUTTON);
         });
 
