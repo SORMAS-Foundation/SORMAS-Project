5f6fab48fd1826f357ae9ae2ca6d92c2449982d9
Pawel Kujawa <pawel.kujawa@softgent.com>
2022-05-30 12:08:04 +0200

    SORQA-287 fix

--- sormas-e2e-tests/src/test/java/org/sormas/e2etests/steps/web/application/tasks/TaskManagementSteps.java
+++ sormas-e2e-tests/src/test/java/org/sormas/e2etests/steps/web/application/tasks/TaskManagementSteps.java
@@ -92,9 +92,9 @@ public class TaskManagementSteps implements En {
           webDriverHelpers.waitUntilIdentifiedElementIsVisibleAndClickable(
               ASSIGNED_USER_FILTER_INPUT);
           String assignedUser = CreateNewTaskSteps.task.getAssignedTo();
-          int indexToSubstring = assignedUser.indexOf("-");
           webDriverHelpers.fillInWebElement(
-              ASSIGNED_USER_FILTER_INPUT, assignedUser.substring(0, indexToSubstring).trim());
+              ASSIGNED_USER_FILTER_INPUT,
+              assignedUser);
           webDriverHelpers.clickOnWebElementBySelector(APPLY_FILTER);
           webDriverHelpers.waitUntilIdentifiedElementIsVisibleAndClickable(lastTaskEditButton, 40);
           webDriverHelpers.clickElementSeveralTimesUntilNextElementIsDisplayed(
