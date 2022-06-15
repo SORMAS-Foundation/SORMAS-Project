8cde6481314059fd05cf8b1dcb3a161b3f5d64ed
Carina Paul <carina.paul@she.net>
2022-06-15 18:45:01 +0300

    #9274- stub ExternalSurveillanceToolGateway's export and delete request

--- sormas-backend/src/test/java/de/symeda/sormas/backend/dashboard/DashboardServiceTest.java
+++ sormas-backend/src/test/java/de/symeda/sormas/backend/dashboard/DashboardServiceTest.java
@@ -1,10 +1,23 @@
 package de.symeda.sormas.backend.dashboard;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.containing;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.Map;
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import de.symeda.sormas.api.externalsurveillancetool.ExternalSurveillanceToolFacade;
+import de.symeda.sormas.backend.MockProducer;
+import org.apache.http.HttpStatus;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 import de.symeda.sormas.api.common.DeletionDetails;
@@ -18,6 +31,30 @@ import de.symeda.sormas.backend.TestDataCreator;
 import de.symeda.sormas.backend.event.EventFacadeEjb;
 
 public class DashboardServiceTest extends AbstractBeanTest {
+
+    private static final int WIREMOCK_TESTING_PORT = 8888;
+    private ExternalSurveillanceToolFacade subjectUnderTest;
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(options().port(WIREMOCK_TESTING_PORT), false);
+
+    @Before
+    public void setup() {
+        configureExternalSurvToolUrlForWireMock();
+        subjectUnderTest = getExternalSurveillanceToolGatewayFacade();
+    }
+
+    @After
+    public void teardown() {
+        clearExternalSurvToolUrlForWireMock();
+    }
+
+    private void configureExternalSurvToolUrlForWireMock() {
+        MockProducer.getProperties().setProperty("survnet.url", String.format("http://localhost:%s", WIREMOCK_TESTING_PORT));
+    }
+    private void clearExternalSurvToolUrlForWireMock() {
+        MockProducer.getProperties().setProperty("survnet.url", "");
+    }
 
 	@Test
 	public void testGetEventCountByStatusWithArchivingAndDeletion() {
@@ -39,6 +76,14 @@ public class DashboardServiceTest extends AbstractBeanTest {
 		assertEquals(Long.valueOf(1), result.get(EventStatus.SCREENING));
 		assertEquals(Long.valueOf(1), result.get(EventStatus.CLUSTER));
 
+        stubFor(
+                post(urlEqualTo("/export")).withRequestBody(containing(signal.getUuid()))
+                        .withRequestBody(containing(event1.getUuid()))
+                        .withRequestBody(containing(screening.getUuid()))
+                        .withRequestBody(containing(cluster.getUuid()))
+                        .withRequestBody(containing("eventUuids"))
+                        .willReturn(aResponse().withStatus(HttpStatus.SC_OK)));
+
 		// archive events (should not have any effect on result)
 		eventFacade.archive(Arrays.asList(signal.getUuid(), event1.getUuid(), screening.getUuid(), cluster.getUuid()));
 
@@ -51,6 +96,15 @@ public class DashboardServiceTest extends AbstractBeanTest {
 		assertEquals(Long.valueOf(1), result.get(EventStatus.CLUSTER));
 
 		DeletionDetails details = new DeletionDetails();
+
+        stubFor(
+                post(urlEqualTo("/delete")).withRequestBody(containing("eventTitle"))
+                        .withRequestBody(containing("description"))
+                        .withRequestBody(containing("null"))
+                        .withRequestBody(containing("null"))
+                        .withRequestBody(containing("null"))
+                        .withRequestBody(containing("events"))
+                        .willReturn(aResponse().withStatus(HttpStatus.SC_OK)));
 
 		// delete events (should have an effect on result)
 		eventFacade.delete(signal.getUuid(), details);
