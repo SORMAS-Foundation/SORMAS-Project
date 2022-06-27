2de702adc5e2f70f628de8445108c2b99b776c52
Carina Paul <47103965+carina29@users.noreply.github.com>
2022-06-27 15:47:01 +0300

    #9274-notice survnet about cases/events archivation/dearchivation (#9542)
    * #9274- notice survnet when a cases(-es) or event(-s) was/were archived/dearchived

--- sormas-api/src/main/java/de/symeda/sormas/api/CoreFacade.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/CoreFacade.java
@@ -21,6 +21,7 @@ import java.util.List;
 
 import de.symeda.sormas.api.common.DeletionDetails;
 import de.symeda.sormas.api.deletionconfiguration.DeletionInfoDto;
+import de.symeda.sormas.api.externalsurveillancetool.ExternalSurveillanceToolException;
 import de.symeda.sormas.api.utils.criteria.BaseCriteria;
 
 public interface CoreFacade<DTO extends EntityDto, INDEX_DTO extends Serializable, REF_DTO extends ReferenceDto, CRITERIA extends BaseCriteria>
@@ -43,6 +44,12 @@ public interface CoreFacade<DTO extends EntityDto, INDEX_DTO extends Serializabl
 	void archive(List<String> entityUuid);
 
 	void dearchive(List<String> entityUuids, String dearchiveReason);
+
+	default void setArchiveInExternalSurveillanceToolForEntity(String uuid, boolean archived) throws ExternalSurveillanceToolException {
+	};
+
+	default void setArchiveInExternalSurveillanceToolForEntities(List<String> uuid, boolean archived) throws ExternalSurveillanceToolException {
+	};
 
 	Date calculateEndOfProcessingDate(String entityUuids);
 
