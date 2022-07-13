455e55eecc3ff94afa8af5acecc6b306f14f425c
Pawel Kujawa <pawel.kujawa@softgent.com>
2022-07-13 12:11:21 +0200

    stabilization

--- sormas-e2e-tests/src/test/java/org/sormas/e2etests/steps/web/application/events/EditEventSteps.java
+++ sormas-e2e-tests/src/test/java/org/sormas/e2etests/steps/web/application/events/EditEventSteps.java
@@ -1372,6 +1372,7 @@ public class EditEventSteps implements En {
         "I set Country combobox to empty value from Edit Event Page",
         () -> {
           webDriverHelpers.clearComboboxInput(COUNTRY_COMBOBOX_INPUT);
+          TimeUnit.MILLISECONDS.sleep(500);
           webDriverHelpers.clickOnWebElementBySelector(DEFAULT_COMBOBOX_VALUE);
         });
     When(
