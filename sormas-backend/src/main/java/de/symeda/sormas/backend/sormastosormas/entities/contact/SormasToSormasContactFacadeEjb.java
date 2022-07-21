54bcbe6cdaf43228f0bd825605f5d868ee3c9ef2
Levente Gal <levente.gal.ext@vitagroup.ag>
2022-07-21 16:34:49 +0300

    #9886 S2S | When sharing back a Contact from target system to the source system the Contact can't be accepted #9784 (Backport of #9784) - fix compilation issue

--- sormas-backend/src/main/java/de/symeda/sormas/backend/sormastosormas/entities/contact/SormasToSormasContactFacadeEjb.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/sormastosormas/entities/contact/SormasToSormasContactFacadeEjb.java
@@ -23,6 +23,7 @@ import static de.symeda.sormas.backend.sormastosormas.ValidationHelper.buildCont
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
