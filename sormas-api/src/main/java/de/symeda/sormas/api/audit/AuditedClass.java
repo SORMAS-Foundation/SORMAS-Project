29824533c29019a46d3b5b8157b4a8100b446c0d
Jonas Cirotzki <jonas.cirotzki@posteo.de>
2022-10-14 16:01:03 +0200

    [#9710] clarify docs

--- sormas-api/src/main/java/de/symeda/sormas/api/audit/AuditedClass.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/audit/AuditedClass.java
@@ -9,7 +9,9 @@ import java.lang.annotation.Target;
  * This annotation is used to mark DTO as auditable. This annotation being present causes the log line to contain at
  * least the class name. If more information is required, the fields to be included in the log line can be marked with
  * {@link AuditInclude}. If `includeAllFields` is set to true, all fields will be included in the log line.
+ * In this case, {@Link AuditExclude} annotation can be used to opt-out fields which should not be included.
  */
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface AuditedClass {
