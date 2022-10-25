30d94632ac1581e4d429e29366ccbb1e703b97d1
Pawel Kujawa <pawel.kujawa@softgent.com>
2022-10-25 16:46:38 +0200

    fix

--- sormas-e2e-tests/src/test/java/org/sormas/e2etests/steps/web/application/cases/CreateNewCaseSteps.java
+++ sormas-e2e-tests/src/test/java/org/sormas/e2etests/steps/web/application/cases/CreateNewCaseSteps.java
@@ -86,6 +86,7 @@ import static org.sormas.e2etests.pages.application.cases.CreateNewCasePage.RESP
 import static org.sormas.e2etests.pages.application.cases.CreateNewCasePage.RESPONSIBLE_REGION_COMBOBOX;
 import static org.sormas.e2etests.pages.application.cases.CreateNewCasePage.SAVE_BUTTON;
 import static org.sormas.e2etests.pages.application.cases.CreateNewCasePage.SELECT_COMMIT_BUTTON;
+import static org.sormas.e2etests.pages.application.cases.CreateNewCasePage.SELECT_PERSON_WINDOW_CONFIRM_BUTTON;
 import static org.sormas.e2etests.pages.application.cases.CreateNewCasePage.SEX_COMBOBOX;
 import static org.sormas.e2etests.pages.application.cases.CreateNewCasePage.START_DATA_IMPORT_BUTTON;
 import static org.sormas.e2etests.pages.application.cases.CreateNewCasePage.UUID_EXTERNAL_ID_EXTERNAL_TOKEN_LIKE_INPUT;
@@ -718,9 +719,9 @@ public class CreateNewCaseSteps implements En {
               PERSON_CASE_WINDOW_SEARCH_FIRST_RESULT_OPTION);
           webDriverHelpers.clickOnWebElementBySelector(
               PERSON_CASE_WINDOW_SEARCH_FIRST_RESULT_OPTION);
-          webDriverHelpers.waitUntilElementIsVisibleAndClickable(SELECT_COMMIT_BUTTON);
+          webDriverHelpers.waitUntilElementIsVisibleAndClickable(SELECT_PERSON_WINDOW_CONFIRM_BUTTON);
           TimeUnit.SECONDS.sleep(2); // wait for system reaction
-          webDriverHelpers.clickOnWebElementBySelector(SELECT_COMMIT_BUTTON);
+          webDriverHelpers.clickOnWebElementBySelector(SELECT_PERSON_WINDOW_CONFIRM_BUTTON);
         });
 
     When(
