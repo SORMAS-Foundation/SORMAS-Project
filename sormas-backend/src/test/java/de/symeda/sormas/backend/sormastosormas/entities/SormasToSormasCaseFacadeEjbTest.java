ceb83431d970328c556ca0772b87f5edd5dc1761
Carina Paul <carina.paul@she.net>
2022-11-07 18:22:13 +0200

    #10830 - adjust testShareCase() unit test

--- sormas-backend/src/test/java/de/symeda/sormas/backend/sormastosormas/entities/SormasToSormasCaseFacadeEjbTest.java
+++ sormas-backend/src/test/java/de/symeda/sormas/backend/sormastosormas/entities/SormasToSormasCaseFacadeEjbTest.java
@@ -15,6 +15,14 @@
 
 package de.symeda.sormas.backend.sormastosormas.entities;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.containing;
+import static com.github.tomakehurst.wiremock.client.WireMock.exactly;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.arrayWithSize;
 import static org.hamcrest.Matchers.hasSize;
@@ -34,11 +42,15 @@ import java.util.List;
 
 import javax.ws.rs.core.Response;
 
+import org.apache.http.HttpStatus;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 import de.symeda.sormas.api.Disease;
 import de.symeda.sormas.api.caze.CaseDataDto;
@@ -98,8 +110,13 @@ import de.symeda.sormas.backend.user.User;
 @RunWith(MockitoJUnitRunner.class)
 public class SormasToSormasCaseFacadeEjbTest extends SormasToSormasTest {
 
+	private static final int WIREMOCK_TESTING_PORT = 8888;
+
+	@Rule
+	public WireMockRule wireMockRule = new WireMockRule(options().port(WIREMOCK_TESTING_PORT), false);
+
 	@Test
-	public void testShareCase() throws SormasToSormasException {
+	public void testShareCase_WithCaseNotAllowedToBeSharedWithReportingTool() throws SormasToSormasException {
 
 		UserReferenceDto officer = creator.createUser(rdcf, creator.getUserRoleReference(DefaultUserRole.SURVEILLANCE_OFFICER)).toReference();
 		useSurveillanceOfficerLogin(rdcf);
@@ -109,6 +126,7 @@ public class SormasToSormasCaseFacadeEjbTest extends SormasToSormasTest {
 			dto.setPerson(person.toReference());
 			dto.setSurveillanceOfficer(officer);
 			dto.setClassificationUser(officer);
+			dto.setDontShareWithReportingTool(true);
 		});
 
 		SormasToSormasOptionsDto options = new SormasToSormasOptionsDto();
@@ -144,7 +162,13 @@ public class SormasToSormasCaseFacadeEjbTest extends SormasToSormasTest {
 				return encryptShareData(new ShareRequestAcceptData(null, null));
 			});
 
+		stubFor(
+			post(urlEqualTo("/export")).withRequestBody(containing(caze.getUuid()))
+				.withRequestBody(containing("caseUuids"))
+				.willReturn(aResponse().withStatus(HttpStatus.SC_OK)));
+
 		getSormasToSormasCaseFacade().share(Collections.singletonList(caze.getUuid()), options);
+		wireMockRule.verify(exactly(0), postRequestedFor(urlEqualTo("/export")));
 
 		List<SormasToSormasShareInfoDto> shareInfoList =
 			getSormasToSormasShareInfoFacade().getIndexList(new SormasToSormasShareInfoCriteria().caze(caze.toReference()), 0, 100);
