f0bcd32a808390b19a77f62496ecea78dd01cd33
Maté Strysewske <mate.strysewske@vitagroup.ag>
2022-11-14 14:51:32 +0100

    #10901 - Add user right annotation

--- sormas-backend/src/main/java/de/symeda/sormas/backend/deletionconfiguration/DeletionConfigurationFacadeEjb.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/deletionconfiguration/DeletionConfigurationFacadeEjb.java
@@ -6,7 +6,9 @@ import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
 import de.symeda.sormas.api.deletionconfiguration.DeletionConfigurationFacade;
+import de.symeda.sormas.api.user.UserRight;
 import de.symeda.sormas.backend.common.CronService;
+import de.symeda.sormas.backend.util.RightsAllowed;
 
 @Stateless(name = "DeletionConfigurationFacade")
 public class DeletionConfigurationFacadeEjb implements DeletionConfigurationFacade {
@@ -16,6 +18,7 @@ public class DeletionConfigurationFacadeEjb implements DeletionConfigurationFaca
 
 	@Override
 	@Asynchronous
+	@RightsAllowed(UserRight._DEV_MODE)
 	public void startAutomaticDeletion() {
 		cronService.deleteExpiredEntities();
 	}
