e39e462ec4a2efa0a650a6e10d301655bb2f0298
razvancornita <rrazvan.cornita@gmail.com>
2022-11-24 15:04:04 +0200

    Added html file type (#11007)

--- sormas-backend/src/main/java/de/symeda/sormas/backend/common/ConfigFacadeEjb.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/common/ConfigFacadeEjb.java
@@ -127,7 +127,7 @@ public class ConfigFacadeEjb implements ConfigFacade {
 	public static final String INTERFACE_DEMIS_JNDINAME = "interface.demis.jndiName";
 
 	public static final String ALLOWED_FILE_EXTENSIONS = "allowed.file.extensions";
-	public static final String DEFAULT_ALLOWED_FILETYPES = ".pdf,.txt,.doc,.docx,.odt,.xls,.xlsx,.ods,.ppt,.pptx,.odp,.jpg,.png,.gif,.msg";
+	public static final String DEFAULT_ALLOWED_FILETYPES = ".pdf,.txt,.doc,.docx,.odt,.xls,.xlsx,.ods,.ppt,.pptx,.odp,.jpg,.jpeg,.png,.gif,.msg,.html";
 
 	private static final String DAYS_AFTER_SYSTEM_EVENT_GETS_DELETED = "daysAfterSystemEventGetsDeleted";
 
