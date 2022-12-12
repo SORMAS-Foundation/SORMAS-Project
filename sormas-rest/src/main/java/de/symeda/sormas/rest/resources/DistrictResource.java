0094fc99e00a4106ef442b0af950fb45b740be7d
Jonas Cirotzki <jonas.cirotzki@posteo.de>
2022-12-12 14:16:28 +0100

    [#10251] move resources to resource pkg

---
+++ sormas-rest/src/main/java/de/symeda/sormas/rest/resources/DistrictResource.java
@@ -0,0 +1,98 @@
+/*******************************************************************************
+ * SORMAS® - Surveillance Outbreak Response Management & Analysis System
+ * Copyright © 2016-2018 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *******************************************************************************/
+package de.symeda.sormas.rest.resources;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.validation.Valid;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import de.symeda.sormas.api.FacadeProvider;
+import de.symeda.sormas.api.PushResult;
+import de.symeda.sormas.api.caze.CriteriaWithSorting;
+import de.symeda.sormas.api.common.Page;
+import de.symeda.sormas.api.infrastructure.district.DistrictCriteria;
+import de.symeda.sormas.api.infrastructure.district.DistrictDto;
+import de.symeda.sormas.api.infrastructure.district.DistrictIndexDto;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+/**
+ * @see <a href="https://jersey.java.net/documentation/latest/">Jersey documentation</a>
+ * @see <a href="https://jersey.java.net/documentation/latest/jaxrs-resources.html#d0e2051">Jersey documentation HTTP Methods</a>
+ *
+ */
+@Path("/districts")
+@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+public class DistrictResource extends EntityDtoResource {
+
+	@GET
+	@Path("/all/{since}")
+	public List<DistrictDto> getAll(@PathParam("since") long since) {
+		return FacadeProvider.getDistrictFacade().getAllAfter(new Date(since));
+	}
+
+	@POST
+	@Path("/query")
+	public List<DistrictDto> getByUuids(List<String> uuids) {
+		List<DistrictDto> result = FacadeProvider.getDistrictFacade().getByUuids(uuids);
+		return result;
+	}
+
+	@POST
+	@Path("/push")
+	public List<PushResult> postDistricts(@Valid List<DistrictDto> dtos) {
+		List<PushResult> result = savePushedDto(dtos, FacadeProvider.getDistrictFacade()::save);
+		return result;
+	}
+
+	@GET
+	@Path("/uuids")
+	public List<String> getAllUuids() {
+		return FacadeProvider.getDistrictFacade().getAllUuids();
+	}
+
+	@POST
+	@Path("/indexList")
+	public Page<DistrictIndexDto> getIndexList(
+		@RequestBody CriteriaWithSorting<DistrictCriteria> criteriaWithSorting,
+		@QueryParam("offset") int offset,
+		@QueryParam("size") int size) {
+		return FacadeProvider.getDistrictFacade()
+			.getIndexPage(criteriaWithSorting.getCriteria(), offset, size, criteriaWithSorting.getSortProperties());
+	}
+
+	@POST
+	@Path("/archive")
+	public List<String> archive(@RequestBody List<String> uuids) {
+		return FacadeProvider.getDistrictFacade().archive(uuids);
+	}
+
+	@POST
+	@Path("/dearchive")
+	public List<String> dearchive(@RequestBody List<String> uuids) {
+		return FacadeProvider.getDistrictFacade().dearchive(uuids);
+	}
+
+}
---
+++ sormas-rest/src/main/java/de/symeda/sormas/rest/resources/WeeklyReportResource.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * SORMAS® - Surveillance Outbreak Response Management & Analysis System
+ * Copyright © 2016-2018 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *******************************************************************************/
+package de.symeda.sormas.rest.resources;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+
+import de.symeda.sormas.api.FacadeProvider;
+import de.symeda.sormas.api.PushResult;
+import de.symeda.sormas.api.report.WeeklyReportDto;
+
+@Path("/weeklyreports")
+@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+@Consumes(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+public class WeeklyReportResource extends EntityDtoResource {
+
+	@GET
+	@Path("/all/{since}")
+	public List<WeeklyReportDto> getAllWeeklyReports(@Context SecurityContext sc, @PathParam("since") long since) {
+		return FacadeProvider.getWeeklyReportFacade().getAllWeeklyReportsAfter(new Date(since));
+	}
+
+	@GET
+	@Path("/all/{since}/{size}/{lastSynchronizedUuid}")
+	public List<WeeklyReportDto> getAllWeeklyReports(
+		@Context SecurityContext sc,
+		@PathParam("since") long since,
+		@PathParam("size") int size,
+		@PathParam("lastSynchronizedUuid") String lastSynchronizedUuid) {
+		return FacadeProvider.getWeeklyReportFacade().getAllWeeklyReportsAfter(new Date(since), size, lastSynchronizedUuid);
+	}
+
+	@POST
+	@Path("/query")
+	public List<WeeklyReportDto> getByUuids(@Context SecurityContext sc, List<String> uuids) {
+		List<WeeklyReportDto> result = FacadeProvider.getWeeklyReportFacade().getByUuids(uuids);
+		return result;
+	}
+
+	@POST
+	@Path("/push")
+	public List<PushResult> postWeeklyReports(@Valid List<WeeklyReportDto> dtos) {
+		List<PushResult> result = savePushedDto(dtos, FacadeProvider.getWeeklyReportFacade()::saveWeeklyReport);
+		return result;
+	}
+
+	@GET
+	@Path("/uuids")
+	public List<String> getAllUuids(@Context SecurityContext sc) {
+		return FacadeProvider.getWeeklyReportFacade().getAllUuids();
+	}
+}
