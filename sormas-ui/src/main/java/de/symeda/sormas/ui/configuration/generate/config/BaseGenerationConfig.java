fe71891c46dddb15794f29892e48706364f92e6e
Adrian Dinu <adrian.dinu@she.net>
2022-12-21 07:50:53 +0200

    Bug/10922 developer tab npe error (#11210)
    * #10922 show error for cases
    
    * #10922 show error for contacts
    
    * #10922 show error for events
    
    * #10922 show error for sample
    
    * #10922 refactor code
    
    * #10922 refactor code
    
    * #10922 refactor code

---
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/configuration/generate/config/BaseGenerationConfig.java
@@ -0,0 +1,75 @@
+package de.symeda.sormas.ui.configuration.generate.config;
+
+import java.time.LocalDate;
+
+import org.apache.commons.lang3.StringUtils;
+
+import de.symeda.sormas.api.Disease;
+import de.symeda.sormas.api.infrastructure.district.DistrictReferenceDto;
+import de.symeda.sormas.api.infrastructure.region.RegionReferenceDto;
+
+public class BaseGenerationConfig {
+
+	protected String entityCount;
+	protected LocalDate startDate;
+	protected LocalDate endDate;
+	protected Disease disease;
+	protected RegionReferenceDto region;
+	protected DistrictReferenceDto district;
+
+	public int getEntityCountAsNumber() {
+		return StringUtils.isBlank(getEntityCount()) ? 0 : Integer.parseInt(getEntityCount());
+	}
+
+	public int convetToNumber(String value){
+		return StringUtils.isBlank(value) ? 0 : Integer.parseInt(value);
+	}
+
+	public String getEntityCount() {
+		return entityCount;
+	}
+
+	public void setEntityCount(String entityCount) {
+		this.entityCount = entityCount;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public Disease getDisease() {
+		return disease;
+	}
+
+	public void setDisease(Disease disease) {
+		this.disease = disease;
+	}
+
+	public RegionReferenceDto getRegion() {
+		return region;
+	}
+
+	public void setRegion(RegionReferenceDto region) {
+		this.region = region;
+	}
+
+	public DistrictReferenceDto getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(DistrictReferenceDto district) {
+		this.district = district;
+	}
+}
