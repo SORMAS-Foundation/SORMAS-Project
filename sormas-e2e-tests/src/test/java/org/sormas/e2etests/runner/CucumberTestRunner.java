6b3a6701bd797a3ba6d383de7c93a8a8382baf7e
Pawel Kujawa <pawel.kujawa@softgent.com>
2023-01-05 11:46:32 +0100

    rerunAttempts = 3

--- sormas-e2e-tests/src/test/java/org/sormas/e2etests/runner/CucumberTestRunner.java
+++ sormas-e2e-tests/src/test/java/org/sormas/e2etests/runner/CucumberTestRunner.java
@@ -29,7 +29,7 @@ import org.junit.runner.RunWith;
     runLevel = CourgetteRunLevel.SCENARIO,
     showTestOutput = true,
     rerunFailedScenarios = true,
-    rerunAttempts = 4,
+    rerunAttempts = 3,
     cucumberOptions =
         @CucumberOptions(
             features = "src/test/resources/features",
