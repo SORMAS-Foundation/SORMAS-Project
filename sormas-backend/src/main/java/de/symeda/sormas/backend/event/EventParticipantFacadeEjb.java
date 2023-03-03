862c1bd062d5fb0424dca7ebca1dc470f733c346
sergiupacurariu <62688603+sergiupacurariu@users.noreply.github.com>
2023-03-03 13:32:26 +0200

    #11360 -[Person merge] Introduce merging of event participants in the… (#11581)
    * #11360 -[Person merge] Introduce merging of event participants in the person merge flow - fix rights and info message
    
    * #11360 use PERSON_MERGE right for backend method authorization
    
    ---------
    
    Co-authored-by: Levente Gal <levente.gal.ext@vitagroup.ag>

--- sormas-backend/src/main/java/de/symeda/sormas/backend/event/EventParticipantFacadeEjb.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/event/EventParticipantFacadeEjb.java
@@ -358,6 +358,7 @@ public class EventParticipantFacadeEjb
 	 * @param secondPersonUuid
 	 * @return
 	 */
+	@RightsAllowed(UserRight._PERSON_MERGE)
 	public List<EventParticipantSelectionDto> getEventParticipantsWithSameEvent(String firstPersonUuid, String secondPersonUuid) {
 
 		CriteriaBuilder cb = em.getCriteriaBuilder();
