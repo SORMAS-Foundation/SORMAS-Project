4cd3816650d34eb2fb60ac5c27a8b3647b583c17
sergiupacurariu <62688603+sergiupacurariu@users.noreply.github.com>
2023-03-31 10:53:23 +0300

    #11602 - Show zero values filter in statistics should consider only r… (#11724)
    * #11602 - Show zero values filter in statistics should consider only records in specified date range
    
    * #11602 - Show zero values filter in statistics should consider only records in specified date range

--- sormas-api/src/main/java/de/symeda/sormas/api/statistics/StatisticsHelper.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/statistics/StatisticsHelper.java
@@ -19,6 +19,7 @@ package de.symeda.sormas.api.statistics;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.temporal.IsoFields;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
@@ -49,6 +50,7 @@ import de.symeda.sormas.api.user.UserRoleFacade;
 import de.symeda.sormas.api.user.UserRoleReferenceDto;
 import de.symeda.sormas.api.utils.DateHelper;
 import de.symeda.sormas.api.utils.EpiWeek;
+import de.symeda.sormas.api.utils.UtilDate;
 
 public final class StatisticsHelper {
 
@@ -212,14 +214,6 @@ public final class StatisticsHelper {
 		return ageIntervalList;
 	}
 
-	/**
-	 *
-	 * @param attribute
-	 * @param subAttribute
-	 * @param caseFacade
-	 *            Needed for StatisticsCaseAttribute.ONSET_TIME, REPORT_TIME, OUTCOME_TIME
-	 * @return
-	 */
 	public static List<StatisticsGroupingKey> getTimeGroupingKeys(
 		StatisticsCaseAttribute attribute,
 		StatisticsCaseSubAttribute subAttribute,
@@ -240,13 +234,28 @@ public final class StatisticsHelper {
 			return new ArrayList<>();
 		}
 
-		LocalDate earliest = oldestCaseDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-		LocalDate now = LocalDate.now();
+		return getTimeGroupingKeys(subAttribute, oldestCaseDate, new Date());
+	}
+
+	/**
+	 *
+	 * @param subAttribute
+	 *            Needed for StatisticsCaseAttribute.ONSET_TIME, REPORT_TIME, OUTCOME_TIME
+	 * @return
+	 */
+	public static List<StatisticsGroupingKey> getTimeGroupingKeys(StatisticsCaseSubAttribute subAttribute, Date dateFrom, Date dateTo) {
+
+		if (dateFrom == null && dateTo == null) {
+			return new ArrayList<>();
+		}
+
+		LocalDate earliest = dateFrom.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		LocalDate latest = dateTo.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 
 		switch (subAttribute) {
 		case YEAR:
 			List<StatisticsGroupingKey> years = new ArrayList<>();
-			for (int i = earliest.getYear(); i <= now.getYear(); i++) {
+			for (int i = earliest.getYear(); i <= latest.getYear(); i++) {
 				years.add(new Year(i));
 			}
 			return years;
@@ -266,25 +275,31 @@ public final class StatisticsHelper {
 			return epiWeeks;
 		case QUARTER_OF_YEAR:
 			List<StatisticsGroupingKey> quarterOfYearList = new ArrayList<>();
-			QuarterOfYear earliestQuarter = new QuarterOfYear(new Quarter(1), new Year(earliest.getYear()));
-			QuarterOfYear latestQuarter = new QuarterOfYear(new Quarter(4), new Year(now.getYear()));
-			while (earliestQuarter.getYear().getValue() <= latestQuarter.getYear().getValue()) {
-				quarterOfYearList.add(new QuarterOfYear(earliestQuarter.getQuarter(), earliestQuarter.getYear()));
-				earliestQuarter.increaseQuarter();
+			QuarterOfYear earliestQuarter = new QuarterOfYear(new Quarter(earliest.get(IsoFields.QUARTER_OF_YEAR)), new Year(earliest.getYear()));
+			QuarterOfYear latestQuarter = new QuarterOfYear(new Quarter(latest.get(IsoFields.QUARTER_OF_YEAR)), new Year(latest.getYear()));
+			while (earliestQuarter.compareTo(latestQuarter) <= 0) {
+				quarterOfYearList.add(earliestQuarter);
+				earliestQuarter = earliestQuarter.createNextQuarter();
 			}
 			return quarterOfYearList;
 		case MONTH_OF_YEAR:
 			List<StatisticsGroupingKey> monthOfYearList = new ArrayList<>();
-			for (int year = earliest.getYear(); year <= now.getYear(); year++) {
+			for (int year = earliest.getYear(); year <= latest.getYear(); year++) {
 				for (Month month : Month.values()) {
-					monthOfYearList.add(new MonthOfYear(month, year));
+					if ((earliest.getYear() == year && earliest.getMonth().getValue() <= month.getMonthNumber())
+						|| (latest.getYear() == year && latest.getMonth().getValue() >= month.getMonthNumber())
+						|| (year > earliest.getYear() && year < latest.getYear())) {
+						monthOfYearList.add(new MonthOfYear(month, year));
+					}
 				}
 			}
 			return monthOfYearList;
 		case EPI_WEEK_OF_YEAR:
 			List<StatisticsGroupingKey> epiWeekOfYearList = new ArrayList<>();
-			for (int year = earliest.getYear(); year <= now.getYear(); year++) {
-				epiWeekOfYearList.addAll(DateHelper.createEpiWeekList(year));
+			for (int year = earliest.getYear(); year <= latest.getYear(); year++) {
+				epiWeekOfYearList.addAll(
+					DateHelper
+						.createEpiWeekListFromInterval(DateHelper.getEpiWeek(UtilDate.from(earliest)), DateHelper.getEpiWeek(UtilDate.from(latest))));
 			}
 			return epiWeekOfYearList;
 		default:
@@ -314,7 +329,9 @@ public final class StatisticsHelper {
 		CaseFacade caseFacade,
 		RegionFacade regionFacade,
 		DistrictFacade districtFacade,
-		UserRoleFacade userRoleFacade) {
+		UserRoleFacade userRoleFacade,
+		Date dateFrom,
+		Date dateTo) {
 
 		if (subAttribute != null) {
 			switch (subAttribute) {
@@ -325,7 +342,7 @@ public final class StatisticsHelper {
 			case QUARTER_OF_YEAR:
 			case MONTH_OF_YEAR:
 			case EPI_WEEK_OF_YEAR:
-				return StatisticsHelper.getTimeGroupingKeys(attribute, subAttribute, caseFacade);
+				return StatisticsHelper.getTimeGroupingKeys(subAttribute, dateFrom, dateTo);
 			case REGION:
 				return (List<StatisticsGroupingKey>) (List<? extends StatisticsGroupingKey>) regionFacade.getAllActiveByServerCountry();
 			case DISTRICT:
