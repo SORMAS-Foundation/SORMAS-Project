36950dbff750662b743cc72e4fc02b327847c7e5
Levente Gal <levente.gal.ext@vitagroup.ag>
2023-04-03 16:17:43 +0300

    #11596 Add merge confirmation pop-up to contacts view and rephrase content

---
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/contact/MergeContactsViewConfiguration.java
@@ -0,0 +1,40 @@
+/*
+ * SORMAS® - Surveillance Outbreak Response Management & Analysis System
+ * Copyright © 2016-2023 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package de.symeda.sormas.ui.contact;
+
+public class MergeContactsViewConfiguration {
+
+	/**
+	 * Used to block the initial loading of the duplicates and show a warning message
+	 * until the user edits the filters and loads the list of cases
+	 */
+	private boolean filtersApplied;
+
+	public MergeContactsViewConfiguration() {
+	}
+
+	public MergeContactsViewConfiguration(boolean filtersApplied) {
+		this.filtersApplied = filtersApplied;
+	}
+
+	public boolean isFiltersApplied() {
+		return filtersApplied;
+	}
+
+	public void setFiltersApplied(boolean filtersApplied) {
+		this.filtersApplied = filtersApplied;
+	}
+}
