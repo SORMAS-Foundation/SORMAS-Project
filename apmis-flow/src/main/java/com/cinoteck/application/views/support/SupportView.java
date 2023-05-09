062ebb1bd3d80496855176610a920da14b5c85be
Segun <salamioluwasegun.a@gmail.com>
2023-05-09 10:54:55 +0100

    -ee

--- apmis-flow/src/main/java/com/cinoteck/application/views/support/SupportView.java
+++ apmis-flow/src/main/java/com/cinoteck/application/views/support/SupportView.java
@@ -1,12 +1,19 @@
 package com.cinoteck.application.views.support;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import com.cinoteck.application.views.MainLayout;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.formlayout.FormLayout.ResponsiveStep;
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
@@ -16,49 +23,67 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamRegistration;
+import com.vaadin.flow.server.StreamResource;
+import com.vaadin.flow.server.VaadinSession;
 
 @PageTitle("Support")
 @Route(value = "support", layout = MainLayout.class)
 public class SupportView extends VerticalLayout {
 
-    public SupportView() {
-        Div aboutView = new Div();
-        aboutView.getStyle().set("height", "100%");
-        aboutView.getStyle().set("padding-left", "90px");
-        aboutView.getStyle().set("padding-right", "90px");
+	public SupportView() {
+		Div aboutView = new Div();
+		aboutView.getStyle().set("height", "100%");
+		aboutView.getStyle().set("padding-left", "90px");
+		aboutView.getStyle().set("padding-right", "90px");
 
-        Div apmisImageContainer = new Div();
-        apmisImageContainer.getStyle().set("height", "140px");
-        apmisImageContainer.getStyle().set("display", "flex");
-        apmisImageContainer.getStyle().set("justify-content", "center");
+		Div apmisImageContainer = new Div();
+		apmisImageContainer.getStyle().set("height", "140px");
+		apmisImageContainer.getStyle().set("display", "flex");
+		apmisImageContainer.getStyle().set("justify-content", "center");
 
-        Image img = new Image("images/apmislogo.png", "APMIS-LOGO");
-        img.getStyle().set("max-height", "-webkit-fill-available");
+		Image img = new Image("images/apmislogo.png", "APMIS-LOGO");
+		img.getStyle().set("max-height", "-webkit-fill-available");
 
-        apmisImageContainer.add(img);
+		apmisImageContainer.add(img);
 
-        Div aboutText = new Div();
+		Div aboutText = new Div();
 		aboutText.getStyle().set("height", "121px");
-        Paragraph text = new Paragraph("The Afghanistan Polio Management Information System (APMIS) is an online data system that simplifies and improves the use and management of polio immunization-related data. APMIS facilitates field data entry, immunization data storage, data visualization, and real-time monitoring of polio immunization activities in Afghanistan.  Using this system will assist in evaluating immunization campaign activities and identifying program challenges.");
-        text.getStyle().set("color", "green");
-        aboutText.add(text);
+		Paragraph text = new Paragraph(
+				"The Afghanistan Polio Management Information System (APMIS) is an online data system that simplifies and improves the use and management of polio immunization-related data. APMIS facilitates field data entry, immunization data storage, data visualization, and real-time monitoring of polio immunization activities in Afghanistan.  Using this system will assist in evaluating immunization campaign activities and identifying program challenges.");
+		text.getStyle().set("color", "green");
+		aboutText.add(text);
 
-        HorizontalLayout guides = new HorizontalLayout();
-        Tab techguide = new Tab("Technical Guide");
-        techguide.getStyle().set("color", "green");
-        Tab userguide = new Tab("User Guide");
-        userguide.getStyle().set("color", "green");
+		HorizontalLayout guides = new HorizontalLayout();
+		Tab techguide = new Tab("Technical Guide");
+		techguide.getStyle().set("color", "green");
+		Tab userguide = new Tab("User Guide");
+		userguide.getStyle().set("color", "green");
 
-        Tabs tabs = new Tabs(techguide, userguide);
-       // tabs.getStyle().set("background", "#434343");
+		Tabs tabs = new Tabs(techguide, userguide);
+		// tabs.getStyle().set("background", "#434343");
 
-        guides.add(tabs);
+		guides.add(tabs);
 
 		
+		Anchor anchor = new Anchor("src/main/webapp/docs/APMIS_User_Guide.pdf", "A document");
+		anchor.getElement().setAttribute("router-ignore", true);
+		anchor.getStyle().set("background", "red");
+		
+//		PdfViewer pdfViewer = new PdfViewer();
+		StreamResource resource = new StreamResource("example.pdf", () -> getClass().getResourceAsStream("/docs/APMIS_User_Guide.pdf"));
+//		pdfViewer.setSrc(resource);
+//		pdfViewer.openThumbnailsView();
+//		add(pdfViewer);  
+		anchor.setHref(resource);
 
 		aboutView.add(apmisImageContainer, aboutText, guides);
-        add(aboutView);
+		add(aboutView);
+		add(anchor);
+//		add(downloadButton);
+		
+		
 
-    }
+	}
 
 }
