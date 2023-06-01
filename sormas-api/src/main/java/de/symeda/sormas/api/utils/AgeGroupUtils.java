1ddd4cf434f5d87905a55b4b916a4d2da84e4534
Stefan Kock <stefan.kock@vitagroup.ag>
2023-06-01 16:33:14 +0200

    #11963 Removed compile dependencies in sormas-api
    1. New domainlib:
    commons-codec:commons-codec
    org.springframework:spring-context
    
    2. Superfluous dependencies removed:
    org.apache.poi:poi-ooxml
    org.bouncycastle:bcpkix-jdk15on
    io.swagger.core.v3:swagger-jaxrs2
    io.swagger.core.v3:swagger-jaxrs2-servlet-initializer-v2
    
    3. pom.xml cleanup
    - Explictly used apache commons dependencies listed
    - Dependencies sorted
    - Removed redundant scopes
    
    4. Use commons-collections4 instead of version 3
    
    5. Added now missing dependencies in sormas-app
    com.fasterxml.jackson.core:jackson-databind
    org.springframework:spring-context

--- sormas-api/src/main/java/de/symeda/sormas/api/utils/AgeGroupUtils.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/utils/AgeGroupUtils.java
@@ -21,7 +21,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
