69eb9da6956947b7849b09ae24ebcb213d10ce22
Christopher Riedel <christopher.riedel@vitagroup.ag>
2023-06-22 13:31:47 +0200

    Feature 11796 create a new environment entity and index dto (#12076)
    * #11796: Created Entity, Dto, Facade,Service, Enums and other additional classes for Environment
    
    * #11796 added description for approximate dto json size, introduce in #7560
    
    * SORQA-990 - added fixes
    
    * #11796: fiixed app db script
    
    * #11796: Added more necessary stuff
    
    * #11796: inJurisdictionOrOwned returns always true
    
    * #11796: Added column annotations on the environment entity
    
    * #11796: fixed tests
    
    * Revert "SORQA-990 - added fixes"
    
    This reverts commit 05bad0d61578883915ca81f5dc66e2ebd93743f0.
    
    * #11796: Fixed versioning and deletion triggers
    
    * #11796: fixed review findings
    
    * #11796: fixed review findings
    
    * #11796: fixed review findings
    
    * #11796: implemented necessary methods in EnvironmentFacadeEjb
    
    ---------
    
    Co-authored-by: Martin Wahnschaffe <martin.wahnschaffe@vitagroup.ag>
    Co-authored-by: Razvan <razvan.dutu.ext@vitagroup.ag>
    Co-authored-by: Levente Gal <levente.gal.ext@vitagroup.ag>

---
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/environment/EnvironmentQueryContext.java
@@ -0,0 +1,20 @@
+package de.symeda.sormas.backend.environment;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.From;
+
+import de.symeda.sormas.backend.common.QueryContext;
+
+public class EnvironmentQueryContext extends QueryContext<Environment, EnvironmentJoins> {
+
+	public EnvironmentQueryContext(CriteriaBuilder cb, CriteriaQuery<?> query, From<?, Environment> root) {
+		super(cb, query, root, new EnvironmentJoins(root));
+	}
+
+	@Override
+	protected Expression<?> createExpression(String name) {
+		return null;
+	}
+}
