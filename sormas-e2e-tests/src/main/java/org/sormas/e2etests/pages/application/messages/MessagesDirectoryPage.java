cbc88a55c22423a60d7db044ea70030d4fabf90d
Michal Kozakiewicz <michal.kozakiewicz@softgent.com>
2023-06-27 11:49:05 +0200

    SORQA-1002 Stabilize "Test delete option in Lab Messages" - selector was corrected

--- sormas-e2e-tests/src/main/java/org/sormas/e2etests/pages/application/messages/MessagesDirectoryPage.java
+++ sormas-e2e-tests/src/main/java/org/sormas/e2etests/pages/application/messages/MessagesDirectoryPage.java
@@ -10,7 +10,7 @@ public class MessagesDirectoryPage {
   public static final By MESSAGES_DETAILED_TABLE_ROWS =
       By.cssSelector("div.v-grid-tablewrapper tbody tr");
   public static final By MESSAGE_EYE_ICON =
-      By.xpath("(//div//span[@class='v-icon v-icon-eye'])[1]");
+      By.xpath("(//div//span[@class='v-icon v-icon-eye'][1]");
   public static final By MESSAGE_UUID_TEXT = By.id("uuid");
   public static final By MESSAGE_POPUP_HEADER =
       By.xpath("//div[@class='popupContent']//div[@class='v-window-header' and text()='Meldung']");
