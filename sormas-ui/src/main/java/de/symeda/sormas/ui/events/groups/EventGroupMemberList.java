678c04bf673090b0894a54ea10e7b25985483ef7
Christopher Riedel <christopher.riedel@vitagroup.ag>
2023-07-03 13:45:24 +0200

    #11946: Allow users to link/unlink event groups to events they own (#12100)
    * #11946: Allow users to link/unlink event groups to events they own

--- sormas-ui/src/main/java/de/symeda/sormas/ui/events/groups/EventGroupMemberList.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/events/groups/EventGroupMemberList.java
@@ -81,11 +81,10 @@ public class EventGroupMemberList extends PaginationList<EventIndexDto> {
 			UserProvider user = UserProvider.getCurrent();
 			if (user.hasUserRight(UserRight.EVENTGROUP_LINK)) {
 				listEntry.addUnlinkEventListener(i, (ClickListener) clickEvent -> {
-					if (!user.hasNationJurisdictionLevel() && !user.hasRegion(new RegionReferenceDto(event.getRegionUuid()))
-							&& !user.isAdmin()) {
+					if (!user.hasNationJurisdictionLevel() && !user.hasRegion(new RegionReferenceDto(event.getRegionUuid())) && !user.isAdmin()) {
 						new Notification(
 							I18nProperties.getString(Strings.headingEventGroupUnlinkEventIssue),
-							I18nProperties.getString(Strings.errorEventFromAnotherJurisdiction),
+							I18nProperties.getString(Strings.errorEventsFromAnotherJurisdiction),
 							Notification.Type.ERROR_MESSAGE,
 							false).show(Page.getCurrent());
 						return;
