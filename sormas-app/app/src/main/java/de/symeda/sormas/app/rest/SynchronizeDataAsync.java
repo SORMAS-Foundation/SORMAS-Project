40fb5f46560063985560a57beffac0689662cb47
sergiupacurariu <62688603+sergiupacurariu@users.noreply.github.com>
2023-09-05 11:17:32 +0300

    #12355 - Core entities that are obsolete (archived/soft deleted) are not removed from the mobile app

--- sormas-app/app/src/main/java/de/symeda/sormas/app/rest/SynchronizeDataAsync.java
+++ sormas-app/app/src/main/java/de/symeda/sormas/app/rest/SynchronizeDataAsync.java
@@ -178,7 +178,6 @@ public class SynchronizeDataAsync extends AsyncTask<Void, Void, Void> {
 				// Pull changed data and push existing data that has been changed on the mobile device
 				syncCallbacks.ifPresent(c -> c.getUpdateSynchronizationStepCallback().accept(SynchronizationDialog.SynchronizationStep.SYNCHRONIZE));
 				synchronizeChangedData();
-				pushNewPullMissingAndDeleteInvalidData();
 
 				syncModeTrace.stop();
 				break;
