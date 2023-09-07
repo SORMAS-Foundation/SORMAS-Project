0498eaa0e4dd5e9d614cb03efa4880cc27ce6e31
Maté Strysewske <mate.strysewske@vitagroup.ag>
2023-09-07 10:41:27 +0200

    #12366 - Add facility to external messages (#12482)
    * #12366 - Add facility to external messages
    
    * #12366 - Change relation type

--- sormas-backend/src/main/resources/sql/sormas_schema.sql
+++ sormas-backend/src/main/resources/sql/sormas_schema.sql
@@ -12707,4 +12707,11 @@ ALTER TABLE environmentsamples ALTER COLUMN reportdate DROP DEFAULT;
 
 INSERT INTO schema_version (version_number, comment) VALUES (525, 'Add report date to environment samples #12501');
 
+-- 2023-09-01 Add person facility to external messages #12366
+ALTER TABLE externalmessage ADD COLUMN personfacility_id bigint;
+ALTER TABLE externalmessage_history ADD COLUMN personfacility_id bigint;
+ALTER TABLE externalmessage ADD CONSTRAINT fk_externalmessage_personfacility_id FOREIGN KEY (personfacility_id) REFERENCES facility(id);
+
+INSERT INTO schema_version (version_number, comment) VALUES (526, 'Add person facility to external messages #12366');
+
 -- *** Insert new sql commands BEFORE this line. Remember to always consider _history tables. ***
