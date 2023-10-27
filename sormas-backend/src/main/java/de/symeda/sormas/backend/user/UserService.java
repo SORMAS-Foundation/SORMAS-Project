e61e394738d6abf9f0023008a41663ab9def23e2
Levente Gal <levente.gal.ext@vitagroup.ag>
2023-10-27 10:27:14 +0300

    #11435 Assign multiple limited diseases to users - wip

--- sormas-backend/src/main/java/de/symeda/sormas/backend/user/UserService.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/user/UserService.java
@@ -291,8 +291,9 @@ public class UserService extends AdoServiceWithUserFilterAndJurisdiction<User> {
 
 		// eliminate users that are limited to others diseases
 		if (limitedDisease != null) {
-			Predicate restrictOtherLimitedDiseaseUsers = cb
-				.or(cb.isNull(userRoot.get(User.LIMITED_DISEASES)), cb.like(userRoot.get(User.LIMITED_DISEASES), "%" + limitedDisease.name() + '%'));
+            Predicate restrictOtherLimitedDiseaseUsers = cb.or(
+                    cb.isNull(userRoot.get(User.LIMITED_DISEASES)),
+                    cb.like(userRoot.get(User.LIMITED_DISEASES).as(String.class), "%" + limitedDisease.name() + '%'));
 			filter = CriteriaBuilderHelper.and(cb, filter, restrictOtherLimitedDiseaseUsers);
 		}
 
@@ -387,10 +388,9 @@ public class UserService extends AdoServiceWithUserFilterAndJurisdiction<User> {
 		filter = CriteriaBuilderHelper.and(cb, filter, jurisdictionFilter);
 
 		if (limitedDisease != null) {
-			Predicate restrictOtherLimitedDiseaseUsers =
-				cb.or(
-					cb.isNull(userRoot.get(User.LIMITED_DISEASES)),
-					cb.like(userRoot.get(User.LIMITED_DISEASES), "%" + limitedDisease.name() + "%"));
+            Predicate restrictOtherLimitedDiseaseUsers = cb.or(
+                    cb.isNull(userRoot.get(User.LIMITED_DISEASES)),
+                    cb.like(userRoot.get(User.LIMITED_DISEASES).as(String.class), "%" + limitedDisease.name() + "%"));
 			filter = CriteriaBuilderHelper.and(cb, filter, restrictOtherLimitedDiseaseUsers);
 		}
 
