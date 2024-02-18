dd0887b0d97542f6d4e41f74a4317e3cc59e2cee
daveotengo <30934250+daveotengo@users.noreply.github.com>
2024-02-18 01:30:37 +0000

    fix dashboard bug after merge#12880

--- sormas-api/src/main/java/de/symeda/sormas/api/dashboard/DashboardCriteria.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/dashboard/DashboardCriteria.java
@@ -91,24 +91,13 @@ public class DashboardCriteria extends BaseDashboardCriteria<DashboardCriteria> 
 		return this;
 	}
 
-
-
 	public void setDateFrom(Date dateFrom) {
 		this.dateFrom = dateFrom;
 	}
 
 	public void setDateTo(Date dateTo) {
 		this.dateTo = dateTo;
-  }
-  
-	public Date getDateFrom() {
-		return dateFrom;
-	}
-
-	public Date getDateTo() {
-		return dateTo;
-	}
-
+  	}
 
 	public void setOutcome(CaseOutcome outcome) {
 		this.outcome = outcome;
