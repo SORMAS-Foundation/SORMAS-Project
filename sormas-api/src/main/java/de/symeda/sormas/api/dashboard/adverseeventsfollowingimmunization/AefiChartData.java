221b30a993887024fbda3a92d398261507e4e787
Obinna Henry <55580796+obinna-h-n@users.noreply.github.com>
2024-10-16 14:40:16 +0100

    update sormas_schema.sql for aefi module, fix tests, update user rights in web descriptors

--- sormas-api/src/main/java/de/symeda/sormas/api/dashboard/adverseeventsfollowingimmunization/AefiChartData.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/dashboard/adverseeventsfollowingimmunization/AefiChartData.java
@@ -1,17 +1,14 @@
 /*
  * SORMAS® - Surveillance Outbreak Response Management & Analysis System
  * Copyright © 2016-2024 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -22,6 +19,9 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.symeda.sormas.api.audit.AuditedClass;
+
+@AuditedClass
 public class AefiChartData implements Serializable {
 
 	private static final long serialVersionUID = 3538219674050390425L;
