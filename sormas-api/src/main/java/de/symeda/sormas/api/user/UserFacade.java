d40c820d744e4f01dd7656cf7195b146a36dffe9
daveotengo <30934250+daveotengo@users.noreply.github.com>
2024-10-29 10:00:04 +0000

    clean

--- sormas-api/src/main/java/de/symeda/sormas/api/user/UserFacade.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/user/UserFacade.java
@@ -93,7 +93,7 @@ public interface UserFacade {
 	List<UserReferenceDto> getUserRefsByDistrict(DistrictReferenceDto district, Disease limitedDisease, UserRight... userRights);
 
 	/**
-	 *
+	 * 
 	 * @param district
 	 *            reference of the district to be filtered for. When this district is null, it is not filtered in this regard.
 	 *            NOTE: some users don't have a district (often users with NATIONAL_USER role, for example). They will
