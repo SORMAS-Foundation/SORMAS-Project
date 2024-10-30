ef1600c61f463d2bcbb1bd0957becf98e5616a7f
Marin <marin@bend.md>
2024-10-30 10:33:26 +0200

    Configure and Restrict Sample Types and Pathogen Test Types for RSV #13158

--- sormas-api/src/main/java/de/symeda/sormas/api/sample/PathogenTestType.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/sample/PathogenTestType.java
@@ -17,45 +17,65 @@
  *******************************************************************************/
 package de.symeda.sormas.api.sample;
 
+import de.symeda.sormas.api.CountryHelper;
 import de.symeda.sormas.api.Disease;
 import de.symeda.sormas.api.i18n.I18nProperties;
 import de.symeda.sormas.api.utils.DataHelper;
 import de.symeda.sormas.api.utils.Diseases;
+import de.symeda.sormas.api.utils.HideForCountries;
 
 public enum PathogenTestType {
 
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	ANTIBODY_DETECTION,
 	ANTIGEN_DETECTION,
 	RAPID_TEST,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	CULTURE,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	HISTOPATHOLOGY,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	ISOLATION,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	IGM_SERUM_ANTIBODY,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	IGG_SERUM_ANTIBODY,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	IGA_SERUM_ANTIBODY,
 	@Diseases(value = {
 		Disease.CORONAVIRUS }, hide = true)
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	INCUBATION_TIME,
 	@Diseases(value = {
 		Disease.CORONAVIRUS }, hide = true)
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	INDIRECT_FLUORESCENT_ANTIBODY,
 	@Diseases(value = {
 		Disease.CORONAVIRUS }, hide = true)
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	DIRECT_FLUORESCENT_ANTIBODY,
 	@Diseases(value = {
 		Disease.CORONAVIRUS }, hide = true)
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	MICROSCOPY,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	NEUTRALIZING_ANTIBODIES,
 	PCR_RT_PCR,
 	@Diseases(value = {
 		Disease.CORONAVIRUS }, hide = true)
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	GRAM_STAIN,
 	@Diseases(value = {
 		Disease.CORONAVIRUS }, hide = true)
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	LATEX_AGGLUTINATION,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	CQ_VALUE_DETECTION,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	SEQUENCING,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	DNA_MICROARRAY,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	TMA,
 	OTHER;
 
--- sormas-api/src/main/java/de/symeda/sormas/api/sample/SampleMaterial.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/sample/SampleMaterial.java
@@ -17,51 +17,73 @@
  *******************************************************************************/
 package de.symeda.sormas.api.sample;
 
+import de.symeda.sormas.api.CountryHelper;
 import de.symeda.sormas.api.Disease;
 import de.symeda.sormas.api.i18n.I18nProperties;
 import de.symeda.sormas.api.utils.DataHelper;
 import de.symeda.sormas.api.utils.Diseases;
+import de.symeda.sormas.api.utils.HideForCountries;
 
 public enum SampleMaterial {
 
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	BLOOD,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	SERA,
 	@Diseases(value = {
 		Disease.CORONAVIRUS }, hide = true)
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	STOOL,
 	NASAL_SWAB,
 	THROAT_SWAB,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	NP_SWAB,
 	@Diseases(value = {
 		Disease.CORONAVIRUS }, hide = true)
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	RECTAL_SWAB,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	CEREBROSPINAL_FLUID,
 	@Diseases(value = {
 		Disease.CORONAVIRUS }, hide = true)
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	CRUST,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	TISSUE,
 	@Diseases(value = {
 		Disease.CORONAVIRUS }, hide = true)
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	URINE,
 	@Diseases(value = {
 		Disease.CORONAVIRUS }, hide = true)
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	CORNEA_PM,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	SALIVA,
 	@Diseases(value = {
 		Disease.CORONAVIRUS }, hide = true)
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	URINE_PM,
 	@Diseases(value = {
 		Disease.CORONAVIRUS }, hide = true)
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	NUCHAL_SKIN_BIOPSY,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	SPUTUM,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	ENDOTRACHEAL_ASPIRATE,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	BRONCHOALVEOLAR_LAVAGE,
 	@Diseases(value = {
 		Disease.CORONAVIRUS }, hide = true)
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	BRAIN_TISSUE,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	ANTERIOR_NARES_SWAB,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	OP_ASPIRATE,
 	NP_ASPIRATE,
+	@HideForCountries(countries = CountryHelper.COUNTRY_CODE_LUXEMBOURG)
 	PLEURAL_FLUID,
 	OTHER;
 
