aad5608e00c672a4ff61acaa4c7b04b579b93675
daveotengo <30934250+daveotengo@users.noreply.github.com>
2024-11-11 09:26:20 +0000

    update with license

--- sormas-ui/src/main/java/de/symeda/sormas/ui/dashboard/surveillance/components/disease/tile/RegionalDiseaseBurdenGrid.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/dashboard/surveillance/components/disease/tile/RegionalDiseaseBurdenGrid.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * SORMAS® - Surveillance Outbreak Response Management & Analysis System
+ * Copyright © 2016-2018 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package de.symeda.sormas.ui.dashboard.surveillance.components.disease.tile;
 
 import com.vaadin.v7.data.util.BeanItemContainer;
@@ -38,6 +55,7 @@ public class RegionalDiseaseBurdenGrid extends Grid {
 	private final DecimalFormat decimalFormat;
 
 	public RegionalDiseaseBurdenGrid(DashboardDataProvider dashboardDataProvider) {
+
 		this.dashboardDataProvider = dashboardDataProvider;
 		regionDtoList = FacadeProvider.getRegionFacade().getAllActiveRegions();
 
@@ -83,6 +101,7 @@ public class RegionalDiseaseBurdenGrid extends Grid {
 	}
 
 	public void refresh(){
+
 		setColumnResizeMode(ColumnResizeMode.ANIMATED);
 		setSelectionMode(Grid.SelectionMode.NONE);
 
@@ -231,6 +250,7 @@ public class RegionalDiseaseBurdenGrid extends Grid {
 	}
 
 	public String makeDIvsCount(String num) {
+
 		String  endDiv = " </div>";
 		String divWithStyleAttr="<div style='";
 
@@ -242,6 +262,7 @@ public class RegionalDiseaseBurdenGrid extends Grid {
 	}
 
 	public String element(String type, String style, String content) {
+
 		StringBuilder sb = new StringBuilder();
 		sb.append("<").append(type);
 		if (style != null) {
