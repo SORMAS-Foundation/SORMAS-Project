387d057c7ee3ab1669daf0199a002ee4e28d6c7e
Raul Bob <raulbob@gmail.com>
2025-05-28 19:20:16 +0200

    #13375 - Added EPI data hanling for doctor declaration processing
    Added fields for activities as case and exposures to external message:
    - `ExternalMessageDto`
    - `ExternalMessage`
    - `ExternalMessageFacadeEjb`
    
    Added handling of EPI data in `AbstractDoctorDeclarationMessageProcessingFlow`:
    - creation of `Exposure` entries based on the `ExternalMessage.exposures` fields
    - creation of 'ActivityAsCase` entries based on the `ExternalMessage.activitiesAsCase` fields' accordinlgy to the Pertussis documentation.

--- sormas-api/src/main/java/de/symeda/sormas/api/externalmessage/processing/doctordeclaration/AbstractDoctorDeclarationMessageProcessingFlow.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/externalmessage/processing/doctordeclaration/AbstractDoctorDeclarationMessageProcessingFlow.java
@@ -1,6 +1,6 @@
 /*
  * SORMAS® - Surveillance Outbreak Response Management & Analysis System
- * Copyright © 2016-2023 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+ * Copyright © 2016-2026 SORMAS Foundation gGmbH
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -15,11 +15,18 @@
 
 package de.symeda.sormas.api.externalmessage.processing.labmessage;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.symeda.sormas.api.activityascase.ActivityAsCaseDto;
+import de.symeda.sormas.api.activityascase.ActivityAsCaseType;
 import de.symeda.sormas.api.caze.CaseDataDto;
 import de.symeda.sormas.api.caze.CaseOutcome;
 import de.symeda.sormas.api.caze.CaseSelectionDto;
@@ -27,9 +34,12 @@ import de.symeda.sormas.api.caze.InvestigationStatus;
 import de.symeda.sormas.api.caze.surveillancereport.SurveillanceReportDto;
 import de.symeda.sormas.api.contact.ContactDto;
 import de.symeda.sormas.api.contact.SimilarContactDto;
+import de.symeda.sormas.api.epidata.EpiDataDto;
 import de.symeda.sormas.api.event.EventDto;
 import de.symeda.sormas.api.event.EventParticipantDto;
 import de.symeda.sormas.api.event.SimilarEventParticipantDto;
+import de.symeda.sormas.api.exposure.ExposureDto;
+import de.symeda.sormas.api.exposure.ExposureType;
 import de.symeda.sormas.api.externalmessage.ExternalMessageDto;
 import de.symeda.sormas.api.externalmessage.ExternalMessageStatus;
 import de.symeda.sormas.api.externalmessage.processing.AbstractMessageProcessingFlowBase;
@@ -45,6 +55,7 @@ import de.symeda.sormas.api.therapy.TherapyReferenceDto;
 import de.symeda.sormas.api.therapy.TreatmentDto;
 import de.symeda.sormas.api.user.UserDto;
 import de.symeda.sormas.api.utils.DtoCopyHelper;
+import de.symeda.sormas.api.utils.YesNoUnknown;
 import de.symeda.sormas.api.utils.dataprocessing.ProcessingResult;
 import de.symeda.sormas.api.utils.dataprocessing.flow.FlowThen;
 
@@ -231,11 +242,21 @@ public abstract class AbstractDoctorDeclarationMessageProcessingFlow extends Abs
 
 		postBuildCaseSymptoms(caseDto, externalMessageDto);
 		postBuildCaseTherapy(caseDto, externalMessageDto);
+		postBuildActivitiesAsCase(caseDto, externalMessageDto);
+		postBuildExposure(caseDto, externalMessageDto);
 
 		caseDto.setInvestigationStatus(InvestigationStatus.PENDING);
 		caseDto.setOutcome(CaseOutcome.NO_OUTCOME);
 	}
 
+	/**
+	 * Sets the symptoms for the case from the external message, if present.
+	 *
+	 * @param caseDto
+	 *            The case data transfer object to update.
+	 * @param externalMessageDto
+	 *            The external message containing symptom data.
+	 */
 	protected void postBuildCaseSymptoms(CaseDataDto caseDto, ExternalMessageDto externalMessageDto) {
 		if (externalMessageDto.getCaseSymptoms() != null) {
 			final SymptomsDto symptomsDto = SymptomsDto.build();
@@ -246,6 +267,14 @@ public abstract class AbstractDoctorDeclarationMessageProcessingFlow extends Abs
 		}
 	}
 
+	/**
+	 * Sets the therapy information for the case from the external message, if present.
+	 *
+	 * @param caseDto
+	 *            The case data transfer object to update.
+	 * @param externalMessageDto
+	 *            The external message containing therapy data.
+	 */
 	protected void postBuildCaseTherapy(CaseDataDto caseDto, ExternalMessageDto externalMessageDto) {
 
 		TherapyDto therapyDto = caseDto.getTherapy();
@@ -256,6 +285,112 @@ public abstract class AbstractDoctorDeclarationMessageProcessingFlow extends Abs
 
 		logger.debug("[POST BUILD CASE] Therapy set for case with UUID: {}", caseDto.getUuid());
 
+	}
+
+	/**
+	 * Sets the activities as case for the case from the external message, if present.
+	 * <p>
+	 * This method deserializes the activities as case from a JSON string in the external message
+	 * into a list of {@link ActivityAsCaseDto} objects. Each activity is built and copied, and if
+	 * the activity type is missing, it is set to {@link ActivityAsCaseType#UNKNOWN}.
+	 * If there are activities, the {@code activityAsCaseDetailsKnown} field is set to {@link YesNoUnknown#YES}.
+	 * </p>
+	 *
+	 * @param caseDto
+	 *            The case data transfer object to update.
+	 * @param externalMessageDto
+	 *            The external message containing activities as case data in JSON format.
+	 */
+	protected void postBuildActivitiesAsCase(CaseDataDto caseDto, ExternalMessageDto externalMessageDto) {
+
+		if (externalMessageDto.getActivitiesAsCase() != null && !externalMessageDto.getActivitiesAsCase().isEmpty()) {
+			final ArrayList<ActivityAsCaseDto> activitiesAsCase = new ArrayList<>();
+
+			try {
+				ObjectMapper objectMapper = new ObjectMapper();
+				List<ActivityAsCaseDto> deserialActivityAsCaseDtos =
+					objectMapper.readValue(externalMessageDto.getActivitiesAsCase(), new TypeReference<List<ActivityAsCaseDto>>() {
+					});
+				for (ActivityAsCaseDto activityAsCaseDto : deserialActivityAsCaseDtos) {
+					ActivityAsCaseDto newActivityAsCase = ActivityAsCaseDto.build(activityAsCaseDto.getActivityAsCaseType());
+					if (newActivityAsCase.getActivityAsCaseType() == null) {
+						newActivityAsCase.setActivityAsCaseType(ActivityAsCaseType.UNKNOWN);
+					}
+					DtoCopyHelper.copyDtoValues(newActivityAsCase, activityAsCaseDto, true, "uuid");
+					activitiesAsCase.add(newActivityAsCase);
+				}
+			} catch (Exception e) {
+				logger.error("[POST BUILD CASE] Error while processing activities as case for case with UUID: {}", caseDto.getUuid(), e);
+				return;
+			}
+
+			if (!activitiesAsCase.isEmpty()) {
+				EpiDataDto epiData = caseDto.getEpiData();
+				if (epiData == null) {
+					epiData = EpiDataDto.build();
+					caseDto.setEpiData(epiData);
+				}
+
+				epiData.setActivityAsCaseDetailsKnown(YesNoUnknown.YES);
+				epiData.setActivitiesAsCase(activitiesAsCase);
+			}
+
+		} else {
+			logger.debug("[POST BUILD CASE] No activities to set for case with UUID: {}", caseDto.getUuid());
+		}
+	}
+
+	/**
+	 * Sets the exposure information for the case from the external message, if present.
+	 * <p>
+	 * This method deserializes the exposures from a JSON string in the external message
+	 * into a list of {@link ExposureDto} objects. Each exposure is built and copied, and if
+	 * the exposure type is missing, it is set to {@link ExposureType#UNKNOWN}.
+	 * If there are exposures, the {@code exposureDetailsKnown} field is set to {@link YesNoUnknown#YES}.
+	 * </p>
+	 *
+	 * @param caseDto
+	 *            The case data transfer object to update.
+	 * @param externalMessageDto
+	 *            The external message containing exposure data in JSON format.
+	 */
+	protected void postBuildExposure(CaseDataDto caseDto, ExternalMessageDto externalMessageDto) {
+
+		if (externalMessageDto.getExposures() != null && !externalMessageDto.getExposures().isEmpty()) {
+			final ArrayList<ExposureDto> exposures = new ArrayList<>();
+
+			try {
+				ObjectMapper objectMapper = new ObjectMapper();
+				List<ExposureDto> deserialExposureDtos =
+					objectMapper.readValue(externalMessageDto.getExposures(), new TypeReference<List<ExposureDto>>() {
+					});
+				for (ExposureDto exposureDto : deserialExposureDtos) {
+					ExposureDto newExposure = ExposureDto.build(exposureDto.getExposureType());
+					if (newExposure.getExposureType() == null) {
+						newExposure.setExposureType(ExposureType.UNKNOWN);
+					}
+					DtoCopyHelper.copyDtoValues(newExposure, exposureDto, true, "uuid");
+					exposures.add(newExposure);
+				}
+			} catch (Exception e) {
+				logger.error("[POST BUILD CASE] Error while processing exposures for case with UUID: {}", caseDto.getUuid(), e);
+				return;
+			}
+
+			if (!exposures.isEmpty()) {
+				EpiDataDto epiData = caseDto.getEpiData();
+				if (epiData == null) {
+					epiData = EpiDataDto.build();
+					caseDto.setEpiData(epiData);
+				}
+
+				epiData.setExposureDetailsKnown(YesNoUnknown.YES);
+				epiData.setExposures(exposures);
+			}
+
+		} else {
+			logger.debug("[POST BUILD CASE] No exposures to set for case with UUID: {}", caseDto.getUuid());
+		}
 	}
 
 	/**
