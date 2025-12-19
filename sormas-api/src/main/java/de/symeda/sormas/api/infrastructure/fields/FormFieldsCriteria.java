676160dc144c891ada9acbb19a7f6421166f299d
douglasderrick <douglasder12@gmail.com>
2025-12-19 08:47:18 +0000

    Add FormField management API layer
    - Add FormFieldsCriteria for filtering FormField queries
    - Add FormFieldIndexDto for FormField list views
    - Add FormFieldFacade interface extending InfrastructureFacade
    - Enables FormField CRUD operations through API

---
+++ sormas-api/src/main/java/de/symeda/sormas/api/infrastructure/fields/FormFieldsCriteria.java
@@ -0,0 +1,56 @@
+/*
+ * SORMAS® - Surveillance Outbreak Response Management & Analysis System
+ * Copyright © 2016-2022 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package de.symeda.sormas.api.infrastructure.fields;
+
+import java.io.Serializable;
+
+import de.symeda.sormas.api.EntityRelevanceStatus;
+import de.symeda.sormas.api.FormType;
+import de.symeda.sormas.api.utils.criteria.BaseCriteria;
+
+public class FormFieldsCriteria extends BaseCriteria implements Serializable, Cloneable {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final String FORM_TYPE = "formType";
+	public static final String RELEVANCE_STATUS = "relevanceStatus";
+
+	private FormType formType;
+	private EntityRelevanceStatus relevanceStatus;
+
+	public FormType getFormType() {
+		return formType;
+	}
+
+	public FormFieldsCriteria formType(FormType formType) {
+		this.formType = formType;
+		return this;
+	}
+
+	public void setFormType(FormType formType) {
+		this.formType = formType;
+	}
+
+	public EntityRelevanceStatus getRelevanceStatus() {
+		return relevanceStatus;
+	}
+
+	public FormFieldsCriteria relevanceStatus(EntityRelevanceStatus relevanceStatus) {
+		this.relevanceStatus = relevanceStatus;
+		return this;
+	}
+}
+
