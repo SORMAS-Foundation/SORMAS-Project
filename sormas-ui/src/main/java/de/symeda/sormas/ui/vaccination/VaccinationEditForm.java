90c15f7fe9549e44cfc7ab9ee85ec250de17ff82
Karnaiah Pesula <karnaiah.pesula@sormas.org>
2026-03-12 16:46:03 +0100

    Vaccine changes: possible NPE fix

--- sormas-ui/src/main/java/de/symeda/sormas/ui/vaccination/VaccinationEditForm.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/vaccination/VaccinationEditForm.java
@@ -104,7 +104,7 @@ public class VaccinationEditForm extends AbstractEditForm<VaccinationDto> {
 		vaccineName.addValueChangeListener(e -> {
 			Vaccine vaccine = (Vaccine) e.getProperty().getValue();
 			if (vaccine != null) {
-				vaccineManufacturer.setValue(vaccine.getManufacturer().get());
+				vaccineManufacturer.setValue(vaccine.getManufacturer().isEmpty() ? null : vaccine.getManufacturer().get());
 
 				// VaccineType
 				if (vaccine.getVaccineType().isPresent()) {
