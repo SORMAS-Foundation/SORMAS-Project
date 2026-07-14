ddff84704baab86e9ce7a5d918d700ce25b7f759
Pa-Touche <47572440+Pa-Touche@users.noreply.github.com>
2026-07-14 14:09:22 +0200

    FEAT: 14206: Add Syphilis in disease enum

--- sormas-api/src/main/java/de/symeda/sormas/api/Disease.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/Disease.java
@@ -96,6 +96,7 @@ public enum Disease
 	CRYPTOSPORIDIOSIS(true, true, true, false, true, 14, false, false, false, true, 2, 12, false, 0, 0),
 	SHIGELLOSIS(true, true, true, false, true, 14, false, false, false, true, 0, 7, true, 1, 7),
 	SALMONELLOSIS(true, true, true, false, true, 14, false, false, false, true, 1, 3, false, 0, 0),
+	SYPHILIS(true, true, true, false, false, 0, true, false, false, true, 10, 90, false, 0, 0),
 	OTHER(true, true, true, false, true, 21, false, false, false, false, 0, 0, false, 0, 0),
 	UNDEFINED(true, true, true, false, true, 0, false, false, false, false, 0, 0, false, 0, 0);
 
