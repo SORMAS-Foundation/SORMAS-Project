e7ea93596cc2d90890b305e745f6bb8d96e8c182
Pa-Touche <47572440+Pa-Touche@users.noreply.github.com>
2026-08-05 15:15:42 +0200

    Added unit test for PDF encryption.
    Meant to make sure that the PDF-Layout is not tempered when doing a

--- sormas-backend/src/main/java/de/symeda/sormas/backend/externalemail/AttachmentService.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/externalemail/AttachmentService.java
@@ -1,18 +1,16 @@
 /*
  * SORMAS® - Surveillance Outbreak Response Management & Analysis System
  * Copyright © 2016-2023 Helmholtz-Zentrum für Infektionsforschung GmbH (HZI)
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package de.symeda.sormas.backend.externalemail;
@@ -59,29 +57,29 @@ import fr.opensagres.poi.xwpf.converter.pdf.PdfOptions;
 @LocalBean
 public class AttachmentService {
 
-    public static final String DOCX_FILE_EXTENSION = ".docx";
-    public static final String PDF_FILE_EXTENSION = ".pdf";
-    public static final String IMAGE_FILE_EXTENSTIONS = ".jpg,.jpeg,.png,.gif";
-    private static final String TEMP_FILE_PREFIX = "sormas_temp";
-    private static final Random RANDOM = new Random();
+	public static final String DOCX_FILE_EXTENSION = ".docx";
+	public static final String PDF_FILE_EXTENSION = ".pdf";
+	public static final String IMAGE_FILE_EXTENSTIONS = ".jpg,.jpeg,.png,.gif";
+	private static final String TEMP_FILE_PREFIX = "sormas_temp";
+	private static final Random RANDOM = new Random();
 
-    @EJB
-    private DocumentService documentService;
-    @EJB
-    private DocumentStorageService documentStorageService;
-    @EJB
-    private ConfigFacadeEjb.ConfigFacadeEjbLocal configFacade;
+	@EJB
+	private DocumentService documentService;
+	@EJB
+	private DocumentStorageService documentStorageService;
+	@EJB
+	private ConfigFacadeEjb.ConfigFacadeEjbLocal configFacade;
 
-    // @formatter:off
+	// @formatter:off
     private final Map<String, PdfConverter> converters = Map.of(
             DOCX_FILE_EXTENSION, new DocXConverter(),
             IMAGE_FILE_EXTENSTIONS, new ImageConverter()
     );
     // @formatter:on
 
-    public Set<String> getAttachableFileExtensions() {
-        return Set.of((PDF_FILE_EXTENSION + "," + DOCX_FILE_EXTENSION + "," + IMAGE_FILE_EXTENSTIONS).split(","));
-    }
+	public Set<String> getAttachableFileExtensions() {
+		return Set.of((PDF_FILE_EXTENSION + "," + DOCX_FILE_EXTENSION + "," + IMAGE_FILE_EXTENSTIONS).split(","));
+	}
 
 	public Map<File, String> createEncryptedPdfs(Map<File, String> sormasDocuments, String password) {
 		Map<File, String> encryptedFiles = new HashMap<>(Collections.emptyMap());
@@ -93,10 +91,10 @@ public class AttachmentService {
 				final File encryptedPdf;
 				if (fileExtension.equals(PDF_FILE_EXTENSION)) {
 					encryptedPdf = encryptPdf(document, password);
-                } else {
-                    PdfConverter converter = getConverter(fileExtension);
-                    fileName = converter.getConvertedFileName(fileName);
-                    File converted = converter.convert(document);
+				} else {
+					PdfConverter converter = getConverter(fileExtension);
+					fileName = converter.getConvertedFileName(fileName);
+					File converted = converter.convert(document);
 					encryptedPdf = encryptPdf(converted, password);
 					converted.delete();
 				}
@@ -110,92 +108,92 @@ public class AttachmentService {
 		return encryptedFiles;
 	}
 
-    private File encryptPdf(File pdf, String password) throws IOException {
-        PDDocument pdd = Loader.loadPDF(pdf);
+	File encryptPdf(File pdf, String password) throws IOException {
+		PDDocument pdd = Loader.loadPDF(pdf);
 
-        AccessPermission ap = new AccessPermission();
-        StandardProtectionPolicy stpp = new StandardProtectionPolicy(password, password, ap);
-        stpp.setEncryptionKeyLength(128);
-        stpp.setPermissions(ap);
-        pdd.protect(stpp);
+		AccessPermission ap = new AccessPermission();
+		StandardProtectionPolicy stpp = new StandardProtectionPolicy(password, password, ap);
+		stpp.setEncryptionKeyLength(128);
+		stpp.setPermissions(ap);
+		pdd.protect(stpp);
 
-        String encryptedFile = getTmpFilePathForConversion() + "_encrypted" + PDF_FILE_EXTENSION;
-        pdd.save(encryptedFile);
-        pdd.close();
+		String encryptedFile = getTmpFilePathForConversion() + "_encrypted" + PDF_FILE_EXTENSION;
+		pdd.save(encryptedFile);
+		pdd.close();
 
-        return new File(encryptedFile);
-    }
+		return new File(encryptedFile);
+	}
 
-    private PdfConverter getConverter(String fileExtension) {
-        Optional<String> converterKey = converters.keySet().stream().filter(k -> k.contains(fileExtension)).findFirst();
-        if (converterKey.isEmpty()) {
-            throw new IllegalArgumentException("No converter found for file extension " + fileExtension);
-        }
+	private PdfConverter getConverter(String fileExtension) {
+		Optional<String> converterKey = converters.keySet().stream().filter(k -> k.contains(fileExtension)).findFirst();
+		if (converterKey.isEmpty()) {
+			throw new IllegalArgumentException("No converter found for file extension " + fileExtension);
+		}
 
-        return converters.get(converterKey.get());
-    }
+		return converters.get(converterKey.get());
+	}
 
-    private Path getTmpFilePathForConversion() {
-        Path path = Paths.get(configFacade.getTempFilesPath());
-        String fileName = TEMP_FILE_PREFIX + "_converted_" + DateHelper.formatDateForExport(new Date()) + "_" + RANDOM.nextInt(Integer.MAX_VALUE);
+	private Path getTmpFilePathForConversion() {
+		Path path = Paths.get(configFacade.getTempFilesPath());
+		String fileName = TEMP_FILE_PREFIX + "_converted_" + DateHelper.formatDateForExport(new Date()) + "_" + RANDOM.nextInt(Integer.MAX_VALUE);
 
-        return path.resolve(fileName);
+		return path.resolve(fileName);
 
-    }
+	}
 
-    private interface PdfConverter {
+	private interface PdfConverter {
 
-        File convert(File file) throws IOException;
+		File convert(File file) throws IOException;
 
-        default String getConvertedFileName(String fileName) {
-            return fileName.substring(0, fileName.lastIndexOf(".")) + PDF_FILE_EXTENSION;
-        }
-    }
+		default String getConvertedFileName(String fileName) {
+			return fileName.substring(0, fileName.lastIndexOf(".")) + PDF_FILE_EXTENSION;
+		}
+	}
 
-    private class DocXConverter implements PdfConverter {
+	private class DocXConverter implements PdfConverter {
 
-        @Override
-        public File convert(File file) throws IOException {
-            String convertedFilePath = getTmpFilePathForConversion() + PDF_FILE_EXTENSION;
-            try (InputStream inputStream = new FileInputStream(file); OutputStream outputStream = new FileOutputStream(convertedFilePath)) {
-                XWPFDocument document = new XWPFDocument(inputStream);
-                PdfOptions options = PdfOptions.create();
-                // Convert .docx file to .pdf file
-                fr.opensagres.poi.xwpf.converter.pdf.PdfConverter.getInstance().convert(document, outputStream, options);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
+		@Override
+		public File convert(File file) throws IOException {
+			String convertedFilePath = getTmpFilePathForConversion() + PDF_FILE_EXTENSION;
+			try (InputStream inputStream = new FileInputStream(file); OutputStream outputStream = new FileOutputStream(convertedFilePath)) {
+				XWPFDocument document = new XWPFDocument(inputStream);
+				PdfOptions options = PdfOptions.create();
+				// Convert .docx file to .pdf file
+				fr.opensagres.poi.xwpf.converter.pdf.PdfConverter.getInstance().convert(document, outputStream, options);
+			} catch (FileNotFoundException e) {
+				throw new RuntimeException(e);
+			}
 
-            return new File(convertedFilePath);
-        }
-    }
+			return new File(convertedFilePath);
+		}
+	}
 
-    private class ImageConverter implements PdfConverter {
+	private class ImageConverter implements PdfConverter {
 
-        @Override
-        public File convert(File file) throws IOException {
-            String convertedFilePath = getTmpFilePathForConversion() + PDF_FILE_EXTENSION;
+		@Override
+		public File convert(File file) throws IOException {
+			String convertedFilePath = getTmpFilePathForConversion() + PDF_FILE_EXTENSION;
 
-            try (FileOutputStream fos = new FileOutputStream(convertedFilePath);) {
-                Document document = new Document();
-                PdfWriter writer = PdfWriter.getInstance(document, fos);
+			try (FileOutputStream fos = new FileOutputStream(convertedFilePath);) {
+				Document document = new Document();
+				PdfWriter writer = PdfWriter.getInstance(document, fos);
 
-                writer.open();
-                document.open();
+				writer.open();
+				document.open();
 
-                Image image = Image.getInstance(file.getPath());
-                float scale = ((document.getPageSize().getWidth() - document.leftMargin() - document.rightMargin()) / image.getWidth()) * 100;
-                image.scalePercent(scale);
-                document.add(image);
+				Image image = Image.getInstance(file.getPath());
+				float scale = ((document.getPageSize().getWidth() - document.leftMargin() - document.rightMargin()) / image.getWidth()) * 100;
+				image.scalePercent(scale);
+				document.add(image);
 
-                document.close();
-                writer.close();
-            } catch (DocumentException e) {
-                throw new RuntimeException(e);
-            }
+				document.close();
+				writer.close();
+			} catch (DocumentException e) {
+				throw new RuntimeException(e);
+			}
 
-            return new File(convertedFilePath);
-        }
-    }
+			return new File(convertedFilePath);
+		}
+	}
 
 }
