4ed26beb34088905aa703f4e485934e706f894e9
Pa-Touche <47572440+Pa-Touche@users.noreply.github.com>
2026-09-16 07:38:52 +0200

    PR comments

--- sormas-ui/src/main/java/de/symeda/sormas/ui/samples/PathogenTestController.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/samples/PathogenTestController.java
@@ -76,13 +76,9 @@ public class PathogenTestController {
 
 	private final PathogenTestFacade facade = FacadeProvider.getPathogenTestFacade();
 
-	// Antibiotic susceptibility test is applicable for TB(Lux), IMI, IPI and Shigellosis. For others it should be null.
-	private static final List<Disease> AST_ALLOWED_DISEASES =
-		Arrays.asList(
-			Disease.INVASIVE_MENINGOCOCCAL_INFECTION,
-			Disease.INVASIVE_PNEUMOCOCCAL_INFECTION,
-			Disease.SHIGELLOSIS,
-			Disease.GONOCOCCAL_INFECTION);
+	// Antibiotic susceptibility test is applicable for TB(Lux), IMI, IPI, Shigellosis and Gonococcal infection. For others it should be null.
+	private static final List<Disease> AST_ALLOWED_DISEASES = Arrays
+		.asList(Disease.INVASIVE_MENINGOCOCCAL_INFECTION, Disease.INVASIVE_PNEUMOCOCCAL_INFECTION, Disease.SHIGELLOSIS, Disease.GONOCOCCAL_INFECTION);
 
 	public PathogenTestController() {
 	}
